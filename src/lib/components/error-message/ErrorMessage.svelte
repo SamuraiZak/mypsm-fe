c0392ced5e15dfec84c3de74196bc2be2cd46c64
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2023-12-19 17:23:38 +0800

    feat: WIP creates the error message component.

---
+++ src/lib/components/error-message/ErrorMessage.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let condition: boolean = false;
+    export let message: string = '';
+</script>
+
+{#if condition}
+    <span class="ml-[220px] font-sans text-sm italic text-system-danger"
+        >{message}</span
+    >
+{/if}
