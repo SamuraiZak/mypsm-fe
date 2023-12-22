18ef28e14fb4fcc14fcba5d93d30ca9b982ad6d3
Holifield William <holifield.william@impactbs.my>
2023-12-22 08:18:43 +0800

    chore: added some comments for the table head cell

--- src/lib/components/complex-table/CxTableHeadCell.svelte
+++ src/lib/components/complex-table/CxTableHeadCell.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+    // rowSize = size of row that particular headcell can occupy, 
+    // e;g: rowSize=2 mean that headcell will have the size of two rows
+    // columnSize = size of column that particular headcell can occupy, 
+    // e;g: columnSize=3 mean that headcell will have the size of three columns (that header will have 3 sub-headers)
     export let rowSize: number = 1;
     export let columnSize: number = 1;
     export let title: string = '';
