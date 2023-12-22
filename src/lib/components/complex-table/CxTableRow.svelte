3a4f8baf7ffc5ac4a24af9ca6061ddd183478cca
Holifield William <holifield.william@impactbs.my>
2023-12-22 08:19:42 +0800

    feat: added colors option for the table row

--- src/lib/components/complex-table/CxTableRow.svelte
+++ src/lib/components/complex-table/CxTableRow.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
+    export let headerColor: boolean = false;
 </script>
 
-<tr class="border-b last:border-b-0 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-    <slot/>
+<tr
+    class="border-b last:border-b-0 dark:border-gray-700 dark:bg-gray-800 {headerColor
+        ? 'bg-gray-50'
+        : 'bg-white'}"
+>
+    <slot />
 </tr>
