eb7028d9fcfbe659225efa2a862d83af21489b97
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2023-12-26 16:36:51 +0800

    feat: adds comment.

--- src/routes/(guarded)/urus-setia/latihan/kemaskini-peperiksaan/butiran-peperiksaan-[id]/+page.ts
+++ src/routes/(guarded)/urus-setia/latihan/kemaskini-peperiksaan/butiran-peperiksaan-[id]/+page.ts
@@ -1,5 +1,6 @@
+// a module to exports the load function that fetches data for the +page.svelte.
+
 import { mockExams } from '$lib/mocks/latihan/mockExams.js';
-// import { z } from 'zod';
 
 export async function load({ params }) {
     const data: IntExams[] = mockExams;
@@ -17,59 +18,3 @@ export async function load({ params }) {
         },
     };
 }
-
-// const dateScheme = z.coerce
-//     .date({
-//         errorMap: (issue, { defaultError }) => ({
-//             message:
-//                 issue.code === 'invalid_date'
-//                     ? 'Tarikh tidak boleh dibiar kosong.'
-//                     : defaultError,
-//         }),
-//     })
-//     .min(new Date(), {
-//         message: 'Tarikh lepas tidak boleh kurang dari tarikh semasa.',
-//     });
-
-// // z validation schema for the exam form fields=========================================================
-// const examFormSchema = z.object({
-//     examType: z.enum(['perkhidmatan', 'psl'], {
-//         errorMap: (issue, { defaultError }) => ({
-//             message:
-//                 issue.code === 'invalid_enum_value'
-//                     ? 'Jenis latihan perlu dipilih.'
-//                     : defaultError,
-//         }),
-//     }),
-//     examTitle: z
-//         .string({ required_error: 'Tajuk latihan tidak boleh kosong.' })
-//         .min(4, { message: 'Tajuk hendaklah lebih daripada 4 karakter.' })
-//         .max(84, { message: 'Tajuk tidak boleh melebihi 84 karakter.' })
-//         .trim(),
-//     applOpenDate: dateScheme,
-//     applCloseDate: dateScheme,
-//     examDate: dateScheme,
-//     examLocation: z
-//         .string({ required_error: 'Lokasi tidak boleh kosong.' })
-//         .min(4, { message: 'Lokasi hendaklah lebih daripada 4 karakter.' })
-//         .max(124, { message: 'Lokasi tidak boleh melebihi 124 karakter.' })
-//         .trim(),
-// });
-
-// export const actions = {
-//     default: async ({ request }) => {
-//         const formData = Object.fromEntries(await request.formData());
-
-//         try {
-//             const result = examFormSchema.parse(formData);
-//             console.log('SUCCESS', result);
-//         } catch (err) {
-//             const { fieldErrors: errors } = (err as Error).flatten();
-//             const { ...rest } = formData;
-//             return {
-//                 data: rest,
-//                 errors,
-//             };
-//         }
-//     },
-// };
