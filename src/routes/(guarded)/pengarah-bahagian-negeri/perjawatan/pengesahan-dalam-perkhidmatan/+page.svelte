605a63218cc0530a856d679bae81e7605fd5b96d
Holifield William <holifield.william@impactbs.my>
2024-01-02 13:56:16 +0800

    feat: changed table for PDP

--- src/routes/(guarded)/pengarah-bahagian-negeri/perjawatan/pengesahan-dalam-perkhidmatan/+page.svelte
+++ src/routes/(guarded)/pengarah-bahagian-negeri/perjawatan/pengesahan-dalam-perkhidmatan/+page.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
+    import { goto } from '$app/navigation';
+    import SvgCheck from '$lib/assets/svg/SvgCheck.svelte';
+    import SvgEdit from '$lib/assets/svg/SvgEdit.svelte';
+    import FormButton from '$lib/components/buttons/FormButton.svelte';
+    import IconButton from '$lib/components/buttons/IconButton.svelte';
+    import ComplexTable from '$lib/components/complex-table/ComplexTable.svelte';
+    import CxTableBody from '$lib/components/complex-table/CxTableBody.svelte';
+    import CxTableDataCell from '$lib/components/complex-table/CxTableDataCell.svelte';
+    import CxTableHead from '$lib/components/complex-table/CxTableHead.svelte';
+    import CxTableHeadCell from '$lib/components/complex-table/CxTableHeadCell.svelte';
+    import CxTableRow from '$lib/components/complex-table/CxTableRow.svelte';
     import ContentHeader from '$lib/components/content-header/ContentHeader.svelte';
-    import CustomTab from '$lib/components/tab/CustomTab.svelte';
-    import CustomTabContent from '$lib/components/tab/CustomTabContent.svelte';
-    import SectionHeader from '$lib/components/header/SectionHeader.svelte';
-    import DynamicTable from '$lib/components/table/DynamicTable.svelte';
     import FilterContainer from '$lib/components/filter-container/FilterContainer.svelte';
+    import SectionHeader from '$lib/components/header/SectionHeader.svelte';
     import DropdownSelect from '$lib/components/input/DropdownSelect.svelte';
     import ShortTextField from '$lib/components/input/ShortTextField.svelte';
-    import { goto } from '$app/navigation';
-    import { status } from '$lib/mocks/status/status';
     import StaffSelector from '$lib/components/staff-selector/StaffSelector.svelte';
-    import { positions } from '$lib/mocks/positions/positions';
+    import CustomTab from '$lib/components/tab/CustomTab.svelte';
+    import CustomTabContent from '$lib/components/tab/CustomTabContent.svelte';
+    import DynamicTable from '$lib/components/table/DynamicTable.svelte';
     import { lantikanBaru } from '$lib/mocks/pengarah-bahagian-negeri/pengesahan/lantikan-baru';
     import { lantikanKeGred } from '$lib/mocks/pengarah-bahagian-negeri/pengesahan/lantikan-ke-gred';
-    import FormButton from '$lib/components/buttons/FormButton.svelte';
+    import { positions } from '$lib/mocks/positions/positions';
+    import { status } from '$lib/mocks/status/status';
 
     let selectedStatus = status[0].value;
     let selectedPos: string = positions[0].value;
@@ -55,7 +64,7 @@
                 ><FormButton type="perakukan"></FormButton></SectionHeader
             >
 
-            <SectionHeader title="Senarai Kakitangan"></SectionHeader>
+            <SectionHeader title="Senarai Kakitangan" subTitle={'Jumlah Rekod: '+lantikanBaru.length}></SectionHeader>
             <div
                 class="flex max-h-full w-full flex-col items-start justify-start"
             >
@@ -98,23 +107,85 @@
                 title="Tindakan: Perakukan semua kakitangan berkaitan"
                 ><FormButton type="perakukan"></FormButton></SectionHeader
             >
-            <SectionHeader title="Senarai Kakitangan"></SectionHeader>
-            <div
-                class="flex max-h-full w-full flex-col items-start justify-start"
-            >
-                <DynamicTable
-                    hasCheckbox
-                    tableItems={lantikanKeGred}
-                    withActions
-                    actionOptions={['detail']}
-                    detailActions={() => {
-                        goto(
-                            '/pengarah-bahagian-negeri/perjawatan/pengesahan-dalam-perkhidmatan/pengesahan-lantikan-gred',
-                        );
-                    }}
-                    bind:passData
-                ></DynamicTable>
-            </div>
+            <SectionHeader title="Senarai Kakitangan" subTitle={'Jumlah Rekod: '+lantikanKeGred.length}></SectionHeader>
+            <ComplexTable title="">
+                <CxTableHead>
+                    <CxTableRow headerColor>
+                        <CxTableHeadCell rowSize={2} title="Bil." />
+                        <CxTableHeadCell rowSize={2} title="No. Pekerja" />
+                        <CxTableHeadCell rowSize={2} title="Nama Pekerja" />
+                        <CxTableHeadCell
+                            rowSize={2}
+                            title="No. Kad Pengenalan"
+                        />
+                        <CxTableHeadCell
+                            rowSize={2}
+                            title="Jawatan Pertama Dilantik Di Lembaga"
+                        />
+                        <CxTableHeadCell rowSize={2} title="Tarikh Lantikan" />
+                        <CxTableHeadCell rowSize={2} title="Jawatan Semasa" />
+                        <CxTableHeadCell
+                            rowSize={2}
+                            title="Penempatan Semasa"
+                        />
+                        <CxTableHeadCell
+                            columnSize={4}
+                            title="Peperiksaan Perkhidmatan/Kursus Induksi"
+                        />
+                        <CxTableHeadCell rowSize={2} title="Status" />
+                        <CxTableHeadCell rowSize={2} title="" />
+                    </CxTableRow>
+                    <CxTableRow headerColor>
+                        <CxTableHeadCell title="Program Transformasi Minda" />
+                        <CxTableHeadCell
+                            columnSize={2}
+                            title="Peperiksaan JPA Bahagian A/B"
+                        />
+                        <CxTableHeadCell
+                            title="Peperiksaan Jabatan Bahagian IV"
+                        />
+                    </CxTableRow>
+                </CxTableHead>
+                <CxTableBody>
+                    {#each lantikanKeGred as item, i (i)}
+                        <CxTableRow>
+                            <CxTableDataCell>{i + 1}</CxTableDataCell>
+                            <CxTableDataCell>{item.staffId}</CxTableDataCell>
+                            <CxTableDataCell
+                                >{item.employeeName}</CxTableDataCell
+                            >
+                            <CxTableDataCell>{item.idNumber}</CxTableDataCell>
+                            <CxTableDataCell
+                                >{item.firstAppointedPositionOnBoard}</CxTableDataCell
+                            >
+                            <CxTableDataCell
+                                >{item.appointedDate}</CxTableDataCell
+                            >
+                            <CxTableDataCell
+                                >{item.currentPosition}</CxTableDataCell
+                            >
+                            <CxTableDataCell
+                                >{item.currentPlacement}</CxTableDataCell
+                            >
+                            <CxTableDataCell><SvgCheck /></CxTableDataCell>
+                            <CxTableDataCell>{''}</CxTableDataCell>
+                            <CxTableDataCell><SvgCheck /></CxTableDataCell>
+                            <CxTableDataCell><SvgCheck /></CxTableDataCell>
+                            <CxTableDataCell>{item.status}</CxTableDataCell>
+                            <CxTableDataCell>
+                                <IconButton
+                                    onClick={() =>
+                                        goto(
+                                            '/pengarah-bahagian-negeri/perjawatan/pengesahan-dalam-perkhidmatan/pengesahan-lantikan-gred',
+                                        )}
+                                >
+                                    <SvgEdit />
+                                </IconButton>
+                            </CxTableDataCell>
+                        </CxTableRow>
+                    {/each}
+                </CxTableBody>
+            </ComplexTable>
         </CustomTabContent>
     </CustomTab>
 </section>
