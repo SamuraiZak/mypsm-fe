952c045ea5f7d3ce289d14ab6326eaf4e0091236
nurazmina <nurazmina@impact-multimedia.com>
2024-01-04 17:19:25 +0800

    feat: WIP added validation for Urus Setia Persaraan Paksaan

--- src/routes/(guarded)/urus-setia/perjawatan/persaraan/butiran-persaraan-paksaan/+page.svelte
+++ src/routes/(guarded)/urus-setia/perjawatan/persaraan/butiran-persaraan-paksaan/+page.svelte
@@ -14,18 +14,22 @@
     import StepperContent from '$lib/components/stepper/StepperContent.svelte';
     import Stepper from '$lib/components/stepper/Stepper.svelte';
     import TextIconButton from '$lib/components/buttons/TextIconButton.svelte';
-    import SvgPaperAirplane from '$lib/assets/svg/SvgPaperAirplane.svelte';
     import SectionHeader from '$lib/components/header/SectionHeader.svelte';
     import DropdownSelect from '$lib/components/input/DropdownSelect.svelte';
     import { tahun } from '$lib/mocks/ketua-seksyen/cuti/tahun';
     import { status } from '$lib/mocks/urus-setia/persaraan/status';
     import { mockEmployeeDocumentLists } from '$lib/mocks/database/mockEmployeeDocumentLists';
+    import toast, { Toaster } from 'svelte-french-toast';
+    import { z, ZodError } from 'zod';
 
     export let disabled: boolean = true;
 
-    let radioValue: any = 'sah';
     let selectedTahun = tahun[0].value;
     let selectedStatus = status[0].value;
+    let target: any;
+    let errorData: any;
+    let ulasan: string;
+    let keputusan: any;
 
     const options: RadioOption[] = [
         {
@@ -39,6 +43,66 @@
     ];
 
     const currentEmployeeUploadedDocuments = mockEmployeeDocumentLists;
+
+    const submitForm = async (event: Event) => {
+        const formData = new FormData(event.target as HTMLFormElement);
+
+        const exampleFormData = {
+            keputusan: String(formData.get('keputusan')),
+            ulasan: String(formData.get('ulasan')),
+        };
+
+        const exampleFormSchema = z.object({
+            // checkbox schema
+            keputusan: z.enum(['sah', 'tidakSah'], {
+                errorMap: (issue, { defaultError }) => ({
+                    message:
+                        issue.code === 'invalid_enum_value'
+                            ? 'Sila tetapkan pilihan anda.'
+                            : defaultError,
+                }),
+            }),
+            // dateSelectorExample: dateScheme,
+            ulasan: z
+                .string({ required_error: 'Medan ini tidak boleh kosong.' })
+                .min(4, {
+                    message: 'Medan ini hendaklah lebih daripada 4 karakter.',
+                })
+                .max(124, {
+                    message: 'Medan ini tidak boleh melebihi 124 karakter.',
+                })
+                .trim(),
+        });
+
+        try {
+            const result = exampleFormSchema.parse(exampleFormData);
+            if (result) {
+                errorData = [];
+                toast.success('Berjaya disimpan!', {
+                    style: 'background: #333; color: #fff;',
+                });
+
+                const id = crypto.randomUUID().toString();
+                const validatedExamFormData = { ...exampleFormData, id };
+                console.log(
+                    'REQUEST BODY: ',
+                    JSON.stringify(validatedExamFormData),
+                );
+            }
+        } catch (err: unknown) {
+            if (err instanceof ZodError) {
+                const { fieldErrors: errors } = err.flatten();
+                errorData = errors;
+                console.log('ERROR!', err.flatten());
+                toast.error(
+                    'Sila pastikan maklumat adalah lengkap dengan tepat.',
+                    {
+                        style: 'background: #333; color: #fff;',
+                    },
+                );
+            }
+        }
+    };
 </script>
 
 <section class="flex w-full flex-col items-start justify-start">
@@ -123,74 +187,128 @@
     </StepperContent>
     <StepperContent>
         <StepperContentHeader title="Pengesahan Permohonan Persaraan"
-        ></StepperContentHeader>
+            ><TextIconButton
+                primary
+                label="Hantar"
+                form="validateApplicationFormValidation"
+            /></StepperContentHeader
+        >
         <StepperContentBody
-            ><div
-                class="flex w-full flex-col gap-2 border-b border-bdr-primary pb-5"
+            ><form
+                id="validateApplicationFormValidation"
+                on:submit|preventDefault={submitForm}
+                class="flex w-full flex-col gap-2"
             >
-                <div>
-                    <LongTextField
-                        id="tindakanUlasan"
-                        label={'Tindakan/ Ulasan'}
-                        value={'Memenuhi Kriteria'}
-                    ></LongTextField>
+                <div
+                    class="flex w-full flex-col gap-2 border-b border-bdr-primary pb-5"
+                >
+                    <div>
+                        <LongTextField
+                            hasError={errorData?.ulasan}
+                            name="ulasan"
+                            label="Ulasan/Tindakan"
+                            bind:value={ulasan}
+                        />
+                        {#if errorData?.ulasan}
+                            <span
+                                class="ml-[220px] font-sans text-sm italic text-system-danger"
+                                >{errorData?.ulasan[0]}</span
+                            >
+                        {/if}
 
-                    <RadioSingle
-                        {options}
-                        legend=""
-                        bind:userSelected={radioValue}
-                    />
+                        <RadioSingle
+                            disabled={false}
+                            {options}
+                            name="keputusan"
+                            legend={''}
+                            bind:userSelected={keputusan}
+                        ></RadioSingle>
+                        {#if errorData?.keputusan}
+                            <span
+                                class="ml-[220px] font-sans text-sm italic text-system-danger"
+                                >{errorData?.keputusan[0]}</span
+                            >
+                        {/if}
+                    </div>
                 </div>
-            </div></StepperContentBody
+            </form></StepperContentBody
         >
     </StepperContent>
     <StepperContent>
         <StepperContentHeader title="Pengesahan Dokumen Persaraan"
-        ></StepperContentHeader>
+            ><TextIconButton
+                primary
+                label="Hantar"
+                form="validateDocumentFormValidation"
+            /></StepperContentHeader
+        >
         <StepperContentBody
-            ><div
-                class="flex w-full flex-col gap-2 border-b border-bdr-primary pb-5"
+            ><form
+                id="validateDocumentFormValidation"
+                on:submit|preventDefault={submitForm}
+                class="flex w-full flex-col gap-2"
             >
-                <p class="text-sm font-bold">Dokumen Persaraan Kakitangan</p>
-                <p class="text-sm">Fail-fail yang dimuat naik:</p>
-                <ul
-                    class="flex w-full list-decimal flex-col gap-2 pl-4 text-sm"
+                <div
+                    class="flex w-full flex-col gap-2 border-b border-bdr-primary pb-5"
                 >
-                    <li>
-                        <DownloadAttachment
-                            fileName="JPA.BP.SPPP.B01a-Maklumat Pesara.pdf"
-                        />
-                    </li>
-                    <li>
-                        <DownloadAttachment
-                            fileName="JPA.BP.SPPP.B01a-Maklumat Pesara.pdf"
-                        />
-                    </li>
-                    <li>
-                        <DownloadAttachment
-                            fileName="JPA.BP.SPPP.B01a-Maklumat Pesara.pdf"
-                        />
-                    </li>
-                </ul>
-            </div>
-            <div
-                class="flex w-full flex-col gap-2 border-b border-bdr-primary pb-5"
-            >
-                <p class="text-sm font-bold">Pengesahan Urus Setia</p>
-                <div>
-                    <LongTextField
-                        id="tindakanUlasan"
-                        label={'Tindakan/ Ulasan'}
-                        value={'Dokumen-dokumen telah disemak'}
-                    ></LongTextField>
-
-                    <RadioSingle
-                        {options}
-                        legend=""
-                        bind:userSelected={radioValue}
-                    />
+                    <p class="text-sm font-bold">
+                        Dokumen Persaraan Kakitangan
+                    </p>
+                    <p class="text-sm">Fail-fail yang dimuat naik:</p>
+                    <ul
+                        class="flex w-full list-decimal flex-col gap-2 pl-4 text-sm"
+                    >
+                        <li>
+                            <DownloadAttachment
+                                fileName="JPA.BP.SPPP.B01a-Maklumat Pesara.pdf"
+                            />
+                        </li>
+                        <li>
+                            <DownloadAttachment
+                                fileName="JPA.BP.SPPP.B01a-Maklumat Pesara.pdf"
+                            />
+                        </li>
+                        <li>
+                            <DownloadAttachment
+                                fileName="JPA.BP.SPPP.B01a-Maklumat Pesara.pdf"
+                            />
+                        </li>
+                    </ul>
                 </div>
-            </div></StepperContentBody
+                <div
+                    class="flex w-full flex-col gap-2 border-b border-bdr-primary pb-5"
+                >
+                    <p class="text-sm font-bold">Pengesahan Urus Setia</p>
+                    <div>
+                        <LongTextField
+                            hasError={errorData?.ulasan}
+                            name="ulasan"
+                            label="Ulasan/Tindakan"
+                            bind:value={ulasan}
+                        />
+                        {#if errorData?.ulasan}
+                            <span
+                                class="ml-[220px] font-sans text-sm italic text-system-danger"
+                                >{errorData?.ulasan[0]}</span
+                            >
+                        {/if}
+
+                        <RadioSingle
+                            disabled={false}
+                            {options}
+                            name="keputusan"
+                            legend={''}
+                            bind:userSelected={keputusan}
+                        ></RadioSingle>
+                        {#if errorData?.keputusan}
+                            <span
+                                class="ml-[220px] font-sans text-sm italic text-system-danger"
+                                >{errorData?.keputusan[0]}</span
+                            >
+                        {/if}
+                    </div>
+                </div>
+            </form></StepperContentBody
         >
     </StepperContent>
     <StepperContent>
@@ -198,44 +316,64 @@
             ><TextIconButton
                 primary
                 label="Hantar"
-                onClick={() => {
-                    goto('/urus-setia/perjawatan/persaraan');
-                }}><SvgPaperAirplane /></TextIconButton
-            ></StepperContentHeader
+                form="updateApplicationFormValidation"
+            /></StepperContentHeader
         >
         <StepperContentBody
-            ><div
-                class="flex w-full flex-col gap-2 border-b border-bdr-primary pb-5"
+            ><form
+                id="updateApplicationFormValidation"
+                on:submit|preventDefault={submitForm}
+                class="flex w-full flex-col gap-2"
             >
-                <p class="text-sm font-bold">Cetak Surat Iringan</p>
-                <ul
-                    class="flex w-full list-decimal flex-col gap-2 pl-4 text-sm"
+                <div
+                    class="flex w-full flex-col gap-2 border-b border-bdr-primary pb-5"
                 >
-                    <li>
-                        <DownloadAttachment fileName="Surat Iringan" />
-                    </li>
-                </ul>
-            </div>
-            <div
-                class="flex w-full flex-col gap-2 border-b border-bdr-primary pb-5"
-            >
-                <p class="text-sm font-bold">
-                    Maklumat Penghantaran Permohonan
-                </p>
-                <div>
-                    <LongTextField
-                        id="tindakanUlasan"
-                        label={'Tindakan/ Ulasan'}
-                        value={'Dokumen-dokumen telah disemak'}
-                    ></LongTextField>
-
-                    <RadioSingle
-                        {options}
-                        legend=""
-                        bind:userSelected={radioValue}
-                    />
+                    <p class="text-sm font-bold">Cetak Surat Iringan</p>
+                    <ul
+                        class="flex w-full list-decimal flex-col gap-2 pl-4 text-sm"
+                    >
+                        <li>
+                            <DownloadAttachment fileName="Surat Iringan" />
+                        </li>
+                    </ul>
                 </div>
-            </div></StepperContentBody
+                <div
+                    class="flex w-full flex-col gap-2 border-b border-bdr-primary pb-5"
+                >
+                    <p class="text-sm font-bold">
+                        Maklumat Penghantaran Permohonan
+                    </p>
+                    <div>
+                        <LongTextField
+                            hasError={errorData?.ulasan}
+                            name="ulasan"
+                            label="Ulasan/Tindakan"
+                            bind:value={ulasan}
+                        />
+                        {#if errorData?.ulasan}
+                            <span
+                                class="ml-[220px] font-sans text-sm italic text-system-danger"
+                                >{errorData?.ulasan[0]}</span
+                            >
+                        {/if}
+
+                        <RadioSingle
+                            disabled={false}
+                            {options}
+                            name="keputusan"
+                            legend={''}
+                            bind:userSelected={keputusan}
+                        ></RadioSingle>
+                        {#if errorData?.keputusan}
+                            <span
+                                class="ml-[220px] font-sans text-sm italic text-system-danger"
+                                >{errorData?.keputusan[0]}</span
+                            >
+                        {/if}
+                    </div>
+                </div>
+            </form></StepperContentBody
         >
     </StepperContent>
 </Stepper>
+<Toaster/>
