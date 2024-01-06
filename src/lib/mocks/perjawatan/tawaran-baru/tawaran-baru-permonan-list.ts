f8431336de9e222c1ce0a471ef537a3a4e8a2088
calvin <calvin.cliff@impact-multimedia.com>
2024-01-06 23:40:11 +0800

    test: uses consistent value

--- src/lib/mocks/perjawatan/tawaran-baru/tawaran-baru-permonan-list.ts
+++ src/lib/mocks/perjawatan/tawaran-baru/tawaran-baru-permonan-list.ts
@@ -1,26 +1,28 @@
+import { mockEmployees } from "$lib/mocks/database/mockEmployees";
+
 export const staffs = [
     {
-        noPekerja: '2321',
-        namaKakitangan: 'Aina Binti Kasim',
-        noKadPengenalan: '770521-12-1234',
+        noPekerja: mockEmployees[0].employeeNumber,
+        namaPekerja: mockEmployees[0].name,
+        noKadPengenalan: mockEmployees[0].identityDocumentNumber,
         gred: 'E22',
         jawatan: 'Akauntan',
         tarafPendidikan: 'Ijazah Sarjana Muda',
         kelayakanCuti: 30,
     },
     {
-        noPekerja: '2033',
-        namaKakitangan: 'Arif Bin Farhan',
-        noKadPengenalan: '750521-12-1234',
+        noPekerja: mockEmployees[1].employeeNumber,
+        namaPekerja: mockEmployees[1].name,
+        noKadPengenalan: mockEmployees[1].identityDocumentNumber,
         gred: 'E22',
         jawatan: 'Penolong Akauntan',
         tarafPendidikan: 'Ijazah Sarjana Muda',
         kelayakanCuti: 30,
     },
     {
-        noPekerja: '4522',
-        namaKakitangan: 'Basim Kushna',
-        noKadPengenalan: '880521-12-1234',
+        noPekerja: mockEmployees[2].employeeNumber,
+        namaPekerja: mockEmployees[2].name,
+        noKadPengenalan: mockEmployees[2].identityDocumentNumber,
         gred: 'E29',
         jawatan: 'Penolong Akauntan',
         tarafPendidikan: 'Ijazah Sarjana Muda',
