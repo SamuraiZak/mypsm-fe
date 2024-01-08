f2e441ba90b495da4d6efa972201fca69a7ca2ea
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-01-08 17:32:40 +0800

    feat: makes the 2-table a page into tabs

--- src/routes/(guarded)/urus-setia/perjawatan/lantikan-baru/+page.svelte
+++ src/routes/(guarded)/urus-setia/perjawatan/lantikan-baru/+page.svelte
@@ -2,24 +2,17 @@
     // Importing external components and libraries
     import ContentHeader from '$lib/components/content-header/ContentHeader.svelte';
     import FormButton from '$lib/components/buttons/FormButton.svelte';
-    import { status } from '$lib/mocks/status/status';
     import { goto } from '$app/navigation';
-    import SectionHeader from '$lib/components/header/SectionHeader.svelte';
-    import DynamicTable from '$lib/components/table/DynamicTable.svelte';
-    import FilterCard from '$lib/components/filter/FilterCard.svelte';
-    import FilterTextInput from '$lib/components/filter/FilterTextInput.svelte';
-    import FilterSelectInput from '$lib/components/filter/FilterSelectInput.svelte';
-    import FilterDateSelector from '$lib/components/filter/FilterDateSelector.svelte';
+    import SenaraiBelumDiisi from './tab-contents/SenaraiBelumDiisi.svelte';
+    import SenaraiTelahDiisi from './tab-contents/SenaraiTelahDiisi.svelte';
+    import Tabs from '$lib/components/tabs/Tabs.svelte';
+    import TabContent from '$lib/components/tabs/TabContent.svelte';
 
     export let data;
-    let selectedStatus = status[0].value; // Default selected filter
 
     // mock data
     const newStaffUrl = data.props.candidateLists;
     const lantikanBaru = data.props.newHireLists;
-
-    let tempUrl: Candidate;
-    let tempStaff: NewHire;
 
     const base64String = `JVBERi0xLjcKCjEgMCBvYmogICUgZW50cnkgcG9pbnQKPDwKICAvVHlwZSAvQ2F0YWxvZwog
                             IC9QYWdlcyAyIDAgUgo+PgplbmRvYmoKCjIgMCBvYmoKPDwKICAvVHlwZSAvUGFnZXMKICAv
@@ -98,52 +91,14 @@
 <!-- content body starts here -->
 <!-- notes: you may change the flex directions but do not change the height and max height of this section -->
 <section
-    class="max-h-[calc(100vh - 172px)] flex h-full w-full flex-col justify-start overflow-y-auto bg-bgr-primary p-3"
+    class="flex h-full w-full flex-col items-center justify-start overflow-y-auto"
 >
-    <!-- Table filter placeholder -->
-    <FilterCard>
-        <FilterTextInput label="Nama Calon"></FilterTextInput>
-        <FilterTextInput label="ID Calon"></FilterTextInput>
-        <FilterTextInput label="No. Kad Pengenalan"></FilterTextInput>
-        <FilterDateSelector handleDateChange label="Tarikh Mohon"
-        ></FilterDateSelector>
-        <FilterDateSelector handleDateChange label="Tarikh Lantikan"
-        ></FilterDateSelector>
-        <FilterSelectInput
-            label="Status"
-            options={status}
-            selectedVal={selectedStatus}
-        ></FilterSelectInput>
-    </FilterCard>
-
-    <!-- Sample table for testing purposes -->
-    <div class="flex w-full flex-col items-start justify-center">
-        <SectionHeader title="Senarai Rekod Pautan Belum Diisi"></SectionHeader>
-        <DynamicTable
-            withActions
-            actionOptions={['detail']}
-            detailActions={() => {
-                const url = './lantikan-baru/permohonan-baru';
-                goto(url);
-            }}
-            tableItems={newStaffUrl}
-            bind:passData={tempUrl}
-        ></DynamicTable>
-        <br />
-        <SectionHeader title="Senarai Rekod Selesai Diisi"></SectionHeader>
-        <DynamicTable
-            tableItems={lantikanBaru}
-            bind:passData={tempStaff}
-            withActions
-            actionOptions={['edit']}
-            editActions={() => {
-                const url =
-                    './lantikan-baru/kemaskini-permohonan-' +
-                    tempStaff.idSementara +
-                    '-' +
-                    tempStaff.noKadPengenalan;
-                goto(url);
-            }}
-        ></DynamicTable>
-    </div>
+    <Tabs>
+        <TabContent title="Senarai Rekod Selesai Diisi">
+            <SenaraiTelahDiisi listData={lantikanBaru} />
+        </TabContent>
+        <TabContent title="Senarai Rekod Penambahan Calon Lantikan Baru">
+            <SenaraiBelumDiisi listData={newStaffUrl} />
+        </TabContent>
+    </Tabs>
 </section>
