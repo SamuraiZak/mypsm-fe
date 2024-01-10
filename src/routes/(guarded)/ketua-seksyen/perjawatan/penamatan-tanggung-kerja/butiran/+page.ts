95ddb554ac0abac5ba804e2ad5fcbebeef4ff5ea
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-01-10 16:43:51 +0800

    test: usiung json placeholder to test api request and response.

--- src/routes/(guarded)/ketua-seksyen/perjawatan/penamatan-tanggung-kerja/butiran/+page.ts
+++ src/routes/(guarded)/ketua-seksyen/perjawatan/penamatan-tanggung-kerja/butiran/+page.ts
@@ -1,14 +1,14 @@
 // import { goto } from '$app/navigation';
 import api from '$lib/services/core/ky.service.js';
 import { error, fail } from '@sveltejs/kit';
+import toast from 'svelte-french-toast';
 // import toast from 'svelte-french-toast';
 import { superValidate } from 'sveltekit-superforms/client';
 import { z } from 'zod';
-// import type { RequestEvent } from '../../../../$types.js';
 // import { ZodError, z } from 'zod';
 
 // =========================================================================
-// z validation schema and submit function for the new employment form fields
+// z validation schema and submit function for the interim end form fields
 // =========================================================================
 const longTextSchema = z
     .string({ required_error: 'Medan ini tidak boleh kosong.' })
@@ -20,7 +20,7 @@ const longTextSchema = z
     })
     .trim();
 
-// New Employment - Approver Result section
+// Interim - Approver Result section
 export const _approverResultSchema = z.object({
     approverRemark: longTextSchema,
     approverResult: z.enum(['true', 'false'], {
@@ -51,56 +51,25 @@ export const _submitApproverResultForm = async (event: Event) => {
     const form = await superValidate(formData, _approverResultSchema);
 
     if (!form.valid) {
+        toast.error('Sila pastikan maklumat adalah lengkap dengan tepat.', {
+            style: 'background: #333; color: #fff;',
+        });
         return fail(400, form);
     } else {
-        console.log('Request Body: ', formData);
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            body: JSON.stringify(form),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        })
-            .then((response) => response.json())
-            .then((json) => console.log('Response Returned: ', json));
+        console.log('Request: ', form.data);
+
+        await api
+            .post('https://jsonplaceholder.typicode.com/posts', {
+                body: JSON.stringify(form),
+                prefixUrl: '',
+            })
+            .json()
+            .then((json) => console.log('Response: ', json));
+
+        toast.success('Berjaya disimpan!', {
+            style: 'background: #333; color: #fff;',
+        });
     }
 
     return { form };
 };
-
-// export let _errorData: any;
-
-// export const _submitApproverResultForm = async (event: Event) => {
-//     const formElement = event.target as HTMLFormElement;
-//     const formData = new FormData(formElement);
-
-//     const approverResultData = {
-//         approverRemark: String(formData.get('approverRemark')),
-//         approverResult: String(formData.get('approverResult')),
-//     };
-
-//     try {
-//         // _errorData = [];
-//         const result = _approverResultSchema.parse(approverResultData);
-
-//         if (result) {
-//             // _errorData = [];
-//             toast.success('Berjaya disimpan!', {
-//                 style: 'background: #333; color: #fff;',
-//             });
-//             setTimeout(() => goto('../penamatan-tanggung-kerja'), 1500);
-//             return result;
-//         }
-//         return result;
-//     } catch (error: unknown) {
-//         if (error instanceof ZodError) {
-//             const { fieldErrors: errors } = error.flatten();
-//             _errorData = errors;
-//             toast.error('Sila pastikan maklumat adalah lengkap dengan tepat.', {
-//                 style: 'background: #333; color: #fff;',
-//             });
-//             console.log('PAGE.TS', _errorData);
-//             // return errors;
-//         }
-//     }
-// };
