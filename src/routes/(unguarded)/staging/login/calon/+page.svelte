f00af106576ded4cdf48b1d7b7eea4fe85af776c
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-01-10 08:52:58 +0800

    feat: WIP new login page with superforms

---
+++ src/routes/(unguarded)/staging/login/calon/+page.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+    import mypsmLogo from '$lib/assets/MyPSM.png';
+    import lkimLogo from '$lib/assets/logo.png';
+</script>
+
+<div
+    class="w-full rounded-lg bg-ios-backgroundColors-systemBackground-light shadow sm:max-w-md md:mt-0 xl:p-0"
+>
+    <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
+        <div class="flex w-full items-center justify-center">
+            <img class="h-10" src={mypsmLogo} alt="logo" />
+        </div>
+        <div class="inline-flex w-full items-center justify-center">
+            <hr
+                class="h-px w-full border-0 bg-ios-labelColors-separator-light"
+            />
+            <span
+                class="text-md absolute left-1/2 -translate-x-1/2 bg-bgr-primary px-3 font-semibold text-ios-labelColors-label-light"
+                >Log Masuk</span
+            >
+        </div>
+        <p
+            class="text-center text-base text-ios-labelColors-secondaryLabel-light"
+        >
+            Calon Kakitangan LKIM
+        </p>
+
+        <!-- login form starts -->
+
+        <form class="space-y-4 md:space-y-6" action="#">
+            <!-- user name field starts -->
+
+            <div>
+                <label
+                    for="username"
+                    class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                    >No. Kad Pengenalan</label
+                >
+                <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="(Contoh: 850201115670)"
+                    class=" block h-9 w-full rounded-md border border-ios-labelColors-separator-light bg-ios-systemColors-quaternarySystemFill-light p-2.5 text-base autofill:hide-default-inner-shadow focus:border-ios-activeColors-activeBlue-light focus:ring-1 focus:ring-ios-activeColors-activeBlue-light"
+                />
+            </div>
+
+            <!-- username field ends -->
+
+            <!-- password field starts -->
+
+            <div>
+                <label
+                    for="password"
+                    class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                    >Kata Laluan</label
+                >
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    class=" block h-9 w-full rounded-md border border-ios-labelColors-separator-light bg-ios-systemColors-quaternarySystemFill-light p-2.5 text-base focus:border-ios-activeColors-activeBlue-light focus:ring-1 focus:ring-ios-activeColors-activeBlue-light"
+                />
+            </div>
+
+            <!-- password field ends -->
+
+            <!-- remeber me & forgot password starts -->
+
+            <div class="flex items-center justify-between">
+                <div
+                    class="flex flex-row items-start justify-center align-middle"
+                >
+                    <div class="flex h-5 items-center">
+                        <input
+                            id="remember"
+                            aria-describedby="remember"
+                            type="checkbox"
+                            class="focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 h-4 w-4 rounded border border-gray-300 bg-gray-50 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800"
+                        />
+                    </div>
+                    <div class="ml-3 text-sm">
+                        <label
+                            for="remember"
+                            class="leading-loose text-gray-500 dark:text-gray-300"
+                            >Ingat Akaun Saya</label
+                        >
+                    </div>
+                </div>
+                <a
+                    href="#"
+                    class=" text-sm font-medium text-ios-labelColors-link-light hover:underline"
+                    >Terlupa Kata Laluan</a
+                >
+            </div>
+
+            <!-- remeber me & forgot password ends -->
+
+            <!-- submit button starts -->
+
+            <button
+                type="submit"
+                class=" hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-md bg-ios-activeColors-activeBlue-light px-5 py-2.5 text-center text-base font-medium text-white focus:outline-none focus:ring-4"
+                >Log Masuk</button
+            >
+
+            <!-- submit button ends -->
+        </form>
+
+        <!-- login form ends -->
+    </div>
+</div>
---
+++ src/routes/(unguarded)/staging/login/klinik-panel=[id]/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import { superForm, setMessage, setError } from 'sveltekit-superforms/client';
+    import { _userSchema } from './+page';
+  
+    export let data: PageData;
+  
+    const { form, errors, message, constraints, enhance } = superForm(
+      data.form,
+      {
+        SPA: true,
+        validators: _userSchema,
+        onUpdate({ form }) {
+          // Form validation
+          if (form.data.email.includes('spam')) {
+            setError(form, 'email', 'Suspicious email address.');
+          } else if (form.valid) {
+            // TODO: Do something with the validated form.data
+            setMessage(form, 'Valid data!');
+          }
+        }
+      }
+    );
+  </script>
+  
+  <h1>Edit user</h1>
+  
+  {#if $message}<h3>{$message}</h3>{/if}
+  
+  <form method="POST" use:enhance>
+    <label>
+      Name<br />
+      <input
+        aria-invalid={$errors.name ? 'true' : undefined}
+        bind:value={$form.name}
+        {...$constraints.name} />
+    </label>
+    {#if $errors.name}<span class="invalid">{$errors.name}</span>{/if}
+  
+    <label>
+      E-mail<br />
+      <input
+        type="email"
+        aria-invalid={$errors.email ? 'true' : undefined}
+        bind:value={$form.email}
+        {...$constraints.email} />
+    </label>
+    {#if $errors.email}<span class="invalid">{$errors.email}</span>{/if}
+  
+    <button>Submit</button>
+  </form>
