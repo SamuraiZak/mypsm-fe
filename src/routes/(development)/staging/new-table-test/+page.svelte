73fb96ce4371167a523bf32735efa3fd6d6d9862
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-01-11 09:46:39 +0800

    feat: WIP new table

---
+++ src/routes/(development)/staging/new-table-test/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import IbsCommonTable from '$lib/components/staging/ibs-common-table/IBSCommonTable.svelte';
+</script>
+
+<div class="h-full w-full p-6">
+    <IbsCommonTable></IbsCommonTable>
+</div>
