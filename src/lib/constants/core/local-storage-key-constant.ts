17051f7cc682c890bb9a3a4eccd581fe4412a67b
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-01-11 13:26:11 +0800

    feat: Added local storage key constant

---
+++ src/lib/constants/core/local-storage-key-constant.ts
@@ -0,0 +1,11 @@
+// ===============================================================
+// Local Storage Key Constants
+// ===============================================================
+
+export class LocalStorageKeyConstant {
+    public static accessToken = "accessToken";
+    public static userID = "userID";
+    public static currentRole = "currentRole";
+    public static userRoles = "userRoles";
+    public static loginResult = "loginResult";
+}
