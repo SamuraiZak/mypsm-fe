a3c2404afd04c313378ca03803d12e8c46725aa9
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-01-14 04:35:04 +0800

    feat: Show loading screen when store value changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+    import { showLoadingOverlay } from '$lib/stores/globalState';
+    import LoadingOverlay from '$lib/components/loading-overlay/LoadingOverlay.svelte';
     import '../app.css';
 
 	let activeUrl: string;
 </script>
+
+{#if $showLoadingOverlay}
+    <LoadingOverlay></LoadingOverlay>
+{/if}
 
 <slot />
