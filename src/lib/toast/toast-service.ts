99937367c8113002f7636525ab16a110a21eaa59
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-01-15 07:56:27 +0800

    feat: adds toast success.

--- src/lib/toast/toast-service.ts
+++ src/lib/toast/toast-service.ts
@@ -1,10 +1,17 @@
-import toast from "svelte-french-toast";
+import toast from 'svelte-french-toast';
+
+export const getSuccessToast = async () => {
+    toast.success('Berjaya disimpan!', {
+        style: 'background: #333; color: #fff;',
+    });
+};
 
 export const getErrorToast = async () => {
     toast.error('Sila pastikan maklumat adalah lengkap dengan tepat.', {
         style: 'background: #333; color: #fff;',
     });
 };
+
 export const getPromiseToast = async (promise: Promise<unknown>) => {
     return toast.promise(
         promise,
