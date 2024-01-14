e1578bdf9cacbb1519313b8ccf54e5680f5d18a0
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-01-14 12:44:07 +0800

    feat: Modified service helper

--- src/lib/services/core/http.service.ts
+++ src/lib/services/core/http.service.ts
@@ -2,11 +2,9 @@
 // HTTP Request Service
 // ===============================================================
 
-import { env } from '$env/dynamic/public';
 import ky from 'ky';
 
 const http = ky.create({
-    prefixUrl: env.PUBLIC_BASE_API_URL,
     headers: {
         Accept: 'application/json',
         'Content-type': 'application/json',
