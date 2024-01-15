7151fc205e759bf82ec9542f85ea7185b1a3e02f
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-01-15 08:04:35 +0800

    feat: loads data from the api.

--- src/routes/(guarded)/urus-setia/gaji/pergerakan-gaji/+layout.ts
+++ src/routes/(guarded)/urus-setia/gaji/pergerakan-gaji/+layout.ts
@@ -1,10 +1,39 @@
 import { mockSalaryMovementRecord } from '$lib/mocks/gaji/salaryMovementRecord/mockSalaryMovementRecord';
+import api from '$lib/services/core/ky.service';
+// import { showLoadingOverlay } from '$lib/stores/globalState';
 
 export async function load() {
+    // showLoadingOverlay.set(true);
+
+    const getGredLookup = {
+        pageNum: 1,
+        pageSize: 10,
+        orderBy: 'createdAt',
+        orderType: 'asc',
+        filter: {},
+    };
+
+    console.log(JSON.stringify(getGredLookup));
+
+    // Gred lookup
+    const gredResponse: Response = await api
+        .post('api/v1/lookups/grades', {
+            body: JSON.stringify(getGredLookup),
+        })
+        .json();
+
+    const gredLists = gredResponse.data.map((element) => ({
+        value: element.code,
+        name: element.code,
+    }));
+
+    
+
     const salaryMovementRecord: IntSalaryMovementRecord[] =
         await mockSalaryMovementRecord;
     return {
         records: {
+            gredLists,
             salaryMovementRecord,
         },
     };
