ea55111c0d47a72d84b44f8630a0a3ca0ac593f6
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-01-15 08:06:46 +0800

    fix: uses toast service.

--- src/routes/(guarded)/urus-setia/perjawatan/lantikan-baru/permohonan-baru/+page.ts
+++ src/routes/(guarded)/urus-setia/perjawatan/lantikan-baru/permohonan-baru/+page.ts
@@ -1,17 +1,25 @@
-import http from '$lib/services/core/http.service.js';
-import { showLoadingOverlay } from '$lib/stores/globalState';
-import {
-    CommonViewModelConvert,
-    type CommonViewModel,
-} from '$lib/view-models/core/common-view-model';
-import toast from 'svelte-french-toast';
+// import http from '$lib/services/core/http.service.js';
+import api from '$lib/services/core/ky.service';
+import { fail } from '@sveltejs/kit';
+// import { showLoadingOverlay } from '$lib/stores/globalState';
+import { getErrorToast, getPromiseToast } from '$lib/toast/toast-service';
+// import {
+//     CommonViewModelConvert,
+//     type CommonViewModel,
+// } from '$lib/view-models/core/common-view-model';
+// import toast from 'svelte-french-toast';
+import { goto } from '$app/navigation';
 import { superValidate } from 'sveltekit-superforms/client';
 import { z } from 'zod';
 
 // creating schema
 export const _addNewHireSchema = z.object({
-    tempId: z.string(),
-    email: z.string().email(),
+    tempId: z
+        .string({ required_error: 'Sila tetapkan ID sementara calon' })
+        .min(1, { message: 'ID hendaklah lebih daripada 1 angka' }),
+    email: z
+        .string()
+        .email({ message: 'Pastikan emel adalah betul dan lengkap' }),
 });
 
 export const load = async () => {
@@ -20,41 +28,47 @@ export const load = async () => {
 };
 
 export const _submit = async (formData: object) => {
-    showLoadingOverlay.set(true);
-    const response: Response = await http
-        .post('http://127.0.0.1:3333/api/v1/employments/new-hire', {
-            body: JSON.stringify(formData),
-            headers: {
-                Accept: 'application/json',
-                'Content-type': 'application/json',
-            },
-            prefixUrl: '',
+    // showLoadingOverlay.set(true);
+
+    const form = await superValidate(formData, _addNewHireSchema);
+
+    if (!form.valid) {
+        getErrorToast();
+        return fail(400, form);
+    }
+
+    const response: Promise<Response> = api
+        .post('api/v1/employments/new-hire', {
+            body: JSON.stringify(form),
         })
         .json();
 
-    const data: CommonViewModel = CommonViewModelConvert.fromJson(
-        JSON.stringify(response),
-    );
-
-    console.log(data.status);
-
-    showLoadingOverlay.set(false);
-
-    toast.success('Berjaya disimpan!', {
-        style: 'background: #333; color: #fff;',
+    getPromiseToast(response).then(() => {
+        goto('../lantikan-baru');
     });
 
-    if (data.status == 201) {
-        toast.success('Berjaya disimpan!', {
-            style: 'background: #333; color: #fff;',
-        });
-    } else {
-        toast.error('Sila pastikan maklumat adalah lengkap dengan tepat.', {
-            style: 'background: #333; color: #fff;',
-        });
-    }
+    // const data: CommonViewModel = CommonViewModelConvert.fromJson(
+    //     JSON.stringify(response),
+    // );
 
-    const form = await superValidate(_addNewHireSchema);
+    // console.log(data.status);
+
+    // showLoadingOverlay.set(false);
+
+    // toast.success('Berjaya disimpan!', {
+    //     style: 'background: #333; color: #fff;',
+    // });
+
+    // if (data.status == 201) {
+    //     toast.success('Berjaya disimpan!', {
+    //         style: 'background: #333; color: #fff;',
+    //     });
+    // } else {
+    //     toast.error('Sila pastikan maklumat adalah lengkap dengan tepat.', {
+    //         style: 'background: #333; color: #fff;',
+    //     });
+    // }
+
+    // const form = await superValidate(_addNewHireSchema);
     return { form };
-    
 };
