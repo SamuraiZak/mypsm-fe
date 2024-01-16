fbdba555375b998c981f1d2fd9f1c7c1d441effe
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-01-16 08:59:47 +0800

    feat: WIP salary-allowance

--- src/routes/(guarded)/urus-setia/gaji/gaji-elaun/butiran/+page.ts
+++ src/routes/(guarded)/urus-setia/gaji/gaji-elaun/butiran/+page.ts
@@ -1,16 +1,46 @@
 import { goto } from '$app/navigation';
 import api from '$lib/services/core/ky.service';
+import { showLoadingOverlay } from '$lib/stores/globalState';
+import {
+    SalaryDetailResponseConvert,
+    type SalaryDetailResponse,
+} from '$lib/view-models/mypsm/salary/salary-and-allowance/salary-details-response.view-model';
 import toast from 'svelte-french-toast';
 
-export const load = async () => {
-    // const entitlementResponse = await api
-    //     .get('api/v1/leaves/leaves/entitlement')
+export async function load() {
+    showLoadingOverlay.set(true);
+
+    // const response: Response = await api
+    //     .get('api/v1/salaries/allowance/2', {})
     //     .json();
-    // const leaveHistory = leaveHistoryResponse.data;
-    // return {
-    //     leaveHistory,
-    // };
-};
+
+    const result: unknown = {
+        "status": 201,
+        "message": "Record retrieved successfully.",
+        "data": {
+            "employeeNo": "EM00002",
+            "name": "Jon Bovi",
+            "identityCard": "620302138881",
+            "grade": "Pelukis",
+            "currentPlacement": "LKIM Negeri Selangor",
+            "group": "Kumpulan Perkhidmatan Sokongan III"
+        }
+    }
+
+    const response = <JSON>result;
+
+    const salaryDetail: SalaryDetailResponse =
+        SalaryDetailResponseConvert.fromJson(JSON.stringify(response));
+
+    const salaryDetailData = salaryDetail.data;
+
+    showLoadingOverlay.set(false);
+    return {
+        props: {
+            salaryDetailData,
+        },
+    };
+}
 
 export const _submitSalaryAllowance = async () => {
     console.log('first');
