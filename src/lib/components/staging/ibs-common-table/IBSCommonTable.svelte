c05a17ef45bea3909408fedcae0d5365726df528
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-01-17 03:29:12 +0800

    feat: WIP holitable

--- src/lib/components/staging/ibs-common-table/IBSCommonTable.svelte
+++ src/lib/components/staging/ibs-common-table/IBSCommonTable.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
     import SvgCaretDown from '$lib/assets/svg/SvgCaretDown.svelte';
     import SvgCaretUp from '$lib/assets/svg/SvgCaretUp.svelte';
+    import SvgChevronLeft from '$lib/assets/svg/SvgChevronLeft.svelte';
+    import SvgChevronRight from '$lib/assets/svg/SvgChevronRight.svelte';
     import SvgSortDown from '$lib/assets/svg/SvgSortDown.svelte';
     import SvgSortUp from '$lib/assets/svg/SvgSortUp.svelte';
+    import { currentPage } from '$lib/stores/globalState';
+    import { Button, PaginationItem, Select } from 'flowbite-svelte';
 
+    // param for table data
     export let tableData: any[] = [];
 
-    export let request = {
-        pageNum: 1,
-        pageSize: 10,
-        orderBy: '',
-        orderType: '',
-        filter: {},
-    };
+    export let param:any;
+    $: request = param;
 
     export let sortBy: string = Object.keys(tableData[0])[0];
     export let sortDirection: string = 'ascending';
@@ -33,20 +33,28 @@
         } else {
             sortBy = columnName;
             sortDirection = 'ascending';
+            param.orderBy = columnName;
         }
 
         // TODO: Fetch new data with the filter
     }
 </script>
 
+<p>in table orderby {param.orderBy}</p>
+
+<button on:click={()=>{
+    param.orderBy = "THIS";
+}}>
+    another change
+</button>
 <div>
-    <div class="w-full overflow-x-auto">
+    <div class="w-full overflow-x-auto h-[300px]">
         <table
             class="table w-full table-auto border-collapse border border-ios-labelColors-separator-light"
         >
             <!-- table head starts -->
 
-            <thead>
+            <thead class="sticky top-0">
                 <!-- table head row starts -->
 
                 <tr class="h-10 bg-ios-systemColors-quaternarySystemFill-light">
@@ -130,6 +138,27 @@
     <!-- table control -->
     <div class="flex w-full flex-row justify-between">
         <p>leading</p>
-        <p>trailing</p>
+        <div class="flex flex-wrap items-center justify-start gap-2.5">
+            <div
+                class=" flex h-[28px] flex-col items-center justify-center rounded-[3px] border border-bdr-primary bg-bgr-primary px-2.5 focus:outline-none focus:ring-0"
+            >
+                <span class="text-sm text-txt-secondary">
+                    1
+                </span>
+            </div>
+            <Button
+                class="h-[28px] rounded-[3px] border border-bdr-primary bg-bgr-primary px-2.5 text-sm text-txt-secondary hover:bg-bgr-tertiary focus:outline-none focus:ring-0"
+                ><SvgChevronLeft size="14" /></Button
+            >
+
+                <PaginationItem
+                    normalClass="h-[28px] rounded-[3px] border border-bdr-primary bg-bgr-primary px-2.5 text-sm text-txt-secondary hover:bg-bgr-tertiary"
+                    >1</PaginationItem
+                >
+                ...
+
+            
+
+        </div>
     </div>
 </div>
