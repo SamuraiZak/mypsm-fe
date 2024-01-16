f204890f953fb903389c027f505ebee2439895e6
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-01-16 08:53:25 +0800

    feat: Modified sidebar design

--- src/lib/components/sidebar/Sidebar.svelte
+++ src/lib/components/sidebar/Sidebar.svelte
@@ -20,22 +20,34 @@
     let divClass = 'py-2 bg-bgr-secondary h-full overflow-y-auto';
     let spanClass = 'flex-1 text-base text-primary';
     let aClass =
-        'flex items-center p-2 text-base font-normal text-txt-primary transition duration-75 group hover:bg-bgr-tertiary';
+        'flex items-center p-2 text-base font-normal text-txt-primary transition duration-75 group hover:bg-ios-ui-sidebarItem-light-hover';
     let dropdownAClass =
-        'flex items-center p-2 pl-6 text-base font-normal text-txt-primary transition duration-75 group hover:bg-bgr-tertiary';
-    let activeClass =
-        'flex items-center p-2 text-base font-normal text-txt-primary transition duration-75 group bg-bgr-forth hover:bg-bgr-tertiary';
-    let dropdownActiveClass =
-        'flex items-center p-2 pl-6 text-base font-normal text-txt-primary transition duration-75 group bg-bgr-forth hover:bg-bgr-tertiary';
+        'flex items-center p-2 pl-6 text-base font-normal text-txt-primary transition duration-75 group hover:bg-ios-ui-sidebarItem-light-hover';
+    let sidebarItemActive =
+        'flex items-center p-2 text-base font-normal text-ios-activeColors-activeBlue-light transition duration-75 group bg-ios-ui-sidebarItem-light-active border-l-4 border-ios-activeColors-activeBlue-light ';
+    let sidebarDropdownItemActive =
+        'flex items-center p-2 pl-6 text-base font-normal text-ios-activeColors-activeBlue-light transition duration-75 group bg-ios-ui-sidebarItem-light-active border-l-4 border-ios-activeColors-activeBlue-light ';
+
     $: activeUrl = $page.url.pathname;
 </script>
 
+<div
+    class="flex h-[40px] max-h-[40px] min-h-[40px] flex-row items-center border-b bg-ios-backgroundColors-systemBackground-light px-2"
+>
+    <div
+        class="text-sm font-normal tracking-wide text-ios-labelColors-secondaryLabel-light"
+    >
+        Menu
+    </div>
+</div>
 <Sidebar
     {activeUrl}
-    class="h-full w-[250px] overflow-y-auto border-r px-2 py-2"
+    class="h-full w-[250px] overflow-y-auto border-r bg-ios-backgroundColors-systemBackground-light"
 >
-    <SidebarWrapper class="m-0 p-0">
-        <SidebarGroup>
+    <SidebarWrapper
+        class="m-0 rounded-none bg-ios-backgroundColors-systemBackground-light p-0"
+    >
+        <SidebarGroup ulClass="space-y-0">
             {#each sidebarItems as item}
                 {#if !item.dropdown && item.redirect}
                     <SidebarItem
@@ -44,13 +56,14 @@
                         label={item.name}
                         href={item.redirect}
                         active={activeUrl.includes(item.redirect)}
-                        {activeClass}
-                        data-sveltekit-preload-data="tap"
+                        activeClass={sidebarItemActive}
+                        data-sveltekit-preload-data="false"
                     />
                 {:else if item.dropdown}
                     <SidebarDropdownWrapper
+                        ulClass="py-0 space-y-0"
                         spanClass="text-base w-full flex"
-                        btnClass="flex items-center p-2 text-base font-normal text-txt-primary transition duration-75 group hover:bg-bgr-tertiary w-full"
+                        btnClass="flex items-center p-2 text-base font-normal text-txt-primary transition duration-75 group hover:bg-ios-ui-sidebarItem-light-hover w-full"
                         label={item.name}
                     >
                         <svelte:fragment slot="arrowup"
@@ -66,8 +79,8 @@
                                 href={dropdownItem.redirect}
                                 active={!!dropdownItem.redirect &&
                                     activeUrl.includes(dropdownItem.redirect)}
-                                activeClass={dropdownActiveClass}
-                                data-sveltekit-preload-data="tap"
+                                activeClass={sidebarDropdownItemActive}
+                                data-sveltekit-preload-data="false"
                             />
                         {/each}
                     </SidebarDropdownWrapper>
