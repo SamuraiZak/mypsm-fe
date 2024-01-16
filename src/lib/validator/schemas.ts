1ae04dc053baf6cf50d3d871a1192e06592ba49f
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-01-17 03:28:06 +0800

    chore: Removed unused files

--- src/lib/validator/schemas.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const _loginSchema = z.object({
-    username: z.string().min(2),
-    password: z.string().min(6)
-});
-
-export type LoginSchema = typeof _loginSchema;
