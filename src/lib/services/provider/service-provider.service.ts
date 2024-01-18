8faede2c73ef3fe6ee9801910ec236f832107e2c
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-01-18 13:30:35 +0800

    feat: Added service provider

---
+++ src/lib/services/provider/service-provider.service.ts
@@ -0,0 +1,17 @@
+// ===============================================================
+// Service Provider
+// ===============================================================
+
+import { env } from '$env/dynamic/public';
+import ky from 'ky';
+
+const http = ky.create({
+    prefixUrl: env.PUBLIC_BASE_API_URL,
+    headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json',
+    },
+    hooks: {},
+});
+
+export default http;
