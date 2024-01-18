81b6424d1044377129b36e6b70beedd9ed7e6253
nurazmina <nurazmina@impact-multimedia.com>
2024-01-18 20:03:35 +0800

    feat: WIP added Validation at page.ts for Penyokong-Kakitangan Kontrak-Kemaskini

--- src/routes/(guarded)/penyokong/kakitangan-kontrak/pembaharuan/kemaskini-permohonan-[id]-[ic]/+page.ts
+++ src/routes/(guarded)/penyokong/kakitangan-kontrak/pembaharuan/kemaskini-permohonan-[id]-[ic]/+page.ts
@@ -15,8 +15,65 @@ import { mockLookupServiceTypes } from '$lib/mocks/database/mockLookupServiceTyp
 import { mockLookupStates } from '$lib/mocks/database/mockLookupStates';
 import { mockLookupGrades } from '$lib/mocks/database/mockLoopkupGrades';
 import { getEmployees } from '$lib/service/employees/staff-service.js';
+import { fail } from '@sveltejs/kit';
+import toast from 'svelte-french-toast';
+import { superValidate } from 'sveltekit-superforms/client';
+import { z } from 'zod';
+
+// Result From Roles
+const resultOption = z
+    .string()
+    .min(1, { message: 'Sila tetapkan pilihan anda.' });
+
+const stepperResultFromRoles = z
+    .string({ required_error: 'Medan ini latihan tidak boleh kosong.' })
+    .min(4, {
+        message: 'Medan ini hendaklah lebih daripada 4 karakter.',
+    })
+    .max(124, {
+        message: 'Medan ini tidak boleh melebihi 124 karakter.',
+    })
+    .trim();
+
+export const _stepperResultFromRoles = z.object({
+    actionRemark: stepperResultFromRoles,
+    resultOption: resultOption,
+});
+
+export const _submitFormStepperResultFromRoles = async (formData: object) => {
+    const stepperResultFromRoles = await superValidate(
+        formData,
+        _stepperResultFromRoles,
+    );
+
+    if (!stepperResultFromRoles.valid) {
+        toast.error('Sila pastikan maklumat adalah lengkap dengan tepat.', {
+            style: 'background: #333; color: #fff;',
+        });
+        return fail(400, stepperResultFromRoles);
+    } else {
+        console.log('Request Body: ', formData);
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify(stepperResultFromRoles),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                toast.success('Berjaya disimpan!', {
+                    style: 'background: #333; color: #fff;',
+                });
+                console.log('Response Returned: ResultFromRoles-54', json);
+            });
+    }
+    return { stepperResultFromRoles };
+};
 
 export async function load({ params }) {
+    const stepperResultFromRoles = await superValidate(_stepperResultFromRoles);
+
     const data: IntEmployees[] = await getEmployees();
 
     const currentEmployee: IntEmployees | undefined = data.find(
@@ -97,6 +154,7 @@ export async function load({ params }) {
     if (!currentEmployee) throw new Error('Record not found');
 
     return {
+        stepperResultFromRoles,
         record: {
             data,
             currentEmployee,
