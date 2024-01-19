42d49b81d7483b1122386dadaac3771ff4b17b29
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-01-19 17:38:47 +0800

    feat: WIP uses page.ts to store scripts.

--- src/routes/(guarded)/penyokong/perjawatan/tawaran-baru/kemaskini-keputusan-permohonan/keputusan-peranan-lain/KeputusanPerananLain.svelte
+++ src/routes/(guarded)/penyokong/perjawatan/tawaran-baru/kemaskini-keputusan-permohonan/keputusan-peranan-lain/KeputusanPerananLain.svelte
@@ -29,66 +29,7 @@
 
     let passerResult: string = 'passed';
 
-    // =========================================================================
-    // z validation schema and submit function for the new employment form fields
-    // =========================================================================
-    let errorData: any;
 
-    const longTextSchema = z
-        .string({ required_error: 'Medan ini tidak boleh kosong.' })
-        .min(4, {
-            message: 'Medan ini hendaklah lebih daripada 4 karakter.',
-        })
-        .max(124, {
-            message: 'Medan ini tidak boleh melebihi 124 karakter.',
-        })
-        .trim();
-
-    // New Employment - Supporter Result section
-    const supporterResultSchema = z.object({
-        supporterRemark: longTextSchema,
-        supporterResult: z.enum(['true', 'false'], {
-            errorMap: (issue, { defaultError }) => ({
-                message:
-                    issue.code === 'invalid_enum_value'
-                        ? 'Sila tetapkan pilihan anda.'
-                        : defaultError,
-            }),
-        }),
-    });
-
-    export const submitSupporterResultForm = async (event: Event) => {
-        const formElement = event.target as HTMLFormElement;
-        const formData = new FormData(formElement);
-
-        const supporterResultData = {
-            supporterRemark: String(formData.get('supporterRemark')),
-            supporterResult: String(formData.get('supporterResult')),
-        };
-
-        try {
-            errorData = [];
-            const result = supporterResultSchema.parse(supporterResultData);
-            if (result) {
-                errorData = [];
-                toast.success('Berjaya disimpan!', {
-                    style: 'background: #333; color: #fff;',
-                });
-                setTimeout(() => goto('../../perjawatan/tawaran-baru'), 1500);
-            }
-        } catch (error: unknown) {
-            if (error instanceof ZodError) {
-                const { fieldErrors: errors } = error.flatten();
-                errorData = errors;
-                toast.error(
-                    'Sila pastikan maklumat adalah lengkap dengan tepat.',
-                    {
-                        style: 'background: #333; color: #fff;',
-                    },
-                );
-            }
-        }
-    };
 </script>
 
 <form
