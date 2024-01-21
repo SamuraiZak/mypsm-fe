12714c93633d61961f21b9a2ea7d622e8b19c708
Holifield William <holifield.william@impactbs.my>
2024-01-21 23:31:34 +0800

    feat: WIP edited letter editor

--- src/routes/(development)/modal/+page.svelte
+++ src/routes/(development)/modal/+page.svelte
@@ -2,7 +2,11 @@
     // your script goes here
     import JanaSurat from '$lib/components/modal/letter-editor-modal/JanaSurat.svelte';
     import LetterEditorModal from '$lib/components/modal/letter-editor-modal/LetterEditorModal.svelte';
+    import SuratPenangguhanRayuan from '$lib/components/modal/letter-editor-modal/SuratPenangguhanRayuan.svelte';
+    import SuratTawaranPemangkuan from '$lib/components/modal/letter-editor-modal/SuratTawaranPemangkuan.svelte';
 </script>
 
 <!-- <JanaSurat /> -->
-<LetterEditorModal/>
+<!-- <LetterEditorModal/> -->
+<!-- <SuratPenangguhanRayuan/> -->
+<SuratTawaranPemangkuan modalOpen={true} letterTitle="Surat Tawaran Pemangkuan"/>
