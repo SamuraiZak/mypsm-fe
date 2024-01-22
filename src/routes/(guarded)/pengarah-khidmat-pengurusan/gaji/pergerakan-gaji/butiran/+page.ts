d433363f84fcadd858a980f9baed7825fedf8c59
nurazmina <nurazmina@impact-multimedia.com>
2024-01-22 21:51:34 +0800

    feat: WIP added Superform to page.ts for PKP-Pergerakan Gaji-Butiran

---
+++ src/routes/(guarded)/pengarah-khidmat-pengurusan/gaji/pergerakan-gaji/butiran/+page.ts
@@ -0,0 +1,67 @@
+import { getPromiseToast } from '$lib/toast/toast-service';
+import { fail } from '@sveltejs/kit';
+import toast from 'svelte-french-toast';
+import { superValidate } from 'sveltekit-superforms/client';
+import { z } from 'zod';
+
+// Verification Retirement Application
+const longTextField = z
+    .string({ required_error: 'Medan ini latihan tidak boleh kosong.' })
+    .min(4, {
+        message: 'Medan ini hendaklah lebih daripada 4 karakter.',
+    })
+    .max(124, {
+        message: 'Medan ini tidak boleh melebihi 124 karakter.',
+    })
+    .trim();
+
+const resultOption = z
+    .string()
+    .min(1, { message: 'Sila tetapkan pilihan anda.' });
+
+export const _stepperRevisionSalaryMovementSchedule = z.object({
+    meetingActionRemark: longTextField,
+    meetingResultOption: resultOption,
+});
+
+export const _submitFormStepperRevisionSalaryMovementSchedule = async (
+    formData: object,
+) => {
+    const stepperRevisionSalaryMovementSchedule = await superValidate(
+        formData,
+        _stepperRevisionSalaryMovementSchedule,
+    );
+
+    if (!stepperRevisionSalaryMovementSchedule.valid) {
+        toast.error('Sila pastikan maklumat adalah lengkap dengan tepat.', {
+            style: 'background: #333; color: #fff;',
+        });
+        return fail(400, stepperRevisionSalaryMovementSchedule);
+    } const responsePromise = fetch(
+        'https://jsonplaceholder.typicode.com/posts',
+        {
+            method: 'POST',
+            body: JSON.stringify(_stepperRevisionSalaryMovementSchedule),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        },
+    )
+        .then((response) => response.json())
+        .then((json) => {
+            console.log('Response Returned: ', json);
+        });
+    getPromiseToast(responsePromise);
+    return { stepperRevisionSalaryMovementSchedule };
+};
+
+//Async
+export const load = async () => {
+    const stepperRevisionSalaryMovementSchedule = await superValidate(
+        _stepperRevisionSalaryMovementSchedule,
+    );
+
+    return {
+        stepperRevisionSalaryMovementSchedule,
+    };
+};
