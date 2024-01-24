0448a40942dca33e5b281c6fdee3b5393fb953fe
nurazmina <nurazmina@impact-multimedia.com>
2024-01-24 23:03:12 +0800

    feat: WIP added Superform KT-Latihan-Pembiayaan-Baru-Pelanjutan Pelajaran(page.ts)

--- src/routes/(guarded)/kakitangan/latihan/pembiayaan/baru/pelanjutan-pelajaran/+page.ts
+++ src/routes/(guarded)/kakitangan/latihan/pembiayaan/baru/pelanjutan-pelajaran/+page.ts
@@ -1,8 +1,90 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { mockContStudy } from '$lib/mocks/latihan/mockContStudy.js';
 import { fail } from '@sveltejs/kit';
+import {
+    getErrorToast,
+    getPromiseToast,
+} from '$lib/services/core/toast/toast-service';
+import toast from 'svelte-french-toast';
+import { superValidate } from 'sveltekit-superforms/client';
+import { z } from 'zod';
+
+// Stepper Lesson Info
+const option = z.string().min(1, { message: 'Sila tetapkan pilihan anda.' });
+
+const date = z.coerce.date({
+    errorMap: (issue, { defaultError }) => ({
+        message:
+            issue.code === 'invalid_date'
+                ? 'Tarikh tidak boleh dibiar kosong.'
+                : defaultError,
+    }),
+});
+
+const textField = z
+    .string({ required_error: 'Medan ini latihan tidak boleh kosong.' })
+    .min(4, {
+        message: 'Medan ini hendaklah lebih daripada 4 karakter.',
+    })
+    .max(124, {
+        message: 'Medan ini tidak boleh melebihi 124 karakter.',
+    })
+    .trim();
+
+const number = z
+    .string({ required_error: 'Medan ini latihan tidak boleh kosong.' })
+    .min(1, {
+        message: 'Medan ini hendaklah mempunyai nombor.',
+    })
+    .max(124, {
+        message: 'Medan ini tidak boleh melebihi 124 karakter.',
+    })
+    .trim();
+
+export const _stepperLessonInfo = z.object({
+    eduLevel: option,
+    courseName: textField,
+    iptaName: textField,
+    institutions: textField,
+    studyDuration: number,
+    dateAdmission: date.refine((data) => data >= new Date(), {
+        message: 'Tidak boleh kurang atau pada tarikh semasa',
+    }),
+    expectedEndDate: date.refine((data) => data >= new Date(), {
+        message: 'Tidak boleh kurang atau pada tarikh semasa',
+    }),
+});
+
+export const _submitFormStepperLessonInfo = async (formData: object) => {
+    const stepperLessonInfo = await superValidate(formData, _stepperLessonInfo);
+
+    if (!stepperLessonInfo.valid) {
+        toast.error('Sila pastikan maklumat adalah lengkap dengan tepat.', {
+            style: 'background: #333; color: #fff;',
+        });
+        return fail(400, stepperLessonInfo);
+    }
+    const responsePromise = fetch(
+        'https://jsonplaceholder.typicode.com/posts',
+        {
+            method: 'POST',
+            body: JSON.stringify(_stepperLessonInfo),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        },
+    )
+        .then((response) => response.json())
+        .then((json) => {
+            console.log('Response Returned: ', json);
+        });
+    getPromiseToast(responsePromise);
+    return { stepperLessonInfo };
+};
 
 export async function load() {
+    const stepperLessonInfo = await superValidate(_stepperLessonInfo);
+
     const data: IntContStudy[] = await mockContStudy;
 
     // Assuming 'employeeNumber' is the field you want to increment
@@ -27,6 +109,7 @@ export async function load() {
         status: '',
     };
     return {
+        stepperLessonInfo,
         record: {
             data,
             newContStudyApplication,
