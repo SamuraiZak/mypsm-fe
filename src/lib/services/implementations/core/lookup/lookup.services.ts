8f9cee1c0675097afd65383bf7d6a65961447ff1
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-01-24 23:35:02 +0800

    feat: Modified lookup services

--- src/lib/services/implementations/core/lookup/lookup.services.ts
+++ src/lib/services/implementations/core/lookup/lookup.services.ts
@@ -5,6 +5,7 @@
 import http from "$lib/services/provider/service-provider.service";
 import { EnumAgencyGroupConvert } from "$lib/view-models/core/lookup/agency-group/agency-group-senum.view-model";
 import { EnumAssetDeclarationStatusResponseConvert } from "$lib/view-models/core/lookup/asset-declaration/asset-declaration-enum.view-model";
+import { EnumLoginIDTypeResponseConvert } from "$lib/view-models/core/lookup/id-type/id-type-enum-response.view-model";
 import { IDTypeListConvert } from "$lib/view-models/core/lookup/lookup-id-type-list-response.view-model";
 import { RoleListConvert } from "$lib/view-models/core/lookup/lookup-role-list-response.view-model";
 
@@ -40,5 +41,11 @@ export class LookupService{
         return EnumAssetDeclarationStatusResponseConvert.fromResponse(response);
     }
 
+    // get list of login id type
+    static async getEnumLoginIdType(){
+        const response: Response = await http.get('lookups/idType').json();
+
+        return EnumLoginIDTypeResponseConvert.fromResponse(response);
+    }
 
 }
