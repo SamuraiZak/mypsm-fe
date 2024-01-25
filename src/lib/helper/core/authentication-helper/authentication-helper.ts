e12243ecc957b94afbb2b41e7d7a9ca1a9be17a3
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-01-25 14:38:29 +0800

    feat: Added authentication helper to store token in localstorage

--- src/lib/helper/core/authentication-helper/authentication-helper.ts
+++ src/lib/helper/core/authentication-helper/authentication-helper.ts
@@ -1,3 +1,19 @@
 // ===============================================================
 // Authentication Helper Functions
 // ===============================================================
+
+import { LocalStorageKeyConstant } from "$lib/constants/core/local-storage-key-constant";
+import type { AuthenticationResponseViewModel } from "$lib/view-models/core/auth/auth-response.view-model";
+
+export class AuthenticationHelper {
+
+    // store login result in local storage
+    static storeLoginResult(loginResponse: AuthenticationResponseViewModel){
+        localStorage.setItem(LocalStorageKeyConstant.accessToken, loginResponse.data.token);
+    }
+
+    // clear local storage
+    static clearLocalStorage(){
+        localStorage.clear();
+    }
+}
