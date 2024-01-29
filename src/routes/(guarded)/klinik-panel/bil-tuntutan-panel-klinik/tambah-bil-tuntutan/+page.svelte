7f96baa64f01653ac01319bb865aa30a808c604a
nurazmina <nurazmina@impact-multimedia.com>
2024-01-29 20:51:18 +0800

    feat: WIP added validation

--- src/routes/(guarded)/klinik-panel/bil-tuntutan-panel-klinik/tambah-bil-tuntutan/+page.svelte
+++ src/routes/(guarded)/klinik-panel/bil-tuntutan-panel-klinik/tambah-bil-tuntutan/+page.svelte
@@ -19,10 +19,21 @@
     import { klinik } from '$lib/mocks/urus-setia/perubatan/klinik';
     import { tahunRawatan } from '$lib/mocks/klinik-panel/tahun-rawatan';
     import { bulanRawatan } from '$lib/mocks/klinik-panel/bulan-rawatan';
+    import SvgCheck from '$lib/assets/svg/SvgCheck.svelte';
+    import type { PageData } from './$types';
+    import toast, { Toaster } from 'svelte-french-toast';
+    import { z, ZodError } from 'zod';
+    import { superForm } from 'sveltekit-superforms/client';
+    import {
+        _stepperInvoiceInfo,
+        _submitFormStepperInvoiceInfo,
+    } from './+page';
 
+    export let data: PageData;
     export let disabled: boolean = true;
     export let selectedFiles: any = [];
 
+    let errorData: any;
     let target: any;
     let texthidden = false;
     let selectedKlinik = klinik[0].value;
@@ -56,6 +67,67 @@
         selectedFiles.splice(index, 1);
         fileSelectionList.set(selectedFiles);
     }
+
+    const retirementDocumentForm = async (event: Event) => {
+        let uploadedFiles = selectedFiles;
+        const formData = new FormData(event.target as HTMLFormElement);
+
+        const exampleFormData = {
+            uploadedFiles: uploadedFiles,
+        };
+
+        const exampleFormSchema = z.object({
+            // checkbox schema
+
+            uploadedFiles: z.any().array().nonempty({
+                message:
+                    'Sila muat naik dokumen sokongan pada ruangan disediakan.',
+            }),
+        });
+
+        try {
+            const result = exampleFormSchema.parse(exampleFormData);
+            if (result) {
+                errorData = [];
+                toast.success('Berjaya disimpan!', {
+                    style: 'background: #333; color: #fff;',
+                });
+
+                const id = crypto.randomUUID().toString();
+                const validatedExamFormData = { ...exampleFormData, id };
+                console.log(
+                    'REQUEST BODY: ',
+                    JSON.stringify(validatedExamFormData),
+                );
+            }
+        } catch (err: unknown) {
+            if (err instanceof ZodError) {
+                const { fieldErrors: errors } = err.flatten();
+                errorData = errors;
+                console.log('ERROR!', err.flatten());
+                toast.error(
+                    'Sila pastikan maklumat adalah lengkap dengan tepat.',
+                    {
+                        style: 'background: #333; color: #fff;',
+                    },
+                );
+            }
+        }
+    };
+
+    const {
+        form: invoiceInfoForm,
+        errors: invoiceInfoErrors,
+        enhance: invoiceInfoEnhance,
+    } = superForm(data.stepperInvoiceInfo, {
+        SPA: true,
+        validators: _stepperInvoiceInfo,
+        onSubmit() {
+            _submitFormStepperInvoiceInfo($invoiceInfoForm);
+        },
+        taintedMessage:
+            'Terdapat maklumat yang belum disimpan. Adakah anda hendak keluar dari laman ini?',
+    });
 </script>
 
 <section class="flex w-full flex-col items-start justify-start">
@@ -73,71 +145,122 @@
 
 <Stepper>
     <StepperContent>
-        <StepperContentHeader title="Maklumat Invois"></StepperContentHeader>
-        <StepperContentBody>
-            <div class="flex w-full flex-col gap-2">
-                <DropdownSelect
-                    id="klinikDropdown"
-                    label="Klinik"
-                    dropdownType="label-left-full"
-                    bind:index={selectedKlinik}
-                    options={klinik}
-                ></DropdownSelect>
-                <DateSelector
-                    {disabled}
-                    {handleDateChange}
-                    label={'Tarikh Invoice'}
-                />
-                <TextField
-                    {disabled}
-                    id="noInvois"
-                    label={'No. Invois'}
-                    value={''}
-                ></TextField>
-                <DropdownSelect
-                    id="bulanRawatanDropdown"
-                    label="Bulan Rawatan"
-                    dropdownType="label-left-full"
-                    bind:index={selectedBulanRawatan}
-                    options={bulanRawatan}
-                ></DropdownSelect>
-                <DropdownSelect
-                    id="tahunRawatanDropdown"
-                    label="Tahun Rawatan"
-                    dropdownType="label-left-full"
-                    bind:index={selectedTahunRawatan}
-                    options={tahunRawatan}
-                ></DropdownSelect>
-                <TextField
-                    {disabled}
-                    id="jumlah"
-                    label={'Jumlah'}
-                    value={'Kuching'}
-                ></TextField>
-            </div></StepperContentBody
+        <StepperContentHeader title="Maklumat Invois"
+            ><TextIconButton
+                primary
+                label="Simpan"
+                form="FormStepperInvoiceInfo"
+            >
+                <SvgCheck></SvgCheck>
+            </TextIconButton></StepperContentHeader
+        >
+        <StepperContentBody
+            ><form
+                id="FormStepperInvoiceInfo"
+                class="flex w-full flex-col gap-2"
+                use:invoiceInfoEnhance
+                method="POST"
+            >
+                <div class="flex w-full flex-col gap-2">
+                    <DropdownSelect
+                        hasError={!!$invoiceInfoErrors.name}
+                        dropdownType="label-left-full"
+                        id="name"
+                        label="Klinik"
+                        bind:value={$invoiceInfoForm.name}
+                        options={[
+                            { value: '1', name: 'Klinik A' },
+                            { value: '2', name: 'Klinik B' },
+                            { value: '3', name: 'Klinik C' },
+                        ]}
+                    ></DropdownSelect>
+                    {#if $invoiceInfoErrors.name}
+                        <span
+                            class="ml-[-500px] font-sans text-sm italic text-system-danger"
+                            >{$invoiceInfoErrors.name}</span
+                        >
+                    {/if}
+                    <DateSelector
+                        {disabled}
+                        {handleDateChange}
+                        label={'Tarikh Invoice'}
+                    />
+                    <TextField
+                        {disabled}
+                        id="noInvois"
+                        label={'No. Invois'}
+                        value={''}
+                    ></TextField>
+                    <DropdownSelect
+                        hasError={!!$invoiceInfoErrors.treatmentMonth}
+                        dropdownType="label-left-full"
+                        id="treatmentMonth"
+                        label="Bulan Rawatan"
+                        bind:value={$invoiceInfoForm.treatmentMonth}
+                        options={[
+                            { value: '1', name: 'Januari' },
+                            { value: '2', name: 'Februari' },
+                            { value: '3', name: 'Mac' },
+                        ]}
+                    ></DropdownSelect>
+                    {#if $invoiceInfoErrors.treatmentMonth}
+                        <span
+                            class="ml-[-500px] font-sans text-sm italic text-system-danger"
+                            >{$invoiceInfoErrors.treatmentMonth}</span
+                        >
+                    {/if}
+                    <DropdownSelect
+                        hasError={!!$invoiceInfoErrors.treatmentYear}
+                        dropdownType="label-left-full"
+                        id="treatmentYear"
+                        label="Bulan Rawatan"
+                        bind:value={$invoiceInfoForm.treatmentYear}
+                        options={[
+                            { value: '1', name: '2020' },
+                            { value: '2', name: '2021' },
+                            { value: '3', name: '2022' },
+                        ]}
+                    ></DropdownSelect>
+                    {#if $invoiceInfoErrors.treatmentYear}
+                        <span
+                            class="ml-[-500px] font-sans text-sm italic text-system-danger"
+                            >{$invoiceInfoErrors.treatmentYear}</span
+                        >
+                    {/if}
+                    <TextField
+                        {disabled}
+                        id="jumlah"
+                        label={'Jumlah'}
+                        value={'Kuching'}
+                    ></TextField>
+                </div>
+            </form></StepperContentBody
         >
     </StepperContent>
-    <StepperContent>
-        <StepperContentHeader title="Dokumen Sokongan yang Berkaitan"
+    <StepperContent
+        ><StepperContentHeader title="Dokumen Sokongan yang Berkaitan"
             ><TextIconButton
                 primary
                 label="Hantar"
-                onClick={() => {
-                    goto('');
-                }}><SvgPaperAirplane /></TextIconButton
+                form="retirementDocumentFormValidation"
+                ><SvgPaperAirplane /></TextIconButton
             ></StepperContentHeader
         >
         <StepperContentBody
-            ><div class="flex w-full flex-col gap-2">
-                <div
-                    class="flex max-h-full w-full flex-col items-center justify-center gap-2.5 pb-5 pt-5"
-                >
-                    <SectionHeader title="Muat Naik Dokumen"
+            ><form
+                id="retirementDocumentFormValidation"
+                on:submit|preventDefault={retirementDocumentForm}
+                class="flex w-full flex-col gap-2"
+            >
+                <div class="flex w-full flex-col gap-2">
+                    <p class="text-sm font-bold">Muat Naik Dokumen</p>
+                    <SectionHeader subTitle="Dokumen Sokongan"
                         ><div hidden={$fileSelectionList.length == 0}>
                             <FileInputField id="fileInput" {handleOnChange}
                             ></FileInputField>
                         </div></SectionHeader
                     >
+
                     <div
                         class="border-bdr-primaryp-5 flex h-fit w-full flex-col items-center justify-center gap-2.5 rounded-lg border p-2.5"
                     >
@@ -157,8 +280,9 @@
                                 class=" text-sm text-txt-tertiary"
                                 hidden={$fileSelectionList.length > 0}
                             >
-                                Seret dan lepas fail anda ke dalam ruangan ini atau pilih fail dari peranti anda
+                                Pilih fail dari peranti anda.
                             </p>
+                            <!-- svelte-ignore a11y-click-events-have-key-events -->
                             <div
                                 class="text-txt-tertiary"
                                 hidden={$fileSelectionList.length > 0}
@@ -185,25 +309,28 @@
                             </div>
                         </div>
                     </div>
-                </div>
-                <p class="pb-5 text-sm text-system-danger">
-                    Sila muat naik dokumen sokongan anda pada ruangan yang
-                    disediakan di atas.
-                </p>
-                <div class="flex w-full flex-col gap-2">
-                    <p class="text-sm font-bold">Fail-fail yang dimuat naik:</p>
+                    <div class="w-full">
+                        {#if errorData?.uploadedFiles}
+                            <span
+                                class="font-sans text-sm italic text-system-danger"
+                                >{errorData?.uploadedFiles[0]}</span
+                            >
+                        {/if}
+                    </div>
+                    <p class="text-sm">Fail-fail yang telah dimuat naik:</p>
                     <ul
                         class="flex w-full list-decimal flex-col gap-2 pl-4 text-sm"
                     >
                         <li>
-                            <DownloadAttachment fileName="Dokumen Sokongan-1" />
+                            <DownloadAttachment fileName="Resit-1" />
                         </li>
                         <li>
-                            <DownloadAttachment fileName="Dokumen Sokongan-2" />
+                            <DownloadAttachment fileName="Resit-2" />
                         </li>
                     </ul>
                 </div>
-            </div></StepperContentBody
+            </form></StepperContentBody
         >
     </StepperContent>
 </Stepper>
+<Toaster />
