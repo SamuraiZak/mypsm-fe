8de55f55a90503d0a4ae12be514260b54aa2f415
nurazmina <nurazmina@impact-multimedia.com>
2024-01-29 11:05:12 +0800

    feat: WIP rename Award Category Lookup

--- src/lib/view-models/core/lookup/award-category/award-category.view-model.ts
+++ src/lib/view-models/core/lookup/award-category/award-category.view-model.ts
@@ -2,7 +2,7 @@
 // Award Category Enum View Model
 // ===============================================================
 
-export interface EnumAwardCategoryReponse {
+export interface EnumAwardCategoryReponseViewModel {
     status:  number;
     message: string;
     data:    EnumAwardCategoryData;
@@ -22,17 +22,17 @@ export interface AwardCategory {
 export class EnumAwardCategoryReponseConvert {
 
     // to model from response
-    public static fromResponse(response: Response): EnumAwardCategoryReponse {
+    public static fromResponse(response: Response): EnumAwardCategoryReponseViewModel {
         return JSON.parse(JSON.stringify(response));
     }
 
     // to model from json string
-    public static fromJson(json: string): EnumAwardCategoryReponse {
+    public static fromJson(json: string): EnumAwardCategoryReponseViewModel {
         return JSON.parse(json);
     }
 
     // to json string from model
-    public static toJson(value: EnumAwardCategoryReponse): string {
+    public static toJson(value: EnumAwardCategoryReponseViewModel): string {
         return JSON.stringify(value);
     }
 }
