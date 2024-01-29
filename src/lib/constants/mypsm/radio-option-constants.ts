88436d0a9824753d3cbcc57a2f1113ddf7fa3e2f
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-01-29 11:51:29 +0800

    feat: creates radio option constants

---
+++ src/lib/constants/mypsm/radio-option-constants.ts
@@ -0,0 +1,54 @@
+export const certifyOptions: RadioOption[] = [
+    {
+        value: true,
+        label: 'SAH',
+    },
+    {
+        value: false,
+        label: 'TIDAK SAH',
+    },
+];
+
+export const approveOptions: RadioOption[] = [
+    {
+        value: true,
+        label: 'LULUS',
+    },
+    {
+        value: false,
+        label: 'TIDAK LULUS',
+    },
+];
+
+export const supportOptions: RadioOption[] = [
+    {
+        value: true,
+        label: 'SOKONG',
+    },
+    {
+        value: false,
+        label: 'TIDAK SOKONG',
+    },
+];
+
+export const integrityOptions: RadioOption[] = [
+    {
+        value: true,
+        label: 'BEBAS',
+    },
+    {
+        value: false,
+        label: 'TIDAK BEBAS',
+    },
+];
+
+export const commonOptions: RadioOption[] = [
+    {
+        value: true,
+        label: 'YA',
+    },
+    {
+        value: false,
+        label: 'TIDAK',
+    },
+];
