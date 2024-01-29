59a4ae37b5cf87386e3ea29d9e28cd1a167a74f6
nurazmina <nurazmina@impact-multimedia.com>
2024-01-29 11:07:55 +0800

    feat: WIP rename Status Lookup

--- src/lib/view-models/core/lookup/employment-status/employment-status.view-model.ts
+++ src/lib/view-models/core/lookup/employment-status/employment-status.view-model.ts
@@ -2,10 +2,10 @@
 // Employment Status Enum Response View Model
 // ===============================================================
 
-export interface EnumEmploymentStatusResponse {
-    status:  number;
+export interface EnumEmploymentStatusResponseViewModel {
+    status: number;
     message: string;
-    data:    EnumEmploymentStatusResponseData;
+    data: EnumEmploymentStatusResponseData;
 }
 
 export interface EnumEmploymentStatusResponseData {
@@ -13,26 +13,29 @@ export interface EnumEmploymentStatusResponseData {
 }
 
 export interface EnumEmploymentStatus {
-    id:            string;
+    id: string;
     occSectorCode: string;
     occSectorName: string;
 }
 
 // Converts JSON strings to/from your types
 export class EnumEmploymentStatusResponseConvert {
-
     // to model from response
-    public static fromResponse(response: Response): EnumEmploymentStatusResponse {
+    public static fromResponse(
+        response: Response,
+    ): EnumEmploymentStatusResponseViewModel {
         return JSON.parse(JSON.stringify(response));
     }
 
     // to model from json string
-    public static fromJson(json: string): EnumEmploymentStatusResponse {
+    public static fromJson(
+        json: string,
+    ): EnumEmploymentStatusResponseViewModel {
         return JSON.parse(json);
     }
 
     // to json string from model
-    public static toJson(value: EnumEmploymentStatusResponse): string {
+    public static toJson(value: EnumEmploymentStatusResponseViewModel): string {
         return JSON.stringify(value);
     }
-} 
+}
