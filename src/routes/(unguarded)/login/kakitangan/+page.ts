9c0677a4b39ef976993c3fd54c713add1d9886ed
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-01-30 17:42:40 +0800

    chore: reorganizes the codes arrangment in login page.ts

--- src/routes/(unguarded)/login/kakitangan/+page.ts
+++ src/routes/(unguarded)/login/kakitangan/+page.ts
@@ -1,13 +1,15 @@
 import { goto } from '$app/navigation';
+import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key-constant';
 import {
+    getLoadingToast,
     getLoginErrorToast,
     getLoginSuccessToast,
 } from '$lib/services/core/toast/toast-service';
-import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key-constant';
 import { AuthService } from '$lib/services/implementations/core/auth/authentication.service';
 import type { AuthenticationRequestViewModel } from '$lib/view-models/core/auth/auth-request.view-model';
 import type { AuthenticationResponseViewModel } from '$lib/view-models/core/auth/auth-response.view-model';
 import type { EnumRoleResponseViewModel } from '$lib/view-models/core/lookup/role/role-enum-reponse.view-model';
+import toast from 'svelte-french-toast';
 import { superValidate } from 'sveltekit-superforms/client';
 import { z } from 'zod';
 
@@ -31,120 +33,126 @@ export const load = async () => {
 };
 
 export const _submit = async (formData: AuthenticationRequestViewModel) => {
-    const response: AuthenticationResponseViewModel = await AuthService.loginEmployee(formData);
+    getLoadingToast();
 
-    if (response.status === 200) {
-        getLoginSuccessToast().finally(() =>
-            setTimeout(() => goto('/calon/halaman-utama'), 2000),
+    const response: AuthenticationResponseViewModel =
+        await AuthService.loginEmployee(formData).finally(() =>
+            toast.dismiss(),
         );
 
-        localStorage.setItem(LocalStorageKeyConstant.currentRole, formData.currentRole);
+    if (response.status === 200) {
+        localStorage.setItem(
+            LocalStorageKeyConstant.currentRole,
+            formData.currentRole,
+        );
+        getLoginSuccessToast().finally(() =>
+            setTimeout(() => {
+                switch (formData.currentRole) {
+                    case 'admin':
+                        goto('/kakitangan/halaman-utama');
+                        break;
 
-        switch (formData.currentRole) {
-            case 'admin':
-                goto('/kakitangan/halaman-utama');
-                break;
-        
-            case 'audit':
-                goto('/kakitangan/halaman-utama');
-                break;
-        
-            case 'kakitangan':
-                goto('/kakitangan/halaman-utama');
-                break;
-        
-            case 'ketua pengarah':
-                goto('/ketua-pengarah/halaman-utama');
-                break;
-        
-            case 'ketua seksyen':
-                goto('/ketua-seksyen/halaman-utama');
-                break;
-        
-            case 'klinik panel':
-                goto('/kakitangan/halaman-utama');
-                break;
-        
-            case 'pelulus':
-                goto('/pelulus/halaman-utama');
-                break;
-        
-            case 'pengarah audit':
-                goto('/pengarah-audit/halaman-utama');
-                break;
-        
-            case 'pengarah bahagian/negeri':
-                goto('/pengarah-bahagian-negeri/halaman-utama');
-                break;
-        
-            case 'penyokong':
-                goto('/penyokong/halaman-utama');
-                break;
-        
-            case 'super admin':
-                goto('/super-admin/halaman-utama');
-                break;
-        
-            case 'timbalan ketua seksyen':
-                goto('/timbalan-ketua-seksyen/halaman-utama');
-                break;
-        
-            case 'unit bahagian/negeri':
-                goto('/kakitangan/halaman-utama');
-                break;
-        
-            case 'unit pengurusan fasiliti':
-                goto('/kakitangan/halaman-utama');
-                break;
-        
-            case 'urus setia cuti':
-                goto('/urus-setia/halaman-utama');
-                break;
-        
-            case 'urus setia elaun-elaun perkhidmatan':
-                goto('/urus-setia/halaman-utama');
-                break;
-        
-            case 'urus setia gaji':
-                goto('/urus-setia/halaman-utama');
-                break;
-        
-            case 'urus setia integriti':
-                goto('/urus-setia/halaman-utama');
-                break;
-        
-            case 'urus setia kakitangan kontrak':
-                goto('/urus-setia/halaman-utama');
-                break;
-        
-            case 'urus setia latihan':
-                goto('/urus-setia/halaman-utama');
-                break;
-        
-            case 'urus setia lnpt':
-                goto('/urus-setia/halaman-utama');
-                break;
-        
-            case 'urus setia perjawatan':
-                goto('/urus-setia/halaman-utama');
-                break;
-        
-            case 'urus setia persaraan':
-                goto('/urus-setia/halaman-utama');
-                break;
-        
-            case 'urus setia perubatan':
-                goto('/urus-setia/halaman-utama');
-                break;
-        
-            case 'urus setia pinjaman & kuarters':
-                goto('/urus-setia/halaman-utama');
-                break;
-        
-            default:
-                break;
-        }
+                    case 'audit':
+                        goto('/kakitangan/halaman-utama');
+                        break;
 
+                    case 'kakitangan':
+                        goto('/kakitangan/halaman-utama');
+                        break;
+
+                    case 'ketua pengarah':
+                        goto('/ketua-pengarah/halaman-utama');
+                        break;
+
+                    case 'ketua seksyen':
+                        goto('/ketua-seksyen/halaman-utama');
+                        break;
+
+                    case 'klinik panel':
+                        goto('/kakitangan/halaman-utama');
+                        break;
+
+                    case 'pelulus':
+                        goto('/pelulus/halaman-utama');
+                        break;
+
+                    case 'pengarah audit':
+                        goto('/pengarah-audit/halaman-utama');
+                        break;
+
+                    case 'pengarah bahagian/negeri':
+                        goto('/pengarah-bahagian-negeri/halaman-utama');
+                        break;
+
+                    case 'penyokong':
+                        goto('/penyokong/halaman-utama');
+                        break;
+
+                    case 'super admin':
+                        goto('/super-admin/halaman-utama');
+                        break;
+
+                    case 'timbalan ketua seksyen':
+                        goto('/timbalan-ketua-seksyen/halaman-utama');
+                        break;
+
+                    case 'unit bahagian/negeri':
+                        goto('/kakitangan/halaman-utama');
+                        break;
+
+                    case 'unit pengurusan fasiliti':
+                        goto('/kakitangan/halaman-utama');
+                        break;
+
+                    case 'urus setia cuti':
+                        goto('/urus-setia/halaman-utama');
+                        break;
+
+                    case 'urus setia elaun-elaun perkhidmatan':
+                        goto('/urus-setia/halaman-utama');
+                        break;
+
+                    case 'urus setia gaji':
+                        goto('/urus-setia/halaman-utama');
+                        break;
+
+                    case 'urus setia integriti':
+                        goto('/urus-setia/halaman-utama');
+                        break;
+
+                    case 'urus setia kakitangan kontrak':
+                        goto('/urus-setia/halaman-utama');
+                        break;
+
+                    case 'urus setia latihan':
+                        goto('/urus-setia/halaman-utama');
+                        break;
+
+                    case 'urus setia lnpt':
+                        goto('/urus-setia/halaman-utama');
+                        break;
+
+                    case 'urus setia perjawatan':
+                        goto('/urus-setia/halaman-utama');
+                        break;
+
+                    case 'urus setia persaraan':
+                        goto('/urus-setia/halaman-utama');
+                        break;
+
+                    case 'urus setia perubatan':
+                        goto('/urus-setia/halaman-utama');
+                        break;
+
+                    case 'urus setia pinjaman & kuarters':
+                        goto('/urus-setia/halaman-utama');
+                        break;
+
+                    default:
+                        break;
+                }
+            }, 2000),
+        );
     } else {
         getLoginErrorToast();
     }
