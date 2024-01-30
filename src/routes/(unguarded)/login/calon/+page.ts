cceec3b9065228bd7b07f1152e87158425cf3405
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-01-30 17:41:35 +0800

    feat: adds loading toast.

--- src/routes/(unguarded)/login/calon/+page.ts
+++ src/routes/(unguarded)/login/calon/+page.ts
@@ -1,11 +1,13 @@
 import { goto } from '$app/navigation';
 import {
+    getLoadingToast,
     getLoginErrorToast,
     getLoginSuccessToast,
 } from '$lib/services/core/toast/toast-service';
 import { AuthService } from '$lib/services/implementations/core/auth/authentication.service';
 import type { AuthenticationRequestViewModel } from '$lib/view-models/core/auth/auth-request.view-model';
 import type { AuthenticationResponseViewModel } from '$lib/view-models/core/auth/auth-response.view-model';
+import toast from 'svelte-french-toast';
 import { superValidate } from 'sveltekit-superforms/client';
 import { z } from 'zod';
 
@@ -24,8 +26,12 @@ export const load = async () => {
 };
 
 export const _submit = async (formData: AuthenticationRequestViewModel) => {
+    getLoadingToast();
     const response: AuthenticationResponseViewModel =
-        await AuthService.loginCandidate(formData);
+        await AuthService.loginCandidate(formData).finally(() =>
+            toast.dismiss(),
+        );
+
     if (response.status === 200) {
         getLoginSuccessToast().finally(() =>
             setTimeout(() => goto('/calon/halaman-utama'), 2000),
