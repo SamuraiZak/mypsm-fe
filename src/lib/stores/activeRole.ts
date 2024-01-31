c26ad2a14cb2b49e218d6d6c0a270a06314280f1
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-01-31 11:29:59 +0800

    feat: modified store subscribe

--- src/lib/stores/activeRole.ts
+++ src/lib/stores/activeRole.ts
@@ -1,17 +1,18 @@
-import { browser } from '$app/environment';
+
+import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key-constant';
 import { writable } from 'svelte/store';
 
 const defaultValue = 'kakitangan';
-const initialValue = browser
-    ? window.localStorage.getItem('currentRole') ?? defaultValue
-    : defaultValue;
+// const initialValue = browser
+//     ? window.localStorage.getItem('currentRole') ?? defaultValue
+//     : defaultValue;
+
+const initialValue = localStorage.getItem(LocalStorageKeyConstant.currentRole)??defaultValue;
 
 const currentRole = writable<string>(initialValue);
 
 currentRole.subscribe((value) => {
-    if (browser) {
-        window.localStorage.setItem('currentRole', value);
-    }
+    localStorage.setItem(LocalStorageKeyConstant.currentRole, value);
 });
 
 export default currentRole;
