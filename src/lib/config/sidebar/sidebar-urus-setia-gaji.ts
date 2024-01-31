fde527fc8d63856f0ddaf3eb475c84c284d6fc37
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-01-31 11:24:42 +0800

    feat: added specific sidebar for each role

---
+++ src/lib/config/sidebar/sidebar-urus-setia-gaji.ts
@@ -0,0 +1,28 @@
+export const sidebarUrusSetiaGaji = [
+    {
+        name: 'Halaman Utama',
+        redirect: '/urus-setia/halaman-utama',
+    },
+    {
+        name: 'Gaji',
+        dropdown: [
+            {
+                name: 'Pergerakan Gaji',
+                redirect: '/urus-setia/gaji/pergerakan-gaji',
+            },
+            {
+                name: 'Gaji dan Elaun-elaun',
+                redirect: '/urus-setia/gaji/gaji-elaun',
+            },
+            {
+                name: 'Gaji Akhir',
+                redirect: '/urus-setia/gaji/gaji-akhir',
+            },
+            {
+                name: 'Sijil Gaji Akhir',
+                redirect: '/urus-setia/gaji/sijil-gaji-akhir',
+            },
+        ],
+    },
+    
+];
---
+++ src/lib/config/sidebar/sidebar-urus-setia-kakitangan-kontrak.ts
@@ -0,0 +1,19 @@
+export const sidebarUrusSetiaKakitanganKontrak = [
+    {
+        name: 'Halaman Utama',
+        redirect: '/urus-setia/halaman-utama',
+    },
+    {
+        name: 'Kakitangan Kontrak',
+        dropdown: [
+            {
+                name: 'Perlantikan Baru (Kontrak)',
+                redirect: '/urus-setia/kakitangan-kontrak/perlantikan',
+            },
+            {
+                name: 'Pembaharuan Kontrak',
+                redirect: '/urus-setia/kakitangan-kontrak/pembaharuan',
+            },
+        ],
+    },
+];
