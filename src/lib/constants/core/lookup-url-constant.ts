888b154cf47297badea4fc310fd7041628a91c0f
LAPTOP-9F2VGD83\Amiz <hamiznaziff@gmail.com>
2024-02-01 13:27:40 +0800

    feat: add new lookup URL

---
+++ src/lib/constants/core/lookup-url-constant.ts
@@ -0,0 +1,37 @@
+export class LookupConstantURL{
+    public static  agencyGroup = "lookups/agency-groups";
+    public static assestDeclarationStatus ="lookups/asset-declaration-statuses";
+    public static awardCategory ="lookups/award-categories";
+    public static award ="lookups/awards";
+    public static city ="lookups/cities";
+    public static country ="lookups/countries";
+    public static employmentStatus ="lookups/employment-statuses";
+    public static ethnicity ="lookups/ethnicities";
+    public static gender ="lookups/genders";
+    public static highestEducationLevel ="lookups/highest-education-levels";
+    public static institution ="lookups/institutions";
+    public static majorMinor ="lookups/major-minors";
+    public static maritalStatus ="lookups/marital-statuses";
+    public static nationality ="lookups/nationalities";
+    public static race ="lookups/races";
+    public static relationship ="lookups/relationships";
+    public static religion ="lookups/religions";
+    public static schemeOfService ="lookups/scheme-of-services";
+    public static serviceClass ="lookups/service-classes";
+    public static sponsorship ="lookups/sponsorships";
+    public static state ="lookups/states";
+    public static title ="lookups/titles";
+    public static role ="lookups/roles";
+    public static serviceGroup ="lookups/service-groups";
+    public static serviceType ="lookups/service-types";
+    public static grade ="lookups/grades";
+    public static position ="lookups/positions";
+    public static section ="lookups/sections";
+    public static unit ="lookups/units";
+    public static division ="lookups/divisions";
+    public static district ="lookups/districts";
+    public static department ="lookups/departments";
+    public static placement ="lookups/placements";
+
+
+}
