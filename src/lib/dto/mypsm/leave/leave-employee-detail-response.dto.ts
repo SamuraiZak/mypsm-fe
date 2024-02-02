117be51156fd3a0bd97cf7bee4fed6875cb315ae
calvin <calvin.cliff@impact-multimedia.com>
2024-02-03 02:41:23 +0800

    feat: creates leave employee detail response dto

---
+++ src/lib/dto/mypsm/leave/leave-employee-detail-response.dto.ts
@@ -0,0 +1,9 @@
+export interface LeaveEmployeeDetailsResponse {
+    employeeNumber: string;
+    name: string;
+    identityCardNumber: string;
+    grade: null;
+    position: null;
+    placement: null;
+    serviceGroup: null;
+}
