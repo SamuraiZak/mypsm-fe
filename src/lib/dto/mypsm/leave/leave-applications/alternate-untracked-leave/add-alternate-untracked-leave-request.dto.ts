fa662d0add5553bb9a35634584f6fe24d03fdc4a
calvin <calvin.cliff@impact-multimedia.com>
2024-02-03 02:42:48 +0800

    fixx: rename files

--- src/lib/dto/mypsm/leave/leave-applications/alternate-untracked-leave/add-alternate-untracked-leave-request.dto.ts
+++
@@ -1,6 +0,0 @@
-export interface AddAlternateUntrackedLeaveRequest {
-    untrackedLeaveType: string;
-    reason: string;
-    startDate: Date;
-    endDate: Date;
-}
--- src/lib/dto/mypsm/leave/leave-applications/alternate-untracked-leave/get-alternate-untracked-leave-respone.dto.ts
+++
@@ -1,8 +0,0 @@
-export interface AlternateUntrackedLeaveRequest {
-    alternateUntrackedId: number;
-    untrackedLeaveType: string;
-    reason: string;
-    startDate: Date;
-    endDate: Date;
-    durationDays: number;
-}
