9a71bddb6dac8f3dfc6bedc18caeff17b1cdb953
calvin <calvin.cliff@impact-multimedia.com>
2024-02-04 05:17:33 +0800

    fix: uses the correct modfel

--- src/routes/(guarded)/urus-setia/cuti/kelulusan-cuti/+page.svelte
+++ src/routes/(guarded)/urus-setia/cuti/kelulusan-cuti/+page.svelte
@@ -6,6 +6,10 @@
     import { tahun } from '$lib/mocks/ketua-seksyen/cuti/tahun';
     import type { PageData } from './$types';
     import { goto } from '$app/navigation';
+    import FilterContainer from '$lib/components/filter-container/FilterContainer.svelte';
+    import StaffSelector from '$lib/components/staff-selector/StaffSelector.svelte';
+    import { Select } from 'flowbite-svelte';
+    import ShortTextField from '$lib/components/input/ShortTextField.svelte';
 
     export let data: PageData;
     let selectedTahun = tahun[0].value;
@@ -23,6 +27,97 @@
 <section
     class="flex h-full w-full flex-col items-center justify-start gap-2.5 overflow-y-auto p-2.5"
 >
+    <FilterContainer>
+        <StaffSelector />
+        <div class="flex flex-row gap-x-2.5">
+            <div class="flex flex-row items-center justify-center gap-x-2.5">
+                <p class="text-sm font-normal">Bulan Cuti</p>
+                <div class="flex flex-row items-center justify-start gap-x-2.5">
+                    <Select
+                        id="bulan-cuti-bulan"
+                        items={[
+                            {
+                                value: 1,
+                                name: 'January',
+                            },
+                            {
+                                value: 2,
+                                name: 'Febrary',
+                            },
+                            {
+                                value: 3,
+                                name: 'March',
+                            },
+                            {
+                                value: 4,
+                                name: 'April',
+                            },
+                            {
+                                value: 5,
+                                name: 'May',
+                            },
+                            {
+                                value: 6,
+                                name: 'June',
+                            },
+                            {
+                                value: 7,
+                                name: 'July',
+                            },
+                            {
+                                value: 8,
+                                name: 'August',
+                            },
+                            {
+                                value: 9,
+                                name: 'September',
+                            },
+                            {
+                                value: 10,
+                                name: 'October',
+                            },
+                            {
+                                value: 11,
+                                name: 'November',
+                            },
+                            {
+                                value: 12,
+                                name: 'December',
+                            },
+                        ]}
+                        placeholder="Bulan"
+                        class="block h-[32px] w-fit rounded-[3px] border border-bdr-primary bg-bgr-primary px-2.5 py-0 text-[12px] text-txt-primary hover:cursor-pointer hover:border-system-primary focus:border-system-primary focus:outline-none focus:ring-0 disabled:cursor-not-allowed disabled:bg-bgr-secondary disabled:text-txt-secondary"
+                        value={''}
+                    ></Select>
+                    <Select
+                        id="bulan-cuti-tahun"
+                        items={[
+                            {
+                                value: 1,
+                                name: '2020',
+                            },
+                            {
+                                value: 2,
+                                name: '2021',
+                            },
+                            {
+                                value: 3,
+                                name: '2022',
+                            },
+                            {
+                                value: 4,
+                                name: '2023',
+                            },
+                        ]}
+                        placeholder="Tahun"
+                        class="block h-[32px] w-fit rounded-[3px] border border-bdr-primary bg-bgr-primary px-2.5 py-0 text-[12px] text-txt-primary hover:cursor-pointer hover:border-system-primary focus:border-system-primary focus:outline-none focus:ring-0 disabled:cursor-not-allowed disabled:bg-bgr-secondary disabled:text-txt-secondary"
+                        value={''}
+                    ></Select>
+                </div>
+            </div>
+        </div>
+        <ShortTextField label="Kod Jenis Cuti" type="text" />
+    </FilterContainer>
 
     <SectionHeader title="Senarai Cuti Yang Telah Diambil Mengikut Tahun"
         ><DropdownSelect
@@ -34,14 +129,13 @@
         ></DropdownSelect>
     </SectionHeader>
     <div class="flex max-h-full w-full flex-col items-start justify-start">
-        <DynamicTable tableItems={data.leaveHistoryList ?? undefined}
-        withActions
-        actionOptions={['edit']}
-        editActions={() => {
-            goto(
-                '/urus-setia/cuti/kelulusan-cuti/butiran-kelulusan-cuti',
-            );
-        }}
+        <DynamicTable
+            tableItems={data.leaveHistoryList ?? undefined}
+            withActions
+            actionOptions={['edit']}
+            editActions={() => {
+                goto('/urus-setia/cuti/kelulusan-cuti/butiran-kelulusan-cuti');
+            }}
         ></DynamicTable>
     </div>
 </section>
