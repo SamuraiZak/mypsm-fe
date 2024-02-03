9afc768d8b97dfe236a52619de421e2cbc26f879
calvin <calvin.cliff@impact-multimedia.com>
2024-02-04 04:35:02 +0800

    feat: creates leave entitlement and history dto

---
+++ src/lib/dto/mypsm/leave/report-leave/leave-entitlement-list-response.dto.ts
@@ -0,0 +1,8 @@
+export interface LeaveEntitlementListResponse {
+    leaveType: string;
+    entitled: string;
+    carryForward: string;
+    totalEntitled: string;
+    leaveTaken: string;
+    leaveBalance: string;
+}
---
+++ src/lib/dto/mypsm/leave/report-leave/leave-history-list-response.dto.ts
@@ -0,0 +1,9 @@
+export interface LeaveHistoryListResponse {
+    employeeName: string;
+    leaveType: string;
+    leaveFrom: Date;
+    leaveTo: Date;
+    leaveTotal: number;
+    status: null;
+    reason: null;
+}
