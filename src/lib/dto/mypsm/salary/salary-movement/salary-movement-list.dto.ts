4ff359c29f5103edf47b43877adb6b1d74de14f8
Holifield William <holifield.william@impactbs.my>
2024-02-03 11:54:54 +0800

    feat: added salary-movement-list.dto

---
+++ src/lib/dto/mypsm/salary/salary-movement/salary-movement-list.dto.ts
@@ -0,0 +1,33 @@
+export interface SalaryMovementListDTO {
+    meetingId:               string;
+    meetingName:             string;
+    meetingDate:             Date;
+    salaryMovementMonth:     number;
+    specialAid:              number;
+    specialRaiseType:        string;
+    specialRaise:            number;
+    employeeName:            string;
+    employeeNumber:          string;
+    employeeGrade:           string;
+    tpg:                     number;
+    kgt:                     null;
+    salary1:                 number;
+    wilayahAllowance1:       number;
+    criticalAllowance1:      number;
+    salary2:                 number;
+    wilayahAllowance2:       number;
+    criticalAllowance2:      number;
+    specialkgt:              number;
+    specialSalary:           number;
+    specialWilayahAllowance: number;
+}
+
+export class SalaryMovementListDTOConvert {
+    public static toListSalaryMovementRequest(json: string): SalaryMovementListDTO {
+        return JSON.parse(json);
+    }
+
+    public static listSalaryMovementRequestToJson(value: SalaryMovementListDTO): string {
+        return JSON.stringify(value);
+    }
+}
