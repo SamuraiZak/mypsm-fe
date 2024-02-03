8075436bdc1627f0ec551f2fc0043bab65efc610
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-03 12:25:10 +0800

    feat: WIP setting page

---
+++ src/routes/(guarded)/tetapan/+layout.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+    import QuickLinkButton from '$lib/components/buttons/QuickLinkButton.svelte';
+    import NotificationCard from '$lib/components/cards/NotificationCard.svelte';
+    import ContentHeader from '$lib/components/content-header/ContentHeader.svelte';
+
+    let notificationList = [
+        {
+            subject: 'Menantikan kelulusan anda',
+            title: 'Elaun Mengunjungi Wilayah Asal',
+            applicant: 'Abdul Khalid',
+            duration: '2 hari yang lalu',
+        },
+        {
+            subject: 'Menantikan kelulusan anda',
+            title: 'Elaun Mengunjungi Wilayah Asal',
+            applicant: 'Abdul Khalid',
+            duration: '2 hari yang lalu',
+        },
+        {
+            subject: 'Menantikan kelulusan anda',
+            title: 'Elaun Mengunjungi Wilayah Asal',
+            applicant: 'Abdul Khalid',
+            duration: '2 hari yang lalu',
+        },
+        {
+            subject: 'Menantikan kelulusan anda',
+            title: 'Elaun Mengunjungi Wilayah Asal',
+            applicant: 'Abdul Khalid',
+            duration: '2 hari yang lalu',
+        },
+        {
+            subject: 'Menantikan kelulusan anda',
+            title: 'Elaun Mengunjungi Wilayah Asal',
+            applicant: 'Abdul Khalid',
+            duration: '2 hari yang lalu',
+        },
+        {
+            subject: 'Menantikan kelulusan anda',
+            title: 'Elaun Mengunjungi Wilayah Asal',
+            applicant: 'Abdul Khalid',
+            duration: '2 hari yang lalu',
+        },
+        {
+            subject: 'Menantikan kelulusan anda',
+            title: 'Elaun Mengunjungi Wilayah Asal',
+            applicant: 'Abdul Khalid',
+            duration: '2 hari yang lalu',
+        },
+        {
+            subject: 'Menantikan kelulusan anda',
+            title: 'Elaun Mengunjungi Wilayah Asal',
+            applicant: 'Abdul Khalid',
+            duration: '2 hari yang lalu',
+        },
+        {
+            subject: 'Menantikan kelulusan anda',
+            title: 'Elaun Mengunjungi Wilayah Asal',
+            applicant: 'Abdul Khalid',
+            duration: '2 hari yang lalu',
+        },
+    ];
+</script>
+
+<!-- content header starts here -->
+<section class="flex w-full flex-col items-start justify-start">
+    <ContentHeader
+        title="Halaman Tetapan"
+        description="Uruskan tetapan untuk akaun anda"
+    ></ContentHeader>
+</section>
+
+<!-- content body starts here -->
+<!-- notes: you may change the flex directions but do not change the height and max height of this section -->
+<section
+    class="flex h-full w-full flex-col items-start justify-start overflow-y-auto"
+>
+    <div class="flex h-full w-full flex-row items-start justify-start">
+        <div
+            class="flex h-full w-full flex-col items-center justify-start overflow-y-auto"
+        >
+            <div
+                class="flex h-full w-full max-w-[600px] flex-col items-center justify-start"
+            >
+                <slot />
+            </div>
+        </div>
+    </div>
+</section>
---
+++ src/routes/(guarded)/tetapan/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import SvgLockRotate from '$lib/assets/svg/SvgLockRotate.svelte';
+
+    const navigateTo = async (url: string) => {
+        goto(url);
+    };
+</script>
+
+<div class="flex h-fit w-full flex-row items-center justify-start px-2 py-5">
+    <span class="text-xl font-semibold leading-loose"> Tetapan Akaun </span>
+</div>
+
+<div class="flex h-full w-full flex-col justify-start space-y-4">
+    <div class="h-fit w-full p-2">
+        <span class="text-base">
+            Sila pilih tetapan yand anda ingin kemaskini
+        </span>
+    </div>
+
+    <div
+        class="flex h-full w-full flex-col items-start justify-start space-y-4 overflow-y-auto"
+    >
+        <button
+            on:click={() => {
+                navigateTo('tetapan/kemaskini-kata-laluan');
+            }}
+            class="flex h-8 w-80 flex-row rounded-md bg-ios-systemColors-quaternarySystemFill-light font-normal text-ios-labelColors-label-light hover:bg-ios-systemColors-tertiarySystemFill-light hover:text-ios-labelColors-link-light"
+        >
+            <div>
+                <span class="flex flex-row justify-start p-2 text-base">
+                    <SvgLockRotate></SvgLockRotate>
+                </span>
+            </div>
+            <span class="flex flex-row justify-start p-2 text-base">
+                Kemaskini Kata Laluan
+            </span>
+        </button>
+    </div>
+</div>
