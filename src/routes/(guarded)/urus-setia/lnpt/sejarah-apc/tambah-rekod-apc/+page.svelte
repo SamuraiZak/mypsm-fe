2d1722d60465190238fc6fd05ab780f5d01f92e6
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-04 12:53:07 +0800

    feat: WIP lnpt pages

--- src/routes/(guarded)/urus-setia/lnpt/sejarah-apc/tambah-rekod-apc/+page.svelte
+++ src/routes/(guarded)/urus-setia/lnpt/sejarah-apc/tambah-rekod-apc/+page.svelte
@@ -1,27 +1,47 @@
 <script lang="ts">
-    import { goto } from '$app/navigation';
     import SvgArrowLeft from '$lib/assets/svg/SvgArrowLeft.svelte';
-    import SvgCalc from '$lib/assets/svg/SvgCalc.svelte';
-    import SvgCheck from '$lib/assets/svg/SvgCheck.svelte';
+
     import TextIconButton from '$lib/components/buttons/TextIconButton.svelte';
     import ContentHeader from '$lib/components/content-header/ContentHeader.svelte';
-    import FilterCard from '$lib/components/filter/FilterCard.svelte';
     import SectionHeader from '$lib/components/header/SectionHeader.svelte';
-    import DropdownSelect from '$lib/components/input/DropdownSelect.svelte';
-    import TextField from '$lib/components/input/TextField.svelte';
-    import StaffSelector from '$lib/components/staff-selector/StaffSelector.svelte';
-    import DynamicTable from '$lib/components/table/DynamicTable.svelte';
-    import { yearsOfService } from '$lib/config/lnptYearsOfService.js';
-    import { years } from '$lib/mocks/dateSelection/years';
-    import { greds } from '$lib/mocks/gred/gred.js';
-    import { positions } from '$lib/mocks/positions/positions.js';
-    import { Checkbox, Radio, Tooltip } from 'flowbite-svelte';
+    import CommonTable from '$lib/components/table/CommonTable.svelte';
+    import CustomTable from '$lib/components/table/CustomTable.svelte';
+    import type { CommonListRequestDTO } from '$lib/dto/core/common/common-list-request.dto.js';
+    import type { TableDTO } from '$lib/dto/core/table/table.dto';
+    import type { CommonEmployeeDTO } from '$lib/dto/mypsm/common/employee/employee.dto.js';
+    import type { PageData } from './$types';
+    import { _updateTable } from './+page';
 
-    export let data;
-    let selectedRow: IntLnptAverage;
-    let selectedGred = '-';
-    let selectedPosition = '-';
-    let selectedYearsOfService = '-';
+    export let data: PageData;
+
+    let employeeList: CommonEmployeeDTO[] = data.props.employeeList;
+
+    let param: CommonListRequestDTO = data.props.param;
+
+    let table: TableDTO = {
+        param: param,
+        meta: data.props.response.data?.meta ?? {
+            pageSize: 1,
+            pageNum: 1,
+            totalData: 1,
+            totalPage: 1,
+        },
+        data: data.props.response.data?.dataList ?? [],
+    };
+
+    async function _search() {
+        _updateTable(table.param).then((value) => {
+            table.data = value.props.response.data?.dataList ?? [];
+            table.meta = value.props.response.data?.meta ?? {
+                pageSize: 1,
+                pageNum: 1,
+                totalData: 1,
+                totalPage: 1,
+            };
+            table.param.pageSize = table.meta.pageSize;
+            table.param.pageNum = table.meta.pageNum;
+        });
+    }
 </script>
 
 <!-- content header starts here -->
@@ -32,7 +52,8 @@
         ><TextIconButton
             label="Kembali"
             onClick={() => {
-                goto('../sejarah-apc');
+                // goto('../sejarah-apc');
+                _search();
             }}
         >
             <SvgArrowLeft />
@@ -49,50 +70,16 @@
         class="flex h-full w-full flex-col items-center justify-start gap-2.5 p-2.5"
     >
         <!-- Table filter placeholder -->
-        <FilterCard>
-            <StaffSelector selectorName="staff-lists" />
-            <!-- Select dropdowns -->
-            <DropdownSelect
-                id="gred-dropdown"
-                label="Gred Semasa"
-                dropdownType="label-top"
-                bind:index={selectedGred}
-                options={greds}
-            ></DropdownSelect>
-            <DropdownSelect
-                id="position-dropdown"
-                label="Jawatan"
-                dropdownType="label-top"
-                bind:index={selectedPosition}
-                options={positions}
-            ></DropdownSelect>
-            <DropdownSelect
-                id="years-of-Service-dropdown"
-                label="Tahun Berkhidmat"
-                dropdownType="label-top"
-                bind:index={selectedYearsOfService}
-                options={yearsOfService}
-            ></DropdownSelect>
-        </FilterCard>
-        <div class="flex max-h-full w-full flex-col items-start justify-start">
-            <SectionHeader
-                title="Senarai Kakitangan Untuk Dikemaskini (Maklumat APC)"
-            ></SectionHeader>
-            <DynamicTable
-                tableItems={data.records.LnptApcRecord}
-                bind:passData={selectedRow}
-                withActions
-                actionOptions={['edit']}
-                editActions={() => {
-                    const url =
-                        './tambah-rekod-apc/butiran-' +
-                        selectedRow.employeeNumber +
-                        '-' +
-                        selectedRow.identityDocumentNumber;
 
-                    goto(url);
+        <div class="flex max-h-full w-full flex-col items-start justify-start">
+            <SectionHeader title="Senarai Semua Kakitangan"></SectionHeader>
+            <CustomTable
+                bind:tableData={table}
+                onUpdate={_search}
+                detailActions={() => {
+                    alert('hello');
                 }}
-            ></DynamicTable>
+            ></CustomTable>
         </div>
     </div>
 </section>
