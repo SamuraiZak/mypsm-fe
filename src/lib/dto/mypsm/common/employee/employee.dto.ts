e38fc62d03d49cf649e1595de0ff250a9477d767
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-04 12:48:13 +0800

    feat: Added dto for common employee list

---
+++ src/lib/dto/mypsm/common/employee/employee.dto.ts
@@ -0,0 +1,21 @@
+export interface CommonEmployeeDTO {
+    employeeNumber: string;
+    name:           string;
+    identityCard:   string;
+    program:        string;
+    scheme:         string;
+    grade:          string;
+    position:       string;
+    placement:      string;
+}
+
+// Converts JSON strings to/from your types
+export class CommonEmployeeConvert {
+    public static fromJson(json: string): CommonEmployeeDTO {
+        return JSON.parse(json);
+    }
+
+    public static toJson(value: CommonEmployeeDTO): string {
+        return JSON.stringify(value);
+    }
+}
