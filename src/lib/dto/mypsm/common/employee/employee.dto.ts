3b077f2ddea12261f8236e59a2183997a918eceb
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-04 18:46:25 +0800

    feat: Modified dto for employee

--- src/lib/dto/mypsm/common/employee/employee.dto.ts
+++ src/lib/dto/mypsm/common/employee/employee.dto.ts
@@ -1,12 +1,12 @@
 export interface CommonEmployeeDTO {
-    employeeNumber: string;
-    name:           string;
-    identityCard:   string;
-    program:        string;
-    scheme:         string;
-    grade:          string;
-    position:       string;
-    placement:      string;
+    employeeNumber?: string;
+    name?:           string;
+    identityCard?:   string;
+    program?:        string;
+    scheme?:         string;
+    grade?:          string;
+    position?:       string;
+    placement?:      string;
 }
 
 // Converts JSON strings to/from your types
