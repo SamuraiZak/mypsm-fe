2c9df60d586e2018cb1e37d64e82622e01f3f051
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-04 18:47:30 +0800

    Feat: Added lnpt services

--- src/lib/services/implementations/mypsm/lnpt/lnpt.service.ts
+++ src/lib/services/implementations/mypsm/lnpt/lnpt.service.ts
@@ -7,6 +7,10 @@ import {
     CommonResponseConvert,
     type CommonResponseDTO,
 } from '$lib/dto/core/common/common-response.dto';
+import {
+    AddAPCRequestConvert,
+    type AddAPCRequestDTO,
+} from '$lib/dto/mypsm/lnpt/apc/add-apc-request.dto';
 import type { ApcEmployeeDTO } from '$lib/dto/mypsm/lnpt/apc/apc-employee.dto';
 import http from '$lib/services/provider/service-provider.service';
 import type { Input } from 'ky';
@@ -28,6 +32,35 @@ export class LNPTServices {
             if (result.status == 'success') {
                 return result;
             } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    static async addAPCRecord(param: AddAPCRequestDTO) {
+        try {
+            // Change the url here
+            let url: Input = 'performance/add-performance-history-apc';
+
+            const response: Response = await http
+                .post(url, {
+                    body: AddAPCRequestConvert.addAPCRequestDTOToJson(param),
+                })
+                .json();
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                // Add your code here
+
+                // change the return variable accordingly
+                return result;
+            } else {
+                // Add your code here
+
+                // change the return variable accordingly
                 return CommonResponseConstant.httpError;
             }
         } catch (error) {
