f58f2dd8a7f7009ea0f465c17f96bd748d7ee094
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-06 08:59:03 +0800

    feat: Added sidebar for unit bahagian

---
+++ src/lib/config/sidebar/sidebar-unit-bahagian.ts
@@ -0,0 +1,15 @@
+export const sidebarUnitBahagian = [
+    {
+        name: 'Halaman Utama',
+        redirect: '/unit-bahagian/halaman-utama',
+    },
+    {
+        name: 'Perubatan',
+        dropdown: [
+            {
+                name: 'Perkhidmatan (Permohonan Panel Klinik)',
+                redirect: '/unit-bahagian/perubatan/permohonan-panel-klinik',
+            },
+        ],
+    },
+];
