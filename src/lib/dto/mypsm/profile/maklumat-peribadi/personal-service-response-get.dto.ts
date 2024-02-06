64d9c71ad81dcab1c698fde72495aba6f2acda21
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-02-07 06:13:36 +0800

    feat: adds dto

--- src/lib/dto/mypsm/profile/maklumat-peribadi/personal-service-response-get.dto.ts
+++ src/lib/dto/mypsm/profile/maklumat-peribadi/personal-service-response-get.dto.ts
@@ -1,45 +1,48 @@
 export interface GetPersonalServiceResponse {
-    currentGrade:          string;
-    currentPosition:       string;
-    placement:             string;
-    serviceType:           string;
-    effectiveDate:         Date;
-    retirementBenefit:     string;
-    EPFNumber:             string;
-    SOCSO:                 string;
-    taxIncome:             string;
-    bankName:              string;
-    accountNumber:         string;
-    program:               string;
-    eligibleLeaveCount:    number;
+    currentGrade: string;
+    currentPosition: string;
+    placement: string;
+    serviceType: string;
+    effectiveDate: Date;
+    retirementBenefit: string;
+    EPFNumber: string;
+    SOCSO: string;
+    taxIncome: string;
+    bankName: string;
+    accountNumber: string;
+    program: string;
+    eligibleLeaveCount: number;
     civilServiceStartDate: Date;
-    confirmServiceDate:    Date;
-    historyList:           any[];
-    firstEffectiveDate:    Date;
-    pastAttachmentDate:    null;
-    actingDate:            Date;
-    interimDate:           Date;
-    pensionScheme:         string;
-    lastSalaryRaiseDate:   Date;
-    lastPromotionDate:     Date;
-    salaryMovementMonth:   number;
-    retirementDate:        Date;
-    salaryEffectiveDate:   Date;
-    maximumSalary:         number;
-    baseSalary:            number;
-    ITKA:                  number;
-    ITP:                   number;
-    EPW:                   number;
-    COLA:                  number;
+    confirmServiceDate: Date;
+    firstEffectiveDate: Date;
+    pastAttachmentDate: Date;
+    actingDate: Date;
+    interimDate: Date;
+    pensionScheme: string;
+    lastSalaryRaiseDate: Date;
+    lastPromotionDate: Date;
+    salaryMovementMonth: number;
+    retirementDate: Date;
+    salaryEffectiveDate: Date;
+    maximumSalary: number;
+    baseSalary: number;
+    ITKA: number;
+    ITP: number;
+    EPW: number;
+    COLA: number;
 }
 
 // Converts JSON strings to/from your types
 export class Convert {
-    public static toGetPersonalService(json: string): GetPersonalServiceResponse {
+    public static toGetPersonalService(
+        json: string,
+    ): GetPersonalServiceResponse {
         return JSON.parse(json);
     }
 
-    public static getPersonalServiceToJson(value: GetPersonalServiceResponse): string {
+    public static getPersonalServiceToJson(
+        value: GetPersonalServiceResponse,
+    ): string {
         return JSON.stringify(value);
     }
 }
