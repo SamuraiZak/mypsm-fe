689583c7432faff0026d5536ad777a7a21efe8fc
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-08 18:42:57 +0800

    feat: Added service provider

---
+++ src/lib/services/service-provider.service.ts
@@ -0,0 +1,33 @@
+// ===============================================================
+// Service Provider
+// ===============================================================
+
+import { env } from '$env/dynamic/public';
+import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key-constant';
+
+import ky from 'ky';
+
+const http = ky.create({
+    prefixUrl: env.PUBLIC_BASE_API_URL,
+    headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json',
+    },
+    timeout: 25000,
+    credentials: 'include',
+    hooks: {
+        beforeRequest: [
+            (request) => {
+                const token = localStorage.getItem(
+                    LocalStorageKeyConstant.accessToken,
+                );
+
+                if (token != null) {
+                    request.headers.set('Authorization', `${token}`);
+                }
+            },
+        ],
+    },
+});
+
+export default http;
