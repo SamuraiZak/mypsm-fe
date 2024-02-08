68e9e2f221bdd634c46c906e86b11f477cb5d0e3
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-08 17:32:04 +0800

    chore!: New structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,35 +1,6 @@
 <script lang="ts">
-    import LoadingOverlay from '$lib/components/loading-overlay/LoadingOverlay.svelte';
     import '../app.css';
-    import { navigating } from '$app/stores';
-    import { afterUpdate } from 'svelte';
-    import { loadingState } from '$lib/stores/globalState';
 
-    let showLoadingOverlay: boolean = false;
-
-    afterUpdate(() => {
-        // Set a timeout to show the LoadingOverlay if still navigating after 200ms
-        const timeoutId = setTimeout(() => {
-            if ($navigating) {
-                showLoadingOverlay = true;
-            }
-        }, 200);
-
-        if ($navigating === null) showLoadingOverlay = false;
-
-        // Clear the timeout when loaded
-        return () => clearTimeout(timeoutId);
-    });
-
-    $: loading = loadingState;
 </script>
-
-{#if $navigating && showLoadingOverlay}
-    <LoadingOverlay />
-{/if}
-
-{#if $loading}
-    <LoadingOverlay></LoadingOverlay>
-{/if}
 
 <slot />
