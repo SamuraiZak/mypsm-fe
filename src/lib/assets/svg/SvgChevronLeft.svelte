99481511065ceb8403559bcc7e7d68419a912515
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-14 19:16:31 +0800

    feat: Added svg as components

---
+++ src/lib/assets/svg/SvgChevronLeft.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    export let size: string = '16';
+</script>
+
+<svg
+    viewBox="0 0 512 512"
+    version="1.1"
+    width={size}
+    height={size}
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    fill="currentColor"
+    ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    ></g><g id="SVGRepo_iconCarrier">
+        <title>chevron-left</title>
+        <g
+            id="Page-1"
+            stroke="none"
+            stroke-width="1"
+            fill="none"
+            fill-rule="evenodd"
+        >
+            <g
+                id="add"
+                fill="currentColor"
+                transform="translate(157.440000, 80.853333)"
+            >
+                <polygon
+                    id="Path"
+                    points="205.254834 320 175.084945 350.169889 -5.68434189e-14 175.084945 175.084945 0 205.254834 30.1698893 60.356556 175.084945"
+                >
+                </polygon>
+            </g>
+        </g>
+    </g></svg
+>
---
+++ src/lib/assets/svg/SvgPower.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let size: string = '16';
+</script>
+
+<svg width={size}
+height={size} viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" fill="none"
+    ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    ></g><g id="SVGRepo_iconCarrier">
+        <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+            d="M22.5 4.742a13 13 0 11-13 0M16 3v10"
+        ></path>
+    </g></svg
+>
+
+<!-- svg repo - quill oval interface icons -->
