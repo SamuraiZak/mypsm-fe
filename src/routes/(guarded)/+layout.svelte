b37bb392f809ff5e03deaf657208e727efe1e715
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-14 19:19:57 +0800

    feat: Modified guarded layouts

--- src/routes/(guarded)/+layout.svelte
+++ src/routes/(guarded)/+layout.svelte
@@ -1,1 +1,156 @@
-<slot></slot>
+<script lang="ts">
+    import SvgBookmark from '$lib/assets/svg/SvgBookmark.svelte';
+    import group from '$lib/assets/images/Group.png';
+    import MyPSM from '$lib/assets/images/MyPSM_alt.png';
+    import Breadcrumb from '$lib/components/breadcrumb/Breadcrumb.svelte';
+    import SvgAccount from '$lib/assets/svg/SvgAccount.svelte';
+    import SvgPower from '$lib/assets/svg/SvgPower.svelte';
+    import {
+        Button,
+        Dropdown,
+        DropdownItem,
+        Avatar,
+        DropdownHeader,
+        DropdownDivider,
+    } from 'flowbite-svelte';
+
+    let currentFullname = 'Mohd Irfan bin Abu Nawas Haji Ismail Sbari Yakub';
+    let currentRole = 'Kakitangan';
+</script>
+
+<section class="h-screen w-screen bg-ios-basic-white">
+    <!-- wrapper starts here -->
+    <div class="flex h-screen w-screen flex-col items-start justify-start">
+        <!-- header starts here -->
+        <header
+            class="flex h-10 max-h-10 min-h-10 w-full flex-row justify-between bg-system-primary"
+        >
+            <!-- leading starts here -->
+            <div class="flex h-full w-fit flex-row items-center justify-center">
+                <!-- logo wrapper starts here -->
+                <div
+                    class="flex w-[200px] flex-row items-center justify-center gap-2"
+                >
+                    <img class="h-6" src={group} alt="" />
+                    <img class="h-4" src={MyPSM} alt="" />
+                </div>
+                <!-- logo wrapper starts here -->
+            </div>
+
+            <!-- leading ends here -->
+
+            <!-- trailing starts here -->
+            <div class="flex h-full w-fit flex-row items-center justify-end">
+                <!-- account button starts here -->
+                <div
+                    class="flex h-[40px] w-fit flex-col items-center justify-center"
+                >
+                    <button
+                        id="accountTile"
+                        class="flex h-full w-[250px] max-w-[250px] flex-row items-center justify-start overflow-hidden text-ios-basic-white hover:bg-ios-activeColors-activeBlue-dark"
+                    >
+                        <!-- leading starts here -->
+                        <div
+                            class="flex h-full w-[40px] min-w-[40px] flex-row items-center justify-center"
+                        >
+                            <span class="">
+                                <SvgAccount size="22"></SvgAccount>
+                            </span>
+                        </div>
+                        <!-- leading ends here -->
+
+                        <!-- trailing starts here -->
+                        <div
+                            class="flex h-full w-full max-w-[200px] flex-col items-start justify-center pr-2"
+                        >
+                            <p
+                                class="w-full truncate text-ellipsis text-start text-base leading-tight"
+                            >
+                                {currentFullname}
+                            </p>
+                            <p
+                                class="w-full truncate text-ellipsis text-start text-sm leading-tight"
+                            >
+                                {currentRole}
+                            </p>
+                        </div>
+                        <!-- trailing ends here -->
+                    </button>
+                    <Dropdown
+                        triggeredBy="#accountTile"
+                        containerClass="w-[250px] border divide-y z-10"
+                    >
+                        <div slot="header" class="px-4 py-2">
+                            <p
+                                class="w-full truncate text-ellipsis text-start text-sm font-semibold leading-normal"
+                            >
+                                {currentFullname}
+                            </p>
+                            <p
+                                class="w-full truncate text-ellipsis text-start text-sm leading-normal"
+                            >
+                                {currentRole}
+                            </p>
+                        </div>
+                        <DropdownItem>Halaman Utama</DropdownItem>
+                        <DropdownItem>Tetapan Akaun</DropdownItem>
+                        <DropdownItem>Tukar Peranan</DropdownItem>
+                        <DropdownItem slot="footer">Log Keluar</DropdownItem>
+                    </Dropdown>
+                </div>
+                <!-- account button ends here -->
+
+
+            </div>
+
+            <!-- trailing ends here -->
+        </header>
+        <!-- header ends here -->
+
+        <!-- body starts here -->
+        <div
+            class="flex h-full max-h-full w-full flex-row overflow-hidden bg-ios-basic-white"
+        >
+            <!-- sidebar starts here -->
+            <div
+                class="flex w-[200px] min-w-[200px] max-w-[200px] flex-col overflow-y-auto border-r bg-ios-basic-white"
+            ></div>
+            <!-- sidebar ends here -->
+
+            <!-- content wrapper starts here -->
+            <div
+                class="flex h-full w-full flex-col items-start overflow-hidden overflow-x-hidden bg-ios-basic-white"
+            >
+                <!-- breadcrumb starts here -->
+                <div
+                    class="flex h-10 max-h-10 min-h-10 w-full flex-row justify-start border-b bg-ios-basic-white px-2"
+                >
+                    <div
+                        class="flex h-full flex-row items-center justify-center gap-2"
+                    >
+                        <span
+                            class="text text-ios-labelColors-secondaryLabel-light"
+                        >
+                            <SvgBookmark size="16"></SvgBookmark>
+                        </span>
+                        <Breadcrumb></Breadcrumb>
+                    </div>
+                </div>
+                <!-- breadcrumb ends here -->
+
+                <!-- content body wrapper starts here -->
+                <div
+                    class="flex h-full w-full flex-col items-start justify-start overflow-hidden"
+                >
+                    <slot />
+                </div>
+
+                <!-- content body wrapper ends here -->
+            </div>
+            <!-- content wrapper ends here -->
+        </div>
+        <!-- body ends here -->
+    </div>
+    <!-- wrapper ends here -->
+</section>
+<slot />
