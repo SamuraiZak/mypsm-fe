7857bb52367885328f12fc7a863f4a03001d5d37
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-14 19:17:01 +0800

    feat: Created breadcrumb components

---
+++ src/lib/components/breadcrumb/Breadcrumb.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    import SvgChevronRight from '$lib/assets/svg/SvgChevronRight.svelte';
+
+    let crumbs: Array<{ label: string; href: string }> = [];
+
+    $: {
+        // Remove zero-length - tokens.
+        const tokens = $page.url.pathname.split('/').filter((t) => t !== '');
+
+        // Create {label, href} pairs for each token.
+        let tokenPath = '';
+
+        crumbs = tokens.map((t) => {
+            tokenPath += '/' + t;
+            t = t.charAt(0).toUpperCase() + t.slice(1);
+            return { label: t, href: tokenPath };
+        });
+    }
+</script>
+
+<div class="flex flex-row items-center justify-start gap-2">
+    {#each crumbs as c, i}
+        <span class="text-base text-ios-labelColors-secondaryLabel-light select-none">
+            {c.label}
+        </span>
+        {#if i !== crumbs.length - 1}
+            <span class="text-ios-labelColors-secondaryLabel-light select-none">
+                <SvgChevronRight size="14"></SvgChevronRight>
+            </span>
+        {/if}
+    {/each}
+</div>
