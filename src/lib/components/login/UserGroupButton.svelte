96e5e940bd6592294d4ad94eae3c09b9c74e43b9
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-15 09:04:11 +0800

    feat: Modified the bg of user group button

--- src/lib/components/login/UserGroupButton.svelte
+++ src/lib/components/login/UserGroupButton.svelte
@@ -2,18 +2,18 @@
     let active: boolean = false;
     export let value = '';
     export let selectedValue = '';
+
+    export let handleSelect = () => {};
     $: {
         active = value == selectedValue;
     }
 </script>
 
 <button
-    on:click={() => {
-        selectedValue = value;
-    }}
-    class="flex h-full max-h-24 overflow-hidden w-full flex-col items-center justify-between rounded border p-2 font-medium text-ios-labelColors-secondaryLabel-light hover:border-ios-systemColors-systemBlue-light hover:text-ios-systemColors-systemBlue-light {active
+    on:click={handleSelect}
+    class="flex h-full max-h-24 w-full flex-col items-center justify-between overflow-hidden rounded border p-2 font-medium hover:border-ios-systemColors-systemBlue-light bg-ios-basic-white hover:text-ios-systemColors-systemBlue-light {active
         ? 'border-ios-systemColors-systemBlue-light text-ios-systemColors-systemBlue-light'
-        : ''}"
+        : 'text-ios-basic-inactiveGray'}"
 >
     <div class="flex h-12 w-full flex-col items-center justify-center">
         <span class="">
@@ -21,6 +21,6 @@
         </span>
     </div>
     <div class="flex h-fit w-full flex-col items-center justify-center">
-        <span class="text-sm leading-tight"> <slot /> </span>
+        <span class="text-sm {active ? 'font-semibold' : 'font-normal'} leading-tight"> <slot /> </span>
     </div>
 </button>
