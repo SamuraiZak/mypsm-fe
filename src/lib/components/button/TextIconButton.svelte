7bdd104823e4c11946f562dfb8d9f5d37c4699a1
Holifield William <holifield.william@impactbs.my>
2024-02-15 17:19:30 +0800

    feat: added TextIconButton component & related icons

---
+++ src/lib/components/button/TextIconButton.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+    import SvgXMark from '$lib/assets/svg/SvgXMark.svelte';
+    import SvgTrash from '$lib/assets/svg/SvgTrash.svelte';
+    import SvgChevronRight from '$lib/assets/svg/SvgChevronRight.svelte';
+    import SvgChevronLeft from '$lib/assets/svg/SvgChevronLeft.svelte';
+    import SvgPrinter from '$lib/assets/svg/SvgPrinter.svelte';
+    import SvgCheck from '$lib/assets/svg/SvgCheck.svelte';
+
+    export let form: string = '';
+    export let type: string = "primary" || "danger"|| "neutral";
+    export let disabled: boolean = false;
+    export let icon: string = '';
+    export let label: string = 'Label';
+    export let onClick = () => {};
+</script>
+
+<button
+    {form}
+    {disabled}
+    on:click={onClick}
+    class="flex flex-row items-center justify-center gap-1 rounded px-2.5 {type==='primary'
+        ? 'bg-ios-systemColors-systemBlue-light hover:bg-ios-systemColors-systemBlue-dark'
+        : type==='danger'
+          ? 'bg-ios-systemColors-systemRed-light hover:bg-ios-systemColors-systemRed-dark'
+          : 'border bg-bgr-primary hover:bg-bgr-secondary'} h-8"
+>
+
+    <!-- icon -->
+    <div class="flex h-full flex-row items-center justify-center">
+        <span
+            class="leading-loose {type==='primary'
+                ? 'text-txt-blend '
+                : type==='danger'
+                  ? 'text-txt-blend'
+                  : 'text-txt-secondary'}"
+        >
+            <!-- icon slot -->
+            {#if icon == 'cancel'}
+                <SvgXMark />
+            {:else if icon == 'delete'}
+                <SvgTrash />
+            {:else if icon == 'check'}
+                <SvgCheck />
+            {:else if icon == 'next'}
+                <SvgChevronRight />
+            {:else if icon == 'previous'}
+                <SvgChevronLeft />
+            {:else if icon == 'print'}
+                <SvgPrinter />
+            {:else}
+                <slot />
+            {/if}
+        </span>
+    </div>
+
+    <!-- label -->
+    <div class="flex h-full flex-row items-center justify-center">
+        <p
+            class="text-sm font-normal leading-loose {type==='primary'
+                ? ' text-ios-basic-white'
+                : type==='danger'
+                  ? ' text-ios-basic-white'
+                  : ' text-ios-labelColors-secondaryLabel-light'}"
+        >
+            {label}
+        </p>
+    </div>
+</button>
