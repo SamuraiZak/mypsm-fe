07d77dbd96bcfc462296772efc8656b9f7051afb
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-02-16 17:37:33 +0800

    feat: adds necessary svg for the customTable.

---
+++ src/lib/assets/svg/SvgSortDown.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let size: string = '16';
+</script>
+
+<svg
+    width={size}
+    height={size}
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    ></g><g id="SVGRepo_iconCarrier">
+        <path
+            d="M13 12H21M13 8H21M13 16H21M6 7V17M6 17L3 14M6 17L9 14"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        ></path>
+    </g></svg
+>
