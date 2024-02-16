1e1282318ce7b99aa016b3b86c3d2559ef054527
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-02-16 17:38:24 +0800

    feat: create table dto

---
+++ src/lib/dto/core/table/table.dto.ts
@@ -0,0 +1,9 @@
+import type { CommonListRequestDTO } from "../common/common-list-request.dto";
+import type { Meta } from "../common/common-response.dto";
+
+export interface TableDTO{
+    param: CommonListRequestDTO;
+    meta: Meta;
+    data: object[];
+    selectedData?: object[];
+}
