93de850ee283766a1f12b1052054c293bd6e04ad
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-19 09:11:56 +0800

    feat: Added new account service

---
+++ src/lib/services/implementation/core/account/account.service.ts
@@ -0,0 +1,38 @@
+// ===============================================================
+// Account Services
+// ===============================================================
+
+import { CommonResponseConstant } from '$lib/constants/core/common-response.constant';
+import {
+    CommonListRequestConvert,
+    type CommonListRequestDTO,
+} from '$lib/dto/core/common/common-list-request.dto';
+import { CommonResponseConvert } from '$lib/dto/core/common/common-response.dto';
+import type { Input } from 'ky';
+import http from '../../service-provider.service';
+
+export class AccountService {
+
+    // update password service
+    static async updatePassword(param: object) {
+        try {
+            let url: Input = 'account/updatePassword';
+
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+}
