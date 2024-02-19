6aed2289f2d858a7fb273ef9f3c7a7ee928a012d
hamiz <hamiz.nazif@impact-multimedia.com>
2024-02-19 17:48:21 +0800

    feat: create new pages to PTB and Stepper

---
+++ src/lib/components/stepper/StepperContentHeader.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let title: string = 'Stepper title';
+</script>
+
+<div
+    id={title}
+    class="flex h-[50px] max-h-[50px] min-h-[50px] w-full flex-row items-center justify-between border-b border-bdr-primary px-2.5"
+>
+    <div class="flex h-full max-h-full flex-row items-center justify-start">
+        <p class="text-sm font-semibold text-txt-primary">{title}</p>
+    </div>
+    <div class="flex h-full max-h-full flex-row items-center justify-end gap-1">
+        <slot />
+    </div>
+</div>
--- src/lib/constants/core/sidebar.constant.ts
+++ src/lib/constants/core/sidebar.constant.ts
@@ -1610,7 +1610,245 @@ export class SidebarConstant {
         },
 
         // Urus Setia Pentadbiran
+        {
+            role: 'Urus Setia Pentadbiran',
+            navItems: [
+                {
+                    name: 'Halaman Utama',
+                    path: '/halaman-utama',
+                },
+                {
+                    name: 'Pinjaman',
+                    path: '',
+                    subItems: [
+                        {
+                            name: 'Pinjaman Komputer, Telefon Pintar dan Kenderaan',
+                            path: '/pinjaman-kuarters/pinjaman',
+                        },
+
+                    ],
+                },
+            ]
+        },
         // urus Seta Tatatertib
+        
         // unit negeri
+        {
+            role: RoleConstant.unitNegeri.code,
+            navItems: [
+                {
+                    name: 'Halaman Utama',
+                    path: '/halaman-utama',
+                },
+                {
+                    name: 'Perjawatan',
+                    path: '',
+                    subItems: [
+                        {
+                            name: 'Lantikan Baru',
+                            path: '/perjawatan/lantikan-baru',
+                        },
+                        {
+                            name: 'Tawaran Baru',
+                            path: '/perjawatan/tawaran-baru',
+                        },
+                        {
+                            name: 'Pengesahan dalam Perkhidmatan',
+                            path: '/perjawatan/pengesahan-dalam-perkhidmatan',
+                        },
+                        {
+                            name: 'PTB & KWAP',
+                            path: '/perjawatan/PTB-dan-KWAP',
+                        },
+                        {
+                            name: 'Pemangkuan',
+                            path: '/perjawatan/pemangkuan',
+                        },
+                        {
+                            name: 'Kenaikan Pangkat',
+                            path: '/perjawatan/kenaikan-pangkat',
+                        },
+                        {
+                            name: 'Pertukaran',
+                            path: '/perjawatan/pertukaran',
+                        },
+                        {
+                            name: 'Tanggung Kerja',
+                            path: '/perjawatan/tanggung-kerja',
+                        },
+                        {
+                            name: 'Persaraan',
+                            path: '/perjawatan/persaraan',
+                        },
+                    ],
+                },
+                {
+                    name: 'Cuti',
+                    path: '',
+                    subItems: [
+                        {
+                            name: 'Cuti (Rehat, Kecemasan, Sakit)',
+                            path: '/cuti/cuti-rehat-kecemasan-sakit',
+                        },
+                        {
+                            name: 'Pengumpulan Gantian Cuti Rehat',
+                            path: '/cuti/pengumpulan-gantian-cuti-rehat',
+                        },
+                        {
+                            name: 'Pengeluaran Gantian Cuti Rehat Awal',
+                            path: '/cuti/pengeluaran-gantian-cuti-rehat-awal',
+                        },
+                        {
+                            name: 'Kelulusan Cuti',
+                            path: '/cuti/kelulusan-cuti',
+                        },
+                    ],
+                },
+                {
+                    name: 'Gaji',
+                    path: '',
+                    subItems: [
+                        {
+                            name: 'Pergerakan Gaji',
+                            path: '/gaji/pergerakan-gaji',
+                        },
+                        {
+                            name: 'Gaji dan Elaun-elaun',
+                            path: '/gaji/gaji-elaun',
+                        },
+                        {
+                            name: 'Gaji Akhir',
+                            path: '/gaji/gaji-akhir',
+                        },
+                        {
+                            name: 'Sijil Gaji Akhir',
+                            path: '/gaji/sijil-gaji-akhir',
+                        },
+                    ],
+                },
+                {
+                    name: ' Integriti',
+                    path: '',
+                    subItems: [
+                        {
+                            name: 'Surcaj',
+                            path: '/integriti/surcaj',
+                        },
+                        {
+                            name: 'Prosiding Tatatertib',
+                            path: '/integriti/prosiding',
+                        },
+                        {
+                            name: 'Prosiding Tatatertib - Rayuan',
+                            path: '/integriti/rayuan',
+                        },
+                    ],
+                },
+                {
+                    name: 'LNPT',
+                    path: '',
+                    subItems: [
+                        {
+                            name: 'LNPT (Purata LNPT)',
+                            path: '/lnpt/purata-lnpt',
+                        },
+                        {
+                            name: 'Purata LNPT (Pengeshan Lantikan Baharu)',
+                            path: '/lnpt/pengesahan-lantikan-baharu',
+                        },
+                        {
+                            name: 'LNPT (Sejarah APC)',
+                            path: '/lnpt/sejarah-apc',
+                        },
+                    ],
+                },
+                {
+                    name: 'Latihan',
+                    path: '',
+                    subItems: [
+                        {
+                            name: 'Latihan (Kemaskini Peperiksaan dalam Jabatan LKIM)',
+                            path: '/latihan/kemaskini-peperiksaan',
+                        },
+                        {
+                            name: 'Latihan (Rekod Peperiksaan)',
+                            path: 'latihan/rekod-peperiksaan',
+                        },
+                        {
+                            name: 'Latihan (Permohonan Pembiayaan Pembelajaran)',
+                            path: '/latihan/pembiayaan',
+                        },
+                        {
+                            name: 'Latihan (Permohonan Tuntutan Pembiayaan Yuran Pengajian)',
+                            path: '/latihan/tuntutan-yuran',
+                        },
+                    ],
+                },
+                {
+                    name: 'Kakitangan Kontrak',
+                    path: '',
+                    subItems: [
+                        {
+                            name: 'Perlantikan Baru (Kontrak)',
+                            path: '/kakitangan-kontrak/perlantikan',
+                        },
+                        {
+                            name: 'Pembaharuan Kontrak',
+                            path: '/kakitangan-kontrak/pembaharuan',
+                        },
+                    ],
+                },
+                {
+                    name: 'Pinjaman & Kuarters',
+                    path: '',
+                    subItems: [
+                        {
+                            name: 'Pinjaman Komputer, Telefon Pintar dan Kenderaan',
+                            path: '/pinjaman-kuarters/pinjaman',
+                        },
+                        {
+                            name: 'Kuarters - Permohonan Menduduki Kuarter',
+                            path: '/pinjaman-kuarters/permohonan-menduduki-kuarter',
+                        },
+                        {
+                            name: 'Kuarters - Permohonan Keluar Kuarter',
+                            path: '/pinjaman-kuarters/permohonan-keluar-kuarter',
+                        },
+                    ],
+                },
+                {
+                    name: 'Perubatan',
+                    path: '',
+                    subItems: [
+                        {
+                            name: 'Perkhidmatan (Permohonan Panel Klinik)',
+                            path: '/perubatan/permohonan-panel-klinik',
+                        },
+                        {
+                            name: 'Perkhidmatan (Bil Tuntutan Klinik)',
+                            path: '/perubatan/bil-tuntutan-klinik',
+                        },
+                        {
+                            name: 'Perkhidmatan (Bil Tuntutan Kakitangan)',
+                            path: 'perubatan/bil-tuntutan-kakitangan',
+                        },
+                    ],
+                },
+                {
+                    name: 'Elaun-elaun Perkhidmatan',
+                    path: '',
+                    subItems: [
+                        {
+                            name: 'Senarai Permohonan Elaun-elaun',
+                            path: '/elaun/senarai-permohonan',
+                        },
+                    ],
+                },
+                {
+                    name: 'Tetapan',
+                    path: '/tetapan',
+                },
+            ],
+        },
     ];
 }
