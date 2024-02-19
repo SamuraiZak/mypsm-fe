991d3e7d90ad37464aaf573e010d0a8a6c8ae9ba
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-19 09:09:07 +0800

    feat: Added svgs as components

--- src/lib/assets/svg/SvgCheck.svelte
+++ src/lib/assets/svg/SvgCheck.svelte
@@ -3,17 +3,21 @@
 </script>
 
 <svg
+    viewBox="0 0 24 24"
     width={size}
     height={size}
-    xmlns="http://www.w3.org/2000/svg"
     fill="none"
-    viewBox="0 0 24 24"
-    stroke-width="1.5"
-    stroke="currentColor"
->
-    <path
+    xmlns="http://www.w3.org/2000/svg"
+    ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+        id="SVGRepo_tracerCarrier"
         stroke-linecap="round"
         stroke-linejoin="round"
-        d="M4.5 12.75l6 6 9-13.5"
-    />
-</svg>
+    ></g><g id="SVGRepo_iconCarrier">
+        <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M9.9647 14.9617L17.4693 7.44735L18.5307 8.50732L9.96538 17.0837L5.46967 12.588L6.53033 11.5273L9.9647 14.9617Z"
+            fill="currentColor"
+        ></path>
+    </g></svg
+>
