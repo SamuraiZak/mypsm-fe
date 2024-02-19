bf364ee86ecc4c8633636d218b61a9582b66c31c
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-19 18:46:59 +0800

    feat: Added multiple svg as component

---
+++ src/lib/assets/svg/SvgBookSolid.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let size: string = '16';
+</script>
+
+<svg
+    fill="currentColor"
+    width={size}
+    height={size}
+    viewBox="0 0 8 8"
+    xmlns="http://www.w3.org/2000/svg"
+    ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    ></g><g id="SVGRepo_iconCarrier">
+        <path
+            d="M1 0c-.07 0-.13.01-.19.03-.39.08-.7.39-.78.78-.03.06-.03.12-.03.19v5.5c0 .83.67 1.5 1.5 1.5h5.5v-1h-5.5c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h5.5v-5.5c0-.28-.22-.5-.5-.5h-.5v3l-1-1-1 1v-3h-3z"
+        ></path>
+    </g></svg
+>
