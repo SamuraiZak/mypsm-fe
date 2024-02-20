071e9b227fea719d9d9df7b18a50f382f84bcff3
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-20 11:01:52 +0800

    feat: Modified text icon button component

--- src/lib/components/button/TextIconButton.svelte
+++ src/lib/components/button/TextIconButton.svelte
@@ -44,7 +44,7 @@
             {:else if icon == 'next'}
                 <SvgChevronRight size="20" />
             {:else if icon == 'previous'}
-                <SvgChevronLeft size="20" />
+                <SvgChevronLeft size="16" />
             {:else if icon == 'print'}
                 <SvgPrinter size="20" />
             {:else}
