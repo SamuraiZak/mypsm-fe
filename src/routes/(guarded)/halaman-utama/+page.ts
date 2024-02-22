b72a9e9fcbe5519f3241e3f94cf9914333d998c3
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-22 17:07:52 +0800

    Feat: WIP halaman utama

--- src/routes/(guarded)/halaman-utama/+page.ts
+++ src/routes/(guarded)/halaman-utama/+page.ts
@@ -1,16 +1,35 @@
 import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key.constant';
+import { UserRoleConstant } from '$lib/constants/core/user-role.constant';
 import { redirect } from '@sveltejs/kit';
 
-export const load = async () => {
-    let token: string | null = localStorage.getItem(
-        LocalStorageKeyConstant.accessToken,
-    );
+const authorisedRoleCode: string[] = [
+    UserRoleConstant.calon.code,
+    UserRoleConstant.kakitangan.code,
+];
 
-    if (token == null) {
+export const load = async () => {
+    const currentRoleCode = localStorage.getItem(
+        LocalStorageKeyConstant.currentRoleCode,
+    );
+    if (currentRoleCode !== null) {
+        if (authorisedRoleCode.includes(currentRoleCode)) {
+            // TODO: YOUR CODE
+        } else {
+            // TODO:
+            console.error(401);
+        }
+    } else {
         throw redirect(300, '../../login');
     }
+    // let token: string | null = localStorage.getItem(
+    //     LocalStorageKeyConstant.accessToken,
+    // );
+
+    // if (token == null) {
+    //     throw redirect(300, '../../login');
+    // }
 
     // TODO: get list of agendas
 
-    // TODO: get list 
+    // TODO: get list
 };
