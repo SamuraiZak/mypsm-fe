4a0fc4d0935430dd8839efa5fba564eaabe529d1
Holifield William <holifield.william@impactbs.my>
2024-02-22 16:58:23 +0800

    feat: added role code for conditional view

--- src/routes/(guarded)/perjawatan/pemangkuan/rekod/+page.svelte
+++ src/routes/(guarded)/perjawatan/pemangkuan/rekod/+page.svelte
@@ -3,11 +3,21 @@
     import CustomTab from '$lib/components/tab/CustomTab.svelte';
     import CustomTabContent from '$lib/components/tab/CustomTabContent.svelte';
     import CustomTable from '$lib/components/table/CustomTable.svelte';
+    import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key.constant';
     import type { CommonListRequestDTO } from '$lib/dto/core/common/common-list-request.dto';
     import type { TableDTO } from '$lib/dto/core/table/table.dto';
     import type { PageData } from './$types';
     import { _updateTable } from './+page';
     export let data: PageData;
+
+    let currentRoleCode: string | null;
+    currentRoleCode = localStorage.getItem(
+        LocalStorageKeyConstant.currentRoleCode,
+    );
+
+    // role code for pelulus and penyokong
+    let supporterRoleCode: string = "a1f3840c-67b6-4bbb-afdb-6023bc45177d";
+    let approverRoleCode: string = "99791982-c4da-4cf8-a2bc-304775445552";
 
     let param: CommonListRequestDTO = data.param;
     let table: TableDTO = {
@@ -46,6 +56,7 @@
     class="max-h-[calc(100vh - 172px)] flex h-full w-full flex-col items-center justify-start"
 >
 <CustomTab>
+    {#if currentRoleCode !== approverRoleCode}
     <!-- Gred 1-54 -->
     <CustomTabContent title="Gred 1-54">
         <ContentHeader
@@ -83,6 +94,7 @@
 
     <!-- pengarah bahagian/negeri should not view this particular tab -->
     <!-- Gred Utama -->
+    {#if currentRoleCode === supporterRoleCode }
     <CustomTabContent title="Gred Utama">
         <ContentHeader
             borderClass="border-none"
@@ -99,5 +111,42 @@
             ></CustomTable>
         </div>
     </CustomTabContent>
+    {/if}
+
+    <!-- For Approver's View -->
+    {:else}
+    <CustomTabContent title="Permohonan Penangguhan/Pindaan Penempatan">
+        <ContentHeader
+            borderClass="border-none"
+            title="Senarai rekod permohonan penangguhan/pindaan penempatan yang menunggu keputusan anda"
+        />
+        <div
+            class="flex max-h-full w-full flex-col items-start justify-start"
+        >
+            <CustomTable
+                onUpdate={_search}
+                enableDetail
+                bind:tableData={table}
+            ></CustomTable>
+        </div>
+    </CustomTabContent>
+
+    <CustomTabContent title="Pengesahan Keputusan Pemangkuan">
+        <ContentHeader
+            borderClass="border-none"
+            title="Senarai rekod pemangkuan yang menunggu keputusan anda"
+        ></ContentHeader>
+        <div
+            class="flex max-h-full w-full flex-col items-start justify-start"
+        >
+            <!-- Table Here -->
+            <CustomTable
+                onUpdate={_search}
+                enableDetail
+                bind:tableData={table}
+            ></CustomTable>
+        </div>
+    </CustomTabContent>
+    {/if}
 </CustomTab>
 </section>
