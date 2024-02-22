4a62d3920f21780b9c2916f9cc86c320bb631ab6
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-02-22 09:17:11 +0800

    feat: creates zod schema for new hire.

---
+++ src/lib/schemas/mypsm/employment/new-hire/schema.ts
@@ -0,0 +1,245 @@
+//==========================================================
+//================== Personal Info Schema ==============
+//==========================================================
+
+import {
+    booleanSchema,
+    codeSchema,
+    dateSchema,
+    longTextSchema,
+    maxDateSchema,
+    minDateSchema,
+    numberIdSchema,
+    numberSchema,
+    shortTextSchema,
+} from '$lib/schemas/common/schema-type';
+import { z } from 'zod';
+
+export const _personalInfoSchema = z
+    .object({
+        id: z.number().readonly(),
+        name: shortTextSchema,
+        alternativeName: z.string(),
+        identityDocumentNumber: shortTextSchema,
+        identityDocumentColor: codeSchema,
+        email: shortTextSchema.email({ message: 'Emel tidak lengkap.' }),
+        propertyDeclarationDate: maxDateSchema,
+        birthDate: maxDateSchema,
+        birthStateId: numberIdSchema,
+        birthCountryId: numberIdSchema,
+        genderId: numberIdSchema,
+        nationalityId: numberIdSchema,
+        religionId: numberIdSchema,
+        raceId: numberIdSchema,
+        titleId: numberIdSchema,
+        ethnicId: numberIdSchema,
+        maritalId: numberIdSchema,
+        assetDeclarationStatusId: numberIdSchema,
+        homeAddress: shortTextSchema,
+        homeCountryId: numberIdSchema,
+        homeStateId: numberIdSchema,
+        homeCityId: numberIdSchema,
+        homePostcode: shortTextSchema,
+        mailAddress: shortTextSchema,
+        mailCountryId: numberIdSchema,
+        mailStateId: numberIdSchema,
+        mailCityId: numberIdSchema,
+        mailPostcode: shortTextSchema,
+        isExPoliceOrSoldier: booleanSchema,
+        isInternalRelationship: booleanSchema,
+        employeeNumber: z.string(),
+        employeeName: z.string(),
+        employeePosition: z.string(),
+        relationshipId: z.number(),
+        isReadOnly: z.boolean().readonly(),
+    })
+    .refine((data) => !data.isInternalRelationship, {
+        message: 'Sila isi medan ini.',
+        path: [
+            'employeeNumber',
+            'employeeName',
+            'employeePosition',
+            'relationship',
+        ],
+    });
+
+//==========================================================
+//================== Academic Schema =====================
+//==========================================================
+
+export const _academicInfoSchema = z.object({
+    // id: numberIdSchema,
+    majorId: numberIdSchema,
+    minorId: numberIdSchema,
+    countryId: numberIdSchema,
+    institutionId: numberIdSchema,
+    educationLevelId: numberIdSchema,
+    sponsorshipId: numberIdSchema,
+    name: codeSchema,
+    completionDate: maxDateSchema,
+    finalGrade: codeSchema,
+    field: shortTextSchema,
+});
+
+export const _academicListSchema = z.object({
+    academicList: z.array(_academicInfoSchema),
+    isReadOnly: z.boolean().readonly(),
+});
+
+//==========================================================
+//================== Experience Schema =====================
+//==========================================================
+
+export const _experienceInfoSchema = z.object({
+    company: shortTextSchema,
+    address: shortTextSchema,
+    position: shortTextSchema,
+    positionCode: codeSchema,
+    startDate: maxDateSchema,
+    endDate: maxDateSchema,
+    salary: numberSchema,
+});
+
+export const _experienceListSchema = z.object({
+    experienceList: z.array(_experienceInfoSchema),
+    isReadOnly: z.boolean().readonly(),
+});
+
+//==========================================================
+//================== Activity Schema =====================
+//==========================================================
+
+export const _activityInfoSchema = z.object({
+    name: shortTextSchema,
+    joinDate: maxDateSchema,
+    position: shortTextSchema,
+    description: longTextSchema,
+});
+
+export const _activityListSchema = z.object({
+    activityList: z.array(_activityInfoSchema),
+    isReadOnly: z.boolean().readonly(),
+});
+
+//==========================================================
+//================== Realtions Schema =====================
+//==========================================================
+
+export const _relationsSchema = z.object({
+    birthCountryId: numberIdSchema,
+    birthStateId: numberIdSchema,
+    relationshipId: numberIdSchema,
+    educationLevelId: numberIdSchema,
+    raceId: numberIdSchema,
+    nationalityId: numberIdSchema,
+    maritalId: numberIdSchema,
+    genderId: numberIdSchema,
+    name: shortTextSchema,
+    alternativeName: z.string(),
+    identityDocumentColor: codeSchema,
+    identityDocumentNumber: shortTextSchema,
+    address: shortTextSchema,
+    postcode: shortTextSchema,
+    birthDate: maxDateSchema,
+    workAddress: shortTextSchema,
+    workPostcode: shortTextSchema,
+    phoneNumber: shortTextSchema,
+    marriageDate: z.date(),
+    inSchool: booleanSchema,
+});
+
+export const _familyListSchema = z.object({
+    dependenciesList: z.array(_relationsSchema),
+    isReadOnly: z.boolean().readonly(),
+});
+
+export const _dependencyListSchema = z.object({
+    dependenciesList: z.array(_relationsSchema),
+    isReadOnly: z.boolean().readonly(),
+});
+
+export const _nextOfKinListSchema = z.object({
+    nextOfKinsList: z.array(_relationsSchema),
+    isReadOnly: z.boolean().readonly(),
+});
+
+//==========================================================
+//================== Service Schema ===================
+//==========================================================
+
+export const _serviceInfoSchema = z.object({
+    candidateId: numberIdSchema,
+    gradeId: numberIdSchema,
+    maxGradeId: numberIdSchema,
+    positionId: numberIdSchema,
+    placementId: numberIdSchema,
+    serviceTypeId: numberIdSchema,
+    serviceGroupId: numberIdSchema,
+    unitId: numberIdSchema,
+    employmentStatusId: numberIdSchema,
+    effectiveDate: minDateSchema,
+    retirementBenefit: codeSchema,
+    epfNumber: shortTextSchema,
+    socsoNumber: shortTextSchema,
+    incomeNumber: shortTextSchema,
+    bankName: shortTextSchema,
+    bankAccount: shortTextSchema,
+    eligibleLeaveCount: numberSchema,
+    civilServiceStartDate: dateSchema,
+    newRecruitEffectiveDate: dateSchema,
+    serviceDate: dateSchema,
+    firstServiceDate: dateSchema,
+    firstConfirmServiceDate: dateSchema,
+    firstEffectiveDate: dateSchema,
+    confirmDate: dateSchema,
+    pensionNumber: shortTextSchema,
+    kgt: numberSchema,
+    retirementDate: minDateSchema,
+    revisionMonth: codeSchema,
+    maximumSalary: numberSchema,
+    baseSalary: numberSchema,
+    itka: numberSchema,
+    itp: numberSchema,
+    epw: numberSchema,
+    cola: numberSchema,
+    isReadOnly: z.boolean().readonly(),
+});
+
+//==========================================================
+//========= Approval Info Schema ===========================
+//==========================================================
+
+export const _approvalResultSchema = z.object({
+    id: numberIdSchema,
+    name: z.string().readonly(),
+    remark: longTextSchema,
+    isApproved: booleanSchema.default(true),
+});
+
+//==========================================================
+//========= Set Approver Info Schema =======================
+//==========================================================
+
+export const _setApproversSchema = z.object({
+    candidateId: numberIdSchema,
+    supporterId: numberIdSchema,
+    approverId: numberIdSchema,
+});
+
+//==========================================================
+//========= Get Approvers Info Schema =======================
+//==========================================================
+
+export const _getNewHireApproversSchema = z.object({
+    isReadonly: z.boolean().readonly(),
+    supporterId: z.number().readonly(),
+    approverId: z.number().readonly(),
+});
+
+//==========================================================
+//================== Document Schema =======================
+//==========================================================
+
+export const _documentsSchema = z.object({
+    isReadonly: z.boolean().readonly(),
+});
