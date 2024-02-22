1b7a160bf652df44ab8ef9cf370f3f240a6f8c30
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-22 17:02:22 +0800

    feat: Added necessary constant value

--- src/lib/constants/core/user-group.constant.ts
+++ src/lib/constants/core/user-group.constant.ts
@@ -2,25 +2,31 @@
 // User Group Constants
 // ===============================================================
 
+import type { LookupDTO } from '$lib/dto/core/lookup/lookup.dto';
 import type { UserGroupDTO } from '$lib/dto/core/user-group/user-group.dto';
 
 export class UserGroupConstant {
-    public static userGroup: UserGroupDTO[] = [
-        {
-            value: 'employee',
-            name: 'Kakitangan',
-        },
-        {
-            value: 'contractor',
-            name: 'Kakitangan Kontrak',
-        },
-        {
-            value: 'candidate',
-            name: 'Calon',
-        },
-        {
-            value: 'clinic',
-            name: 'Klinik Panel',
-        },
+    public static employee: UserGroupDTO = {
+        code: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+        description: 'Kakitangan',
+    };
+    public static contractor: UserGroupDTO = {
+        code: 'ff063e92-84be-45cb-bb27-7c0f96cfdc0',
+        description: 'Kakitangan Kontrak',
+    };
+    public static candidate: UserGroupDTO = {
+        code: 'f82fe23c-d4fd-4d61-9267-b16555c9db12',
+        description: 'Calon Kakitangan',
+    };
+    public static clinic: UserGroupDTO = {
+        code: '9428f85f-5bf0-4a7a-bd4c-261f3b25e491',
+        description: 'Klinik Panel',
+    };
+
+    public static list: UserGroupDTO[] = [
+        this.employee,
+        this.contractor,
+        this.candidate,
+        this.clinic,
     ];
 }
---
+++ src/lib/constants/core/user-role.constant.ts
@@ -0,0 +1,263 @@
+// ===============================================================
+// User Role Constant
+// ===============================================================
+
+import type { UserRoleDTO } from '$lib/dto/core/user-role/user-role.dto';
+
+export class UserRoleConstant {
+    public static urusSetiaPerjawatan: UserRoleDTO = {
+        id: 1,
+        code: '9d9168cc-95a1-46b0-99fb-6443348e153d',
+        description: 'urus setia perjawatan',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static urusSetiaIntegriti: UserRoleDTO = {
+        id: 2,
+        code: 'a168e533-4f9b-4a5a-8869-1f185d6dd512',
+        description: 'urus setia integriti',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static urusSetiaCuti: UserRoleDTO = {
+        id: 3,
+        code: 'e7c2abd3-c131-4b65-80a2-d5097f135adb',
+        description: 'urus setia cuti',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static urusSetiaGaji: UserRoleDTO = {
+        id: 4,
+        code: 'bd55e84d-c7b2-467e-abe7-fb918b73ea45',
+        description: 'urus setia gaji',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static urusSetiaElaunElaunPerkhidmatan: UserRoleDTO = {
+        id: 5,
+        code: '7f7e7f2c-ff3b-4cc4-a175-fefd5afddcfe',
+        description: 'urus setia elaun-elaun perkhidmatan',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static urusSetiaLnpt: UserRoleDTO = {
+        id: 6,
+        code: '3b2e22f9-6122-491a-b9a9-6a7b345d2a31',
+        description: 'urus setia lnpt',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static urusSetiaLatihan: UserRoleDTO = {
+        id: 7,
+        code: '26e7cdff-6027-4263-b1c0-e2a1d7041cf0',
+        description: 'urus setia latihan',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static urusSetiaPentadbiran: UserRoleDTO = {
+        id: 8,
+        code: 'f04feedb-b4b7-496a-bddb-79574e33dd6a',
+        description: 'urus setia pentadbiran',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static urusSetiaPerubatan: UserRoleDTO = {
+        id: 9,
+        code: 'add4b697-ff6f-4c6a-a003-703d0d90a124',
+        description: 'urus setia perubatan',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static urusSetiaPersaraan: UserRoleDTO = {
+        id: 10,
+        code: '99d39620-6b18-4850-adc1-4ab2f50e5491',
+        description: 'urus setia persaraan',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static urusSetiaTatatertib: UserRoleDTO = {
+        id: 11,
+        code: '515d555d-1e95-4ca2-bf47-3c57437fca05',
+        description: 'urus setia tatatertib',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static urusSetiaKhidmatSokongan: UserRoleDTO = {
+        id: 12,
+        code: '1287af33-3693-4660-8a6b-761d72035cf0',
+        description: 'urus setia khidmat sokongan',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static urusSetiaPeringkatNegeri: UserRoleDTO = {
+        id: 13,
+        code: '86ae30d8-5dc4-414f-8af7-96059b207fce',
+        description: 'urus setia peringkat negeri',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static penyokong: UserRoleDTO = {
+        id: 14,
+        code: 'a1f3840c-67b6-4bbb-afdb-6023bc45177d',
+        description: 'penyokong',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static pelulus: UserRoleDTO = {
+        id: 15,
+        code: '99791982-c4da-4cf8-a2bc-304775445552',
+        description: 'pelulus',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static pengarahKhidmatPengurusan: UserRoleDTO = {
+        id: 16,
+        code: '929dd929-ed66-4c54-b0f8-6e469b171db5',
+        description: 'pengarah khidmat pengurusan',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static ketuaSeksyen: UserRoleDTO = {
+        id: 17,
+        code: '506286a2-b51a-42c1-b4d6-ff611b7dec80',
+        description: 'ketua seksyen',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static ketuaPengarah: UserRoleDTO = {
+        id: 18,
+        code: 'cf673afb-3de3-4af1-a723-83bc8dce418f',
+        description: 'ketua pengarah',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static audit: UserRoleDTO = {
+        id: 19,
+        code: 'c3b94b16-22dd-4522-a2ab-df47452db824',
+        description: 'audit',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static admin: UserRoleDTO = {
+        id: 20,
+        code: '119aea32-be65-4ba3-ba19-19447c67f097',
+        description: 'admin',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static superAdmin: UserRoleDTO = {
+        id: 21,
+        code: '314acb4e-185b-4d10-becc-1993857e98e1',
+        description: 'super admin',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static kakitangan: UserRoleDTO = {
+        id: 22,
+        code: '30f9b9da-429d-4965-b1fd-734843c1ed28',
+        description: 'kakitangan',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static calon: UserRoleDTO = {
+        id: 23,
+        code: '0f2656ce-783a-4729-a58d-3abf7712660e',
+        description: 'calon',
+        userGroupCode: 'f82fe23c-d4fd-4d61-9267-b16555c9db12',
+    };
+    public static pengarahIntegriti: UserRoleDTO = {
+        id: 24,
+        code: '1eea87db-610b-48be-9d42-ab94ad58a346',
+        description: 'pengarah integriti',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static pengarahAudit: UserRoleDTO = {
+        id: 25,
+        code: 'aaf4f44d-c867-428a-9de6-9aac79be7425',
+        description: 'pengarah audit',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static timbalanKetuaSeksyen: UserRoleDTO = {
+        id: 26,
+        code: '6fd29446-5534-4ed7-83e9-157c05a4f4ca',
+        description: 'timbalan ketua seksyen',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static unitIntegriti: UserRoleDTO = {
+        id: 27,
+        code: '823725c5-fb2c-41d6-8b5e-9ca85b09715a',
+        description: 'unit integriti',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static unitCuti: UserRoleDTO = {
+        id: 28,
+        code: 'bb690c23-ed4e-4ce3-b36d-ff7acdfc86a1',
+        description: 'unit cuti',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static unitElaunElaunPerkhidmatan: UserRoleDTO = {
+        id: 29,
+        code: 'f69198b9-ca8a-4982-a27c-349c654973b8',
+        description: 'unit elaun-elaun perkhidmatan',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static unitBahagian: UserRoleDTO = {
+        id: 30,
+        code: '23071eec-3e75-430d-94f1-25a90fa5f638',
+        description: 'unit bahagian',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static unitNegeri: UserRoleDTO = {
+        id: 31,
+        code: 'b2ef7122-0257-4330-aa26-2ced1b893f6b',
+        description: 'unit negeri',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static unitPengurusanFasiliti: UserRoleDTO = {
+        id: 32,
+        code: '890dc7ed-fd98-4214-83c1-3244ef950b7c',
+        description: 'unit pengurusan fasiliti',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static pengarahNegeri: UserRoleDTO = {
+        id: 33,
+        code: '44aa484b-9110-4870-86b4-30cb7a266c1f',
+        description: 'pengarah negeri',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static pengarahBahagian: UserRoleDTO = {
+        id: 34,
+        code: '11ba9910-b641-4b62-8ca1-37217c2a9ed9',
+        description: 'pengarah bahagian',
+        userGroupCode: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
+    };
+    public static klinikPanel: UserRoleDTO = {
+        id: 35,
+        code: '24b3d6f7-4196-4659-9b18-919319b40626',
+        description: 'klinik panel',
+        userGroupCode: '9428f85f-5bf0-4a7a-bd4c-261f3b25e491',
+    };
+    public static kakitanganKontrak: UserRoleDTO = {
+        id: 36,
+        code: '442e6f24-2de2-452e-8146-2185ea36479d',
+        description: 'kakitangan kontrak',
+        userGroupCode: 'ff063e92-84be-45cb-bb27-7c0f96cfdc0',
+    };
+
+    public static list: UserRoleDTO[] = [
+        this.urusSetiaPerjawatan,
+        this.urusSetiaIntegriti,
+        this.urusSetiaCuti,
+        this.urusSetiaGaji,
+        this.urusSetiaElaunElaunPerkhidmatan,
+        this.urusSetiaLnpt,
+        this.urusSetiaLatihan,
+        this.urusSetiaPentadbiran,
+        this.urusSetiaPerubatan,
+        this.urusSetiaPersaraan,
+        this.urusSetiaTatatertib,
+        this.urusSetiaKhidmatSokongan,
+        this.urusSetiaPeringkatNegeri,
+        this.penyokong,
+        this.pelulus,
+        this.pengarahKhidmatPengurusan,
+        this.ketuaSeksyen,
+        this.ketuaPengarah,
+        this.audit,
+        this.admin,
+        this.superAdmin,
+        this.kakitangan,
+        this.calon,
+        this.pengarahIntegriti,
+        this.pengarahAudit,
+        this.timbalanKetuaSeksyen,
+        this.unitIntegriti,
+        this.unitCuti,
+        this.unitElaunElaunPerkhidmatan,
+        this.unitBahagian,
+        this.unitNegeri,
+        this.unitPengurusanFasiliti,
+        this.pengarahNegeri,
+        this.pengarahBahagian,
+        this.klinikPanel,
+        this.kakitanganKontrak,
+    ];
+}
