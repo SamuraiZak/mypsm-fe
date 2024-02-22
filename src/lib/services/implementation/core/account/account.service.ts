418fcf5f40574c50315109527a78dadf594066c0
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-22 17:07:29 +0800

    feat: Modified core services

--- src/lib/services/implementation/core/account/account.service.ts
+++ src/lib/services/implementation/core/account/account.service.ts
@@ -2,13 +2,34 @@
 // Account Services
 // ===============================================================
 
-
+import { CommonResponseConstant } from '$lib/constants/core/common-response.constant';
+import {
+    CommonListRequestConvert,
+    type CommonListRequestDTO,
+} from '$lib/dto/core/common/common-list-request.dto';
+import { CommonResponseConvert, type CommonResponseDTO } from '$lib/dto/core/common/common-response.dto';
 import type { Input } from 'ky';
 import http from '../../service-provider.service';
-import { CommonResponseConstant } from '$lib/constants/core/common-response.constant';
-import { CommonResponseConvert } from '$lib/dto/core/common/common-response.dto';
 
 export class AccountServices {
+    // get account details
+    static async getAccountDetails() {
+        try {
+            let url: Input = 'account/current_detail';
+
+            const response: Response = await http.get(url, {}).json();
+
+            const result: CommonResponseDTO = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
 
     // update password service
     static async updatePassword(param: object) {
