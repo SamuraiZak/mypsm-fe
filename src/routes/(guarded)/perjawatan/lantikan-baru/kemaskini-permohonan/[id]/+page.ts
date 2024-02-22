0789c8165986948654b62a46693f010ba2c55fa5
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-02-22 09:18:55 +0800

    chore: separates the schema from the ts files to a dedicated file.

--- src/routes/(guarded)/perjawatan/lantikan-baru/kemaskini-permohonan/[id]/+page.ts
+++ src/routes/(guarded)/perjawatan/lantikan-baru/kemaskini-permohonan/[id]/+page.ts
@@ -1,135 +1,35 @@
+import type { AddApprovalResultRequestBody } from '$lib/dto/core/common/add-approval-results-request.dto';
 import type { CommonResponseDTO } from '$lib/dto/core/common/common-response.dto';
-import type { SupporterApprovalData } from '$lib/dto/mypsm/employment/new-hire/new-hire-get-approvers-response.dto';
-import type { NewHireSecretaryAddUpdateRequestBody } from '$lib/dto/mypsm/employment/new-hire/new-hire-secretary-add-update-request.dto';
-import type { SecretaryApprovalData } from '$lib/dto/mypsm/employment/new-hire/new-hire-secretary-approval-response.dto';
-import type { SecretaryUpdateData } from '$lib/dto/mypsm/employment/new-hire/new-hire-secretary-update-response.dto';
-import type { NewHireSetApproversRequestBody } from '$lib/dto/mypsm/employment/new-hire/new-hire-set-approvers-request.dto';
+import type { CandidateActivityDetailsDTO } from '$lib/dto/mypsm/employment/new-hire/new-hire-activity.dto.js';
+import type { CandidateAcademicDetailsDTO } from '$lib/dto/mypsm/employment/new-hire/new-hire-candidate-academic-details.dto.js';
+import type { CandidateDependenciesDetailsDTO } from '$lib/dto/mypsm/employment/new-hire/new-hire-candidate-dependencies-details.dto.js';
+import type { CandidateExperiencesDetailDTO } from '$lib/dto/mypsm/employment/new-hire/new-hire-candidate-experience-details.dto.js';
+import type { CandidateFamilyDetailsDTO } from '$lib/dto/mypsm/employment/new-hire/new-hire-candidate-family-details.dto.js';
+import type { CandidateNextOfKinDetailsDTO } from '$lib/dto/mypsm/employment/new-hire/new-hire-candidate-next-of-kin-details.dto.js';
+import type { CandidatePersonalDTO } from '$lib/dto/mypsm/employment/new-hire/new-hire-candidate-personal-details.dto.js';
+import type { CandidateNewHireApproverResultDTO } from '$lib/dto/mypsm/employment/new-hire/new-hire-get-approver-result.dto';
+import type { NewHireGetApproversDTO } from '$lib/dto/mypsm/employment/new-hire/new-hire-get-approvers.dto.js';
+import type { NewHireSecretaryServiceUpdateDTO } from '$lib/dto/mypsm/employment/new-hire/new-hire-secretary-service-update.dto.js';
+import type { NewHireSetApproversDTO } from '$lib/dto/mypsm/employment/new-hire/new-hire-set-approvers.dto';
 import { getErrorToast } from '$lib/helpers/core/toast.helper';
-import type { AddApprovalResultRequestBody } from '$lib/services/implementation/mypsm/common/add-approval-results-request.dto';
+import {
+    _academicInfoSchema,
+    _academicListSchema,
+    _activityInfoSchema,
+    _activityListSchema,
+    _approvalResultSchema,
+    _dependencyListSchema,
+    _experienceInfoSchema,
+    _experienceListSchema,
+    _familyListSchema,
+    _nextOfKinListSchema,
+    _personalInfoSchema,
+    _serviceInfoSchema,
+    _setApproversSchema,
+} from '$lib/schemas/mypsm/employment/new-hire/schema.js';
 import { EmploymentServices } from '$lib/services/implementation/mypsm/perjawatan/employment.service';
 import { error } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms/client';
-import { z } from 'zod';
-
-// =========================================================================
-// z validation schema and submit function for the calon lantikan baru form fields
-// =========================================================================
-const shortTextSchema = z
-    .string({ required_error: 'Medan ini tidak boleh kosong.' })
-    .min(4, {
-        message: 'Medan ini hendaklah lebih daripada 4 karakter.',
-    })
-    .max(124, {
-        message: 'Medan ini tidak boleh melebihi 124 karakter.',
-    })
-    .trim();
-const codeSchema = z
-    .string({ required_error: 'Medan ini tidak boleh kosong.' })
-    .min(1, {
-        message: 'Medan ini hendaklah lebih daripada 1 karakter.',
-    })
-    .max(124, {
-        message: 'Medan ini tidak boleh melebihi 124 karakter.',
-    })
-    .trim();
-const longTextSchema = z
-    .string({
-        required_error: 'Medan ini tidak boleh kosong.',
-        invalid_type_error: 'Medan ini tidak boleh kosong.',
-    })
-    .min(4, {
-        message: 'Medan ini hendaklah lebih daripada 4 karakter.',
-    })
-    .max(124, {
-        message: 'Medan ini tidak boleh melebihi 124 karakter.',
-    })
-    .trim();
-
-const dateSchema = z.coerce.date({
-    errorMap: (issue, { defaultError }) => ({
-        message:
-            issue.code === 'invalid_date'
-                ? 'Tarikh tidak boleh dibiar kosong.'
-                : defaultError,
-    }),
-});
-
-const minDateSchema = z.coerce
-    .date({
-        errorMap: (issue, { defaultError }) => ({
-            message:
-                issue.code === 'invalid_date'
-                    ? 'Tarikh tidak boleh dibiar kosong.'
-                    : defaultError,
-        }),
-    })
-    .min(new Date(), {
-        message: 'Tarikh lepas tidak boleh kurang dari tarikh semasa.',
-    });
-
-const booleanSchema = z.boolean({
-    required_error: 'Sila tetapkan pilihan anda.',
-    invalid_type_error: 'Medan ini haruslah jenis boolean.',
-});
-
-const numberSchema = z.coerce.number({
-    required_error: 'Medan ini hendaklah diisi.',
-    invalid_type_error: 'Sila pastikan medan ini ditaip dengan angka',
-});
-
-const numberIdSchema = z.coerce.number({
-    required_error: 'Tidak tepat.',
-    invalid_type_error: 'Sila pastikan ID ditaip dengan angka',
-});
-
-export const _serviceInfoSchema = z.object({
-    candidateId: numberIdSchema,
-    gradeId: numberIdSchema,
-    maxGradeId: numberIdSchema,
-    positionId: numberIdSchema,
-    placementId: numberIdSchema,
-    serviceTypeId: numberIdSchema,
-    serviceGroupId: numberIdSchema,
-    unitId: numberIdSchema,
-    employmentStatusId: numberIdSchema,
-    effectiveDate: minDateSchema,
-    retirementBenefit: codeSchema,
-    epfNumber: shortTextSchema,
-    socsoNumber: shortTextSchema,
-    incomeNumber: shortTextSchema,
-    bankName: shortTextSchema,
-    bankAccount: shortTextSchema,
-    eligibleLeaveCount: numberSchema,
-    civilServiceStartDate: dateSchema,
-    newRecruitEffectiveDate: dateSchema,
-    serviceDate: dateSchema,
-    firstServiceDate: dateSchema,
-    firstConfirmServiceDate: dateSchema,
-    firstEffectiveDate: dateSchema,
-    confirmDate: dateSchema,
-    pensionNumber: shortTextSchema,
-    kgt: numberSchema,
-    retirementDate: minDateSchema,
-    revisionMonth: codeSchema,
-    maximumSalary: numberSchema,
-    baseSalary: numberSchema,
-    itka: numberSchema,
-    itp: numberSchema,
-    epw: numberSchema,
-    cola: numberSchema,
-});
-
-export const _secretaryApprovalInfoSchema = z.object({
-    id: numberIdSchema,
-    remark: longTextSchema,
-    isApproved: booleanSchema.default(true),
-});
-
-export const _secretarySetApproversSchema = z.object({
-    candidateId: numberIdSchema,
-    supporterId: numberIdSchema,
-    approverId: numberIdSchema,
-});
 
 export async function load({ params }) {
     const candidateIdRequestBody = {
@@ -157,7 +57,9 @@ export async function load({ params }) {
         );
 
     const familyInfoResponse: CommonResponseDTO =
-        await EmploymentServices.getCurrentCandidateFamily(candidateIdRequestBody);
+        await EmploymentServices.getCurrentCandidateFamily(
+            candidateIdRequestBody,
+        );
 
     const dependencyInfoResponse: CommonResponseDTO =
         await EmploymentServices.getCurrentCandidateDependencies(
@@ -178,15 +80,17 @@ export async function load({ params }) {
         await EmploymentServices.getCurrentCandidateSecretaryUpdate(
             candidateIdRequestBody,
         );
-    const serviceDetails: SecretaryUpdateData = serviceResponse.data?.details as SecretaryUpdateData;
+    const serviceDetails: NewHireSecretaryServiceUpdateDTO = serviceResponse
+        .data?.details as NewHireSecretaryServiceUpdateDTO;
 
     const secretaryApprovalResponse: CommonResponseDTO =
         await EmploymentServices.getCurrentCandidateSecretaryApproval(
             candidateIdRequestBody,
         );
 
-    const secretaryApprovalDetails: SecretaryApprovalData =
-        secretaryApprovalResponse.data?.details as SecretaryApprovalData;
+    const secretaryApprovalDetails: CandidateNewHireApproverResultDTO =
+        secretaryApprovalResponse.data
+            ?.details as CandidateNewHireApproverResultDTO;
 
     const secretaryGetApproversResponse: CommonResponseDTO =
         await EmploymentServices.getCurrentCandidateApprovers(
@@ -204,6 +108,46 @@ export async function load({ params }) {
         );
 
     // Superformed the data
+    const personalInfoForm = await superValidate(
+        personalDetailResponse.data?.details as CandidatePersonalDTO,
+        _personalInfoSchema,
+    );
+
+    const academicInfoForm = await superValidate(
+        academicInfoResponse.data?.details as CandidateAcademicDetailsDTO,
+        _academicListSchema,
+    );
+
+    const experienceInfoForm = await superValidate(
+        experienceInfoResponse.data?.details as CandidateExperiencesDetailDTO,
+        _experienceListSchema,
+    );
+    const activityInfoForm = await superValidate(
+        activityInfoResponse.data?.details as CandidateActivityDetailsDTO,
+        _activityListSchema,
+    );
+
+    const familyInfoForm = await superValidate(
+        familyInfoResponse.data?.details as CandidateFamilyDetailsDTO,
+        _familyListSchema,
+    );
+
+    const dependencyInfoForm = await superValidate(
+        dependencyInfoResponse.data?.details as CandidateDependenciesDetailsDTO,
+        _dependencyListSchema,
+    );
+    const nextOfKinInfoForm = await superValidate(
+        nextOfKinInfoResponse.data?.details as CandidateNextOfKinDetailsDTO,
+        _nextOfKinListSchema,
+    );
+
+    const addAcademicModal = await superValidate(_academicInfoSchema);
+    const addExperienceModal = await superValidate(_experienceInfoSchema);
+    const addActivityModal = await superValidate(_activityInfoSchema);
+    const addFamilyModal = await superValidate(_familyListSchema);
+    const addNonFamilyModal = await superValidate(_dependencyListSchema);
+    const addNextOfKinModal = await superValidate(_nextOfKinListSchema);
+
     const serviceInfoForm = await superValidate(
         serviceDetails,
         _serviceInfoSchema,
@@ -211,12 +155,12 @@ export async function load({ params }) {
 
     const secretaryApprovalInfoForm = await superValidate(
         secretaryApprovalDetails,
-        _secretaryApprovalInfoSchema,
+        _approvalResultSchema,
     );
 
     const secretarySetApproversForm = await superValidate(
-        secretaryGetApproversResponse.data as SupporterApprovalData,
-        _secretarySetApproversSchema,
+        secretaryGetApproversResponse.data?.details as NewHireGetApproversDTO,
+        _setApproversSchema,
     );
 
     serviceInfoForm.data.candidateId = candidateIdRequestBody.candidateId;
@@ -224,15 +168,22 @@ export async function load({ params }) {
         candidateIdRequestBody.candidateId;
 
     return {
+        personalDetailResponse,
+        personalInfoForm,
+        academicInfoResponse,
+        academicInfoForm,
+        experienceInfoResponse,
+        experienceInfoForm,
+        activityInfoResponse,
+        activityInfoForm,
+        familyInfoResponse,
+        familyInfoForm,
+        dependencyInfoResponse,
+        dependencyInfoForm,
+        nextOfKinInfoResponse,
+        nextOfKinInfoForm,
         supporterResultResponse,
         approverResultResponse,
-        personalDetailResponse,
-        academicInfoResponse,
-        experienceInfoResponse,
-        activityInfoResponse,
-        familyInfoResponse,
-        dependencyInfoResponse,
-        nextOfKinInfoResponse,
         serviceResponse,
         serviceInfoForm,
         documentInfoResponse,
@@ -240,10 +191,187 @@ export async function load({ params }) {
         secretaryApprovalInfoForm,
         secretaryGetApproversResponse,
         secretarySetApproversForm,
+        addAcademicModal,
+        addExperienceModal,
+        addActivityModal,
+        addFamilyModal,
+        addNonFamilyModal,
+        addNextOfKinModal,
     };
 }
 
-export const _submitServiceInfoForm = async (formData: object) => {
+//==================================================
+//=============== Maklumat Peribadi ================
+//==================================================
+// export const _submitFormStepperMaklumatPeribadi = async (formData: object) => {
+//     const form = await superValidate(formData, _stepperMaklumatPeribadi);
+
+//     if (!form.valid) {
+//         getErrorToast();
+//         return fail(400, form);
+//     }
+
+//     // start by rendering loading toast
+//     getLoadingToast();
+
+//     const response: CommonResponseDTO = await ProfileService.editPersonalDetail(
+//         form.data as PutPersonalDetailRequest,
+//     ).finally(() => toast.dismiss());
+//     console.log(response);
+
+//     if (response.status !== 'success') {
+//         // if error toast
+//         getServerErrorToast();
+//         // return error(400, { message: response.message });
+//     }
+//     // if success toast
+//     getSuccessToast();
+//     invalidateAll();
+//     return { form };
+// };
+
+//==================================================
+//=============== Create Academic ================
+//==================================================
+// export const _submitCreateAcademicForm = async (formData: object) => {
+//     const form = await superValidate(formData, _addAcademicInfoSchema);
+
+//     if (!form.valid) {
+//         getErrorToast();
+//         return fail(400, form);
+//     }
+
+//     // start by rendering loading toast
+//     getLoadingToast();
+
+//     const response: CommonResponseDTO =
+//         await ProfileService.createAcademicDetail(
+//             form.data as PostPersonalAcademicRequest,
+//         ).finally(() => toast.dismiss());
+
+//     if (response.status !== 'success') {
+//         // if error toast
+//         getServerErrorToast();
+//     }
+//     // if success toast
+//     getSuccessToast();
+//     invalidateAll();
+//     return { form };
+// };
+
+// export const _submitCreateExperienceForm = async (formData: object) => {
+//     const form = await superValidate(formData, _addExperienceInfoSchema);
+
+//     if (!form.valid) {
+//         getErrorToast();
+//         return fail(400, form);
+//     }
+
+//     // start by rendering loading toast
+//     getLoadingToast();
+
+//     const response: CommonResponseDTO =
+//         await ProfileService.createExperienceDetail(
+//             form.data as PostPersonalExperiencesRequest,
+//         ).finally(() => toast.dismiss());
+
+//     if (response.status !== 'success') {
+//         // if error toast
+//         getServerErrorToast();
+//     }
+//     // if success toast
+//     getSuccessToast();
+//     invalidateAll();
+//     return { form };
+// };
+
+// export const _submitCreateActivityForm = async (formData: object) => {
+//     const form = await superValidate(formData, _addActivityModalSchema);
+
+//     console.log(form);
+
+//     if (!form.valid) {
+//         getErrorToast();
+//         return fail(400, form);
+//     }
+
+//     // start by rendering loading toast
+//     getLoadingToast();
+
+//     const response: CommonResponseDTO =
+//         await ProfileService.createActivityDetail(
+//             form.data as PostPersonalActivityRequest,
+//         ).finally(() => toast.dismiss());
+
+//     if (response.status !== 'success') {
+//         // if error toast
+//         getServerErrorToast();
+//     }
+//     // if success toast
+//     getSuccessToast();
+//     invalidateAll();
+
+//     return { form };
+// };
+
+// export const _submitCreateFamilyForm = async (formData: object) => {
+//     const form = await superValidate(formData, _addRelationModalSchema);
+
+//     if (!form.valid) {
+//         getErrorToast();
+//         return fail(400, form);
+//     }
+
+//     // start by rendering loading toast
+//     getLoadingToast();
+
+//     const response: CommonResponseDTO = await ProfileService.createFamilyDetail(
+//         form.data as PostPersonalFamilyRequest,
+//     ).finally(() => toast.dismiss());
+
+//     if (response.status !== 'success') {
+//         // if error toast
+//         getServerErrorToast();
+//     }
+//     // if success toast
+//     getSuccessToast();
+//     invalidateAll();
+//     return { form };
+// };
+
+export const _submitCreateDependencyForm = async (formData: object) => {
+    const form = await superValidate(formData, _dependencyListSchema);
+
+    if (!form.valid) {
+        getErrorToast();
+        error(400, { message: 'Validation Not Passed!' });
+    }
+
+    const response: CommonResponseDTO =
+        await EmploymentServices.createCurrentCandidateDependenciesDetails(
+            form.data as CandidateDependenciesDetailsDTO,
+        );
+
+    return { response };
+};
+
+export const _submitNextOfKinForm = async (formData: object) => {
+    const form = await superValidate(formData, _nextOfKinListSchema);
+
+    if (!form.valid) {
+        getErrorToast();
+        error(400, { message: 'Validation Not Passed!' });
+    }
+
+    const response: CommonResponseDTO =
+        await EmploymentServices.createCurrentCandidateNextOfKinDetails(
+            form.data as CandidateNextOfKinDetailsDTO,
+        );
+
+    return { response };
+};
+
+export const _submitServiceForm = async (formData: object) => {
     const form = await superValidate(formData, _serviceInfoSchema);
 
     if (!form.valid) {
@@ -251,15 +379,16 @@ export const _submitServiceInfoForm = async (formData: object) => {
         error(400, { message: 'Validation Not Passed!' });
     }
 
-    const response: CommonResponseDTO =  await EmploymentServices.createCurrentCandidateSecretaryUpdate(
-        form.data as NewHireSecretaryAddUpdateRequestBody,
-    )
+    const response: CommonResponseDTO =
+        await EmploymentServices.createCurrentCandidateSecretaryUpdate(
+            form.data as NewHireSecretaryServiceUpdateDTO,
+        );
 
     return { response };
 };
 
 export const _submitSecretaryApprovalForm = async (formData: object) => {
-    const form = await superValidate(formData, _secretaryApprovalInfoSchema);
+    const form = await superValidate(formData, _approvalResultSchema);
 
     if (!form.valid) {
         getErrorToast();
@@ -269,13 +398,13 @@ export const _submitSecretaryApprovalForm = async (formData: object) => {
     const response: CommonResponseDTO =
         await EmploymentServices.createCurrentCandidateSecretaryApproval(
             form.data as AddApprovalResultRequestBody,
-        )
+        );
 
     return { response };
 };
 
 export const _submitSecretarySetApproverForm = async (formData: object) => {
-    const form = await superValidate(formData, _secretarySetApproversSchema);
+    const form = await superValidate(formData, _setApproversSchema);
 
     if (!form.valid) {
         getErrorToast();
@@ -284,8 +413,8 @@ export const _submitSecretarySetApproverForm = async (formData: object) => {
 
     const response: CommonResponseDTO =
         await EmploymentServices.createCurrentCandidateApprovers(
-            form.data as NewHireSetApproversRequestBody,
-        )
+            form.data as NewHireSetApproversDTO,
+        );
 
     return { response };
 };
