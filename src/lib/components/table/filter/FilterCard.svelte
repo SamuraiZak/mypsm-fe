179b001ec6b943cd1b1e0b1a4d84c1d2a7009ea3
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-02-26 16:49:11 +0800

    feat: Added table filter components

---
+++ src/lib/components/table/filter/FilterCard.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    import SvgManifyingGlass from '$lib/assets/svg/SvgManifyingGlass.svelte';
+
+    export let onSearch = () => {};
+    export let cardTitle: string = 'Tetapan Pencarian';
+</script>
+
+<div
+    class="flex h-fit w-full max-w-full flex-col items-center justify-start gap-2 rounded-md border border-ios-activeColors-activeBlue-light bg-ios-basic-white p-2 pb-4"
+>
+    <!-- card header -->
+    <div class="flex h-10 min-h-10 w-full flex-row justify-between border-b">
+        <!-- leading -->
+        <div class="flex h-full max-w-full flex-col justify-center">
+            <span class="text-wrap text-sm font-semibold">
+                {cardTitle}
+            </span>
+        </div>
+        <!-- trailing -->
+        <div class="flex h-full max-w-full flex-col justify-center">
+            <!-- search button -->
+            <button
+                on:click={() => {
+                    onSearch();
+                }}
+                class="flex h-7 min-h-7 flex-row items-center justify-center gap-1 rounded-[3px] bg-ios-activeColors-activeBlue-light px-2.5 py-0"
+            >
+                <!-- icon -->
+                <div class="flex h-full flex-row items-center justify-center">
+                    <span class="leading-loose text-ios-basic-white">
+                        <!-- icon slot -->
+                        <SvgManifyingGlass></SvgManifyingGlass>
+                    </span>
+                </div>
+
+                <!-- label -->
+                <div class="flex h-full flex-row items-center justify-center">
+                    <p
+                        class="text-sm font-normal leading-loose text-ios-basic-white"
+                    >
+                        Cari
+                    </p>
+                </div>
+            </button>
+        </div>
+    </div>
+    <!-- card body -->
+    <div
+        class="flex h-fit w-full max-w-full flex-row flex-wrap justify-start gap-4"
+    >
+        <slot />
+    </div>
+</div>
---
+++ src/lib/components/table/filter/FilterNumberField.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    export let inputValue: number = 0;
+    export let label: string = 'Label';
+    export let inputId: string = 'input';
+    export let disabled: boolean = false;
+</script>
+
+<div class="h-fit w-52 min-w-52 max-w-52 gap-px px-2">
+    <label
+        for={inputId}
+        class="block text-sm font-normal text-ios-labelColors-label-light"
+    >
+        {label}
+    </label>
+    <input
+        {disabled}
+        id={inputId}
+        type="number"
+        bind:value={inputValue}
+        class="{disabled
+            ? 'bg-ios-systemColors-quaternarySystemFill-light'
+            : 'bg-ios-basic-white'} autofill:hide-default-inner-shadow focus:ring-none block h-7 w-full rounded-[3px] border border-ios-labelColors-separator-light bg-ios-basic-white p-2.5 text-sm hover:border-ios-activeColors-activeBlue-light focus:border-ios-activeColors-activeBlue-light focus:ring-0"
+    />
+</div>
