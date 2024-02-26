9179053064b74e31a7a4b26d4b3a7b7dff92b9b8
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-02-26 16:27:00 +0800

    feat: creates a filtercontainer component.

--- src/lib/components/filter-container/FilterContainer.svelte
+++ src/lib/components/filter-container/FilterContainer.svelte
@@ -1,43 +1,22 @@
 <script>
-    import { Card } from 'flowbite-svelte';
-    import FormButton from '../buttons/FormButton.svelte';
-    import CustomCard from '../cards/CustomCard.svelte';
-    import CustomCardBody from '../cards/CustomCardBody.svelte';
+    import TextIconButton from '../button/TextIconButton.svelte';
     import SvgManifyingGlass from '$lib/assets/svg/SvgManifyingGlass.svelte';
-    import TextIconButton from '../buttons/TextIconButton.svelte';
-    import CustomCardHeader from '../cards/CustomCardHeader.svelte';
+    import ContentHeader from '../headers/ContentHeader.svelte';
+    import CustomCard from '../card/CustomCard.svelte';
     export let onFilter = () => {};
 </script>
 
-<!-- <Card class="mb-[10px] min-w-full border-system-primary" shadow={false}>
-    <form class="flex flex-col space-y-6" action="">
-        <div class="flex items-center justify-between">
-            <p class="text-sm font-semibold text-gray-900 dark:text-white">
-                Semua Tetapan Pencarian
-            </p>
-            <FormButton
-                type="find"
-                onClick={() => {
-                    console.log('Cari'); // Placeholder for search action
-                }}
-            />
-        </div>
-        <hr class="border-system-accent" />
-        <div class="flex flex-wrap items-center gap-x-8 gap-y-5">
-            <slot />
-        </div>
-    </form>
-</Card> -->
-
 <CustomCard borderClass="border-system-primary">
-    <CustomCardHeader title="Semua Tetapan Pencarian">
-        <TextIconButton label="Cari" primary onClick={onFilter}>
+    <ContentHeader title="Semua Tetapan Pencarian">
+        <TextIconButton label="Cari" type="primary" onClick={onFilter}>
             <SvgManifyingGlass></SvgManifyingGlass>
         </TextIconButton>
-    </CustomCardHeader>
-    <CustomCardBody>
+    </ContentHeader>
+    <div
+        class="flex h-full min-h-[40px] w-full flex-col items-center justify-start p-2.5"
+    >
         <div class="flex w-full flex-wrap gap-2.5">
             <slot />
         </div>
-    </CustomCardBody>
+    </div>
 </CustomCard>
