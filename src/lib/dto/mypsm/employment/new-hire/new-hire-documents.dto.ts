7a2c0339bc2f6aaa4a551da1369c0b3c936f79ab
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-02-27 17:38:46 +0800

    feat: create uploaded document dto

--- src/lib/dto/mypsm/employment/new-hire/new-hire-documents.dto.ts
+++ src/lib/dto/mypsm/employment/new-hire/new-hire-documents.dto.ts
@@ -1,4 +1,5 @@
-import type { _documentsSchema } from '$lib/schemas/mypsm/employment/new-hire/schema';
+import type { _documentsSchema, _uploadDocumentsSchema } from '$lib/schemas/mypsm/employment/new-hire/schema';
 import type { z } from 'zod';
 
 export type NewHireDocumentsDTO = z.infer<typeof _documentsSchema>;
+export type UploadedDocumentsDTO = z.infer<typeof _uploadDocumentsSchema>;
