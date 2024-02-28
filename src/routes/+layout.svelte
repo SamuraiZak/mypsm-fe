d1bc06bb6689fb8fd59910de1b5efe87991a216a
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-02-28 10:10:57 +0800

    feat: adds a dynamic loading overlay when nagivating.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,28 @@
 <script lang="ts">
+    import LoadingOverlay from '$lib/components/loading-overlay/LoadingOverlay.svelte';
+    import { afterUpdate } from 'svelte';
     import '../app.css';
+    import { navigating } from '$app/stores';
+
+    let showLoadingOverlay: boolean = false;
+
+    afterUpdate(() => {
+        // Set a timeout to show the LoadingOverlay if still navigating after 200ms
+        const timeoutId = setTimeout(() => {
+            if ($navigating) {
+                showLoadingOverlay = true;
+            }
+        }, 200);
+
+        if ($navigating === null) showLoadingOverlay = false;
+
+        // Clear the timeout when loaded
+        return () => clearTimeout(timeoutId);
+    });
 </script>
+
+{#if $navigating && showLoadingOverlay}
+    <LoadingOverlay />
+{/if}
 
 <slot />
