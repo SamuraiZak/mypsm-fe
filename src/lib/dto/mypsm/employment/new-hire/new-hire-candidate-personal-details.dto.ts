d4e9bfdeea6b8dc6a70928c0944939ae71e617ee
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-02-29 11:10:04 +0800

    feat: currates personaldetail dto to response and request.

--- src/lib/dto/mypsm/employment/new-hire/new-hire-candidate-personal-details.dto.ts
+++ src/lib/dto/mypsm/employment/new-hire/new-hire-candidate-personal-details.dto.ts
@@ -1,4 +1,9 @@
 import type { _personalInfoSchema } from '$lib/schemas/mypsm/employment/new-hire/schema';
 import type { z } from 'zod';
 
-export type CandidatePersonalDTO = z.infer<typeof _personalInfoSchema>;
+export type CandidatePersonalResponseDTO = z.infer<typeof _personalInfoSchema>;
+
+export type CandidatePersonalRequestDTO = Omit<
+    CandidatePersonalResponseDTO,
+    'id' | 'isReadOnly'
+>;
