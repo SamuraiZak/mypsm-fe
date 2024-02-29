3d56359535961bd6120b1dac0a73552b2529f557
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-02-29 11:10:20 +0800

    feat: currates secretaryserviceupdate dto to response and request.

--- src/lib/dto/mypsm/employment/new-hire/new-hire-secretary-service-update.dto.ts
+++ src/lib/dto/mypsm/employment/new-hire/new-hire-secretary-service-update.dto.ts
@@ -1,4 +1,12 @@
-import type { _serviceInfoSchema } from "$lib/schemas/mypsm/employment/new-hire/schema";
-import type { z } from "zod";
+import type { _serviceInfoSchema } from '$lib/schemas/mypsm/employment/new-hire/schema';
+import type { z } from 'zod';
 
-export type NewHireSecretaryServiceUpdateDTO = z.infer<typeof _serviceInfoSchema>;
+export type NewHireSecretaryServiceUpdateResponseDTO = Omit<
+    z.infer<typeof _serviceInfoSchema>,
+    'candidateId'
+>;
+
+export type NewHireSecretaryServiceUpdateRequestDTO = Omit<
+    z.infer<typeof _serviceInfoSchema>,
+    'isReadOnly'
+>;
