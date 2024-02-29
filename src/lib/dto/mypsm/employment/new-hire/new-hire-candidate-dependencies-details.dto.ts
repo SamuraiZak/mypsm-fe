d994450678718c8d79417675361ecb0aa9c4bc4f
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-02-29 11:09:30 +0800

    feat: currates dependency dto to response and request.

--- src/lib/dto/mypsm/employment/new-hire/new-hire-candidate-dependencies-details.dto.ts
+++ src/lib/dto/mypsm/employment/new-hire/new-hire-candidate-dependencies-details.dto.ts
@@ -4,7 +4,13 @@ import type {
 } from '$lib/schemas/mypsm/employment/new-hire/schema';
 import type { z } from 'zod';
 
-export type CandidateDependenciesDetailsDTO = z.infer<
+export type CandidateDependenciesDetailResponseDTO = z.infer<
     typeof _dependencyListSchema
 >;
+
+export type CandidateDependenciesDetailRequestDTO = Pick<
+    CandidateDependenciesDetailResponseDTO,
+    'dependenciesList'
+>;
+
 export type Dependency = z.infer<typeof _relationsSchema>;
