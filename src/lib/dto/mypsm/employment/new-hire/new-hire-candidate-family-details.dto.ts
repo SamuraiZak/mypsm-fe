142884b2100777b741834e8acebafed7adc1ef3d
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-02-29 11:09:44 +0800

    feat: currates family dto to response and request.

--- src/lib/dto/mypsm/employment/new-hire/new-hire-candidate-family-details.dto.ts
+++ src/lib/dto/mypsm/employment/new-hire/new-hire-candidate-family-details.dto.ts
@@ -4,6 +4,13 @@ import type {
 } from '$lib/schemas/mypsm/employment/new-hire/schema';
 import type { z } from 'zod';
 
-export type CandidateFamilyDetailsDTO = z.infer<typeof _familyListSchema>;
+export type CandidateFamilyDetailResponseDTO = z.infer<
+    typeof _familyListSchema
+>;
+
+export type CandidateFamilyDetailRequestDTO = Pick<
+    CandidateFamilyDetailResponseDTO,
+    'dependenciesList'
+>;
 
 export type Family = z.infer<typeof _relationsSchema>;
