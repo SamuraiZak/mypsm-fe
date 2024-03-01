7809234e786c9bc782c5f6b1443340ea00e80bec
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-03-01 09:00:20 +0800

    feat: Modified leave services

--- src/lib/services/implementation/mypsm/cuti/leave.service.ts
+++ src/lib/services/implementation/mypsm/cuti/leave.service.ts
@@ -8,10 +8,68 @@ import {
     type CommonListRequestDTO,
 } from '$lib/dto/core/common/common-list-request.dto';
 import { CommonResponseConvert } from '$lib/dto/core/common/common-response.dto';
+import {
+    LeaveEntitlementRequestConvert,
+    type LeaveEntitlementRequestDTO,
+} from '$lib/dto/mypsm/cuti/report/leave-entitlement-request.dto';
+import {
+    LeaveEntitlementConvert,
+    type LeaveEntitlementDTO,
+} from '$lib/dto/mypsm/cuti/report/leave-entitlement.dto';
+import {
+    LeaveHistoryRequestConvert,
+    type LeaveHistoryRequestDTO,
+} from '$lib/dto/mypsm/cuti/report/leave-history-request.dto';
 import type { Input } from 'ky';
 import http from '../../service-provider.service';
 
 export class LeaveServices {
+    // get leave entitlement
+    static async getLeaveEntitlement(param: CommonListRequestDTO) {
+        try {
+            let url: Input = 'leave/report/entitlement';
+
+            const response: Response = await http
+                .post(url, {
+                    body: CommonListRequestConvert.toJson(param),
+                })
+                .json();
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get leave history
+    static async getLeaveHistory(param: CommonListRequestDTO) {
+        try {
+            let url: Input = 'leave/report/history';
+
+            const response: Response = await http
+                .post(url, {
+                    body: CommonListRequestConvert.toJson(param),
+                })
+                .json();
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
     // POST : get leave application list
     static async getLeaveApplicationList(param: CommonListRequestDTO) {
         try {
