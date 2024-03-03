709f63be3233eb617aa96869ff2dc338e0cd89c6
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-03-03 15:55:13 +0800

    feat: WIP elaun modules

---
+++ src/routes/(guarded)/elaun-elaun-perkhidmatan/permohonan/+page.ts
@@ -0,0 +1,66 @@
+import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key.constant';
+import type { CommonListRequestDTO } from '$lib/dto/core/common/common-list-request.dto';
+import type { CommonResponseDTO } from '$lib/dto/core/common/common-response.dto';
+import type { ServiceAllowanceFilterDTO } from '$lib/dto/mypsm/elaun-elaun-perkhidmatan/service-allowance-filter.dto';
+import type { ServiceAllowanceDTO } from '$lib/dto/mypsm/elaun-elaun-perkhidmatan/service-allowance.dto';
+import { ServiceAllowanceServices } from '$lib/services/implementation/mypsm/elaun-elaun-perkhidmatan/service-allowance.service';
+
+export async function load() {
+    // get current role code
+    let currentRoleCode: string =
+        localStorage.getItem(LocalStorageKeyConstant.currentRoleCode) ??
+        'unknown';
+
+    // prepare empty allowance list
+    let allowanceList: ServiceAllowanceDTO[] = [];
+
+    // list request filter
+    const allowanceListFilter: ServiceAllowanceFilterDTO = {
+        employeeNumber: null,
+        name: null,
+        allowanceType: null,
+        applicationDate: null,
+        status: null,
+    };
+
+    // list request body
+    const allowanceListRequestBody: CommonListRequestDTO = {
+        pageNum: 1,
+        pageSize: 5,
+        orderBy: null,
+        orderType: null,
+        filter: allowanceListFilter,
+    };
+
+    // fetch the leave History
+    const allowanceListResponse: CommonResponseDTO =
+        await ServiceAllowanceServices.getAllowanceList(
+            allowanceListRequestBody,
+        );
+
+    if (allowanceListResponse.status == 'success') {
+        allowanceList = allowanceListResponse.data
+            ?.dataList as ServiceAllowanceDTO[];
+    }
+
+    return {
+        props: {
+            currentRoleCode,
+            allowanceList,
+            allowanceListRequestBody,
+            allowanceListResponse,
+        },
+    };
+}
+
+export async function _updateAllowanceTable(param: CommonListRequestDTO) {
+    const response: CommonResponseDTO =
+        await ServiceAllowanceServices.getAllowanceList(param);
+
+    return {
+        props: {
+            param,
+            response,
+        },
+    };
+}
---
+++ src/routes/(guarded)/elaun-elaun-perkhidmatan/permohonan/[id]/+page.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import TextIconButton from '$lib/components/button/TextIconButton.svelte';
+    import ContentHeader from '$lib/components/headers/ContentHeader.svelte';
+    import CustomSelectField from '$lib/components/inputs/select-field/CustomSelectField.svelte';
+    import CustomTextField from '$lib/components/inputs/text-field/CustomTextField.svelte';
+    import Stepper from '$lib/components/stepper/Stepper.svelte';
+    import StepperContent from '$lib/components/stepper/StepperContent.svelte';
+    import StepperContentBody from '$lib/components/stepper/StepperContentBody.svelte';
+    import StepperContentHeader from '$lib/components/stepper/StepperContentHeader.svelte';
+    import { ServiceAllowanceTypeConstant } from '$lib/constants/core/service-allowance.constant';
+    import type { PageData } from './$types';
+
+    export let data: PageData;
+
+    let selectedAllowanceType: string =
+        ServiceAllowanceTypeConstant.culturalClothesAllowance.code;
+</script>
+
+<section class="flex w-full flex-col items-start justify-start">
+    <ContentHeader title="Permohonan Elaun-elaun Perkhidmatan">
+        <TextIconButton
+            type="danger"
+            label="Kembali"
+            icon="previous"
+            onClick={() => {
+                goto('/elaun-elaun-perkhidmatan/permohonan');
+            }}
+        ></TextIconButton>
+    </ContentHeader>
+</section>
+
+<section
+    class="flex h-full w-full flex-col items-center justify-start overflow-y-auto"
+>
+    <Stepper>
+        <StepperContent>
+            <StepperContentHeader title="Butiran Permohonan Elaun"
+            ></StepperContentHeader>
+            <StepperContentBody>
+                <div
+                    class="flex h-full w-full flex-col items-start justify-start"
+                >
+                    <form
+                        id="allowanceForm"
+                        method="POST"
+                        class="flex w-full flex-col items-center justify-start space-y-2 p-2 lg:w-1/2"
+                    >
+                        <div class="flex w-full flex-col gap-2">
+                            <CustomSelectField
+                                id="allowanceType"
+                                label={'Jenis Elaun'}
+                                bind:val={selectedAllowanceType}
+                                options={data.props.allowanceTypeDropdown}
+                            ></CustomSelectField>
+                        </div>
+                    </form>
+                </div>
+            </StepperContentBody>
+        </StepperContent>
+    </Stepper>
+</section>
