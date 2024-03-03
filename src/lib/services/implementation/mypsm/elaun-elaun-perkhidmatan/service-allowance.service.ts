0808a8e2b80204f13682d251dc37d8a99a125e4f
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-03-03 15:54:07 +0800

    feat: Added service allowance services

--- src/lib/services/implementation/mypsm/elaun-elaun-perkhidmatan/service-allowance.service.ts
+++ src/lib/services/implementation/mypsm/elaun-elaun-perkhidmatan/service-allowance.service.ts
@@ -2,34 +2,36 @@
 // Service Allowance Services
 // ===============================================================
 
-import { CommonResponseConstant } from "$lib/constants/core/common-response.constant";
-import { type CommonListRequestDTO, CommonListRequestConvert } from "$lib/dto/core/common/common-list-request.dto";
-import { CommonResponseConvert } from "$lib/dto/core/common/common-response.dto";
-import type { Input } from "ky";
-import http from "$lib/services/implementation/service-provider.service";
+import { CommonResponseConstant } from '$lib/constants/core/common-response.constant';
+import {
+    CommonListRequestConvert,
+    type CommonListRequestDTO,
+} from '$lib/dto/core/common/common-list-request.dto';
+import { CommonResponseConvert } from '$lib/dto/core/common/common-response.dto';
+import http from '$lib/services/implementation/service-provider.service';
+import type { Input } from 'ky';
 
 export class ServiceAllowanceServices {
-
     // POST: get list of allowance
-    static async method(param: CommonListRequestDTO) {
-            try {
-                let url: Input = '';
-    
-                const response: Response = await http
-                    .post(url, {
-                        body: CommonListRequestConvert.toJson(param),
-                    })
-                    .json();
-    
-                const result = CommonResponseConvert.fromResponse(response);
-    
-                if (result.status == 'success') {
-                    return result;
-                } else {
-                    return CommonResponseConstant.httpError;
-                }
-            } catch (error) {
+    static async getAllowanceList(param: CommonListRequestDTO) {
+        try {
+            let url: Input = 'service_allowance/list';
+
+            const response: Response = await http
+                .post(url, {
+                    body: CommonListRequestConvert.toJson(param),
+                })
+                .json();
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
                 return CommonResponseConstant.httpError;
             }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
         }
+    }
 }
