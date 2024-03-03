788ec7b690c44101ad540f73c8ab11d191f81748
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-03-03 15:53:45 +0800

    feat: modified helper functions

--- src/lib/helpers/core/lookup.helper.ts
+++ src/lib/helpers/core/lookup.helper.ts
@@ -4,12 +4,21 @@
 
 import type { DropdownDTO } from '$lib/dto/core/dropdown/dropdown.dto';
 import type { LookupDTO } from '$lib/dto/core/lookup/lookup.dto';
+import { TextAppearanceHelper } from './text-appearance.helper';
 
 export class LookupHelper {
     static toDropdown(lookupList: LookupDTO[]) {
         const dropdownList: DropdownDTO[] = lookupList.map((lookup) => ({
             value: lookup.code,
             name: lookup.description,
+        }));
+
+        return dropdownList;
+    }
+    static toDropdownProper(lookupList: LookupDTO[]) {
+        const dropdownList: DropdownDTO[] = lookupList.map((lookup) => ({
+            value: lookup.code,
+            name: TextAppearanceHelper.toProper(lookup.description),
         }));
 
         return dropdownList;
