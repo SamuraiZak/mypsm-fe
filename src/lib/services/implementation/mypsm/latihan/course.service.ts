7a94902906051747000937c74d16f7a54575aa46
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-03-05 17:42:30 +0800

    feat: creates course - exams services

--- src/lib/services/implementation/mypsm/latihan/course.service.ts
+++ src/lib/services/implementation/mypsm/latihan/course.service.ts
@@ -2,26 +2,316 @@
 // Course Services
 // ===============================================================
 
+import { invalidateAll } from '$app/navigation';
 import { CommonResponseConstant } from '$lib/constants/core/common-response.constant';
 import {
     CommonListRequestConvert,
     type CommonListRequestDTO,
 } from '$lib/dto/core/common/common-list-request.dto';
 import { CommonResponseConvert } from '$lib/dto/core/common/common-response.dto';
+import type { commonIdRequestDTO } from '$lib/dto/core/common/id-request.dto';
+import type { CourseExamApplicationApprovalDTO } from '$lib/dto/mypsm/course/exam/course-exam-application-approval.dto';
+import type {
+    CourseAddExamApplicationRequestDTO,
+    CourseSetExamAttendanceRequestDTO,
+} from '$lib/dto/mypsm/course/exam/course-exam-application.dto';
+import type { CourseCreateExamRequestDTO } from '$lib/dto/mypsm/course/exam/course-exam.dto';
+import { getPromiseToast } from '$lib/helpers/core/toast.helper';
 import http from '$lib/services/implementation/service-provider.service';
 import type { Input } from 'ky';
 
 export class CourseServices {
-    static async method(param: CommonListRequestDTO) {
+    // get list of exams
+    static async getCourseExamList(param: CommonListRequestDTO) {
         try {
-            let url: Input = '';
+            const url: Input = 'course/exam/list';
 
+            // get the promise response
             const response: Response = await http
                 .post(url, {
                     body: CommonListRequestConvert.toJson(param),
                 })
                 .json();
 
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get course exam details
+    static async getCourseExamDetail(param: commonIdRequestDTO) {
+        try {
+            const url: Input = 'course/exam/get';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // create exam record
+    static async createCourseExam(param: CourseCreateExamRequestDTO) {
+        try {
+            const url: Input = 'course/exam/add';
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add exam application record
+    static async addCourseExamApplication(
+        param: CourseAddExamApplicationRequestDTO,
+    ) {
+        try {
+            const url: Input = 'course/exam_application/add';
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get list of exam applications
+    static async getCourseExamApplicationList(param: CommonListRequestDTO) {
+        try {
+            const url: Input = 'course/exam_application/list';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: CommonListRequestConvert.toJson(param),
+                })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get course exam application details
+    static async getCourseExamApplicationDetail(param: commonIdRequestDTO) {
+        try {
+            const url: Input = 'course/exam_application/get';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // set attendance exam application
+    static async setAttendanceCourseExamApplication(
+        param: CourseSetExamAttendanceRequestDTO,
+    ) {
+        try {
+            const url: Input = 'course/exam_application/add';
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get personal detail exam application details
+    static async getCourseExamApplicationPersonalDetail(
+        param: commonIdRequestDTO,
+    ) {
+        try {
+            const url: Input = 'course/exam_application/personal_detail';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get service detail exam application details
+    static async getCourseExamApplicationServiceDetail(
+        param: commonIdRequestDTO,
+    ) {
+        try {
+            const url: Input = 'course/exam_application/employee_detail';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // set secretary approval for exam application
+    static async setCourseExamApplicationSecretaryApproval(
+        param: CourseExamApplicationApprovalDTO,
+    ) {
+        try {
+            const url: Input = 'course/exam_application/secretary_approval/add';
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get secretary approval exam application
+    static async getCourseExamApplicationSecreataryApproval(
+        param: commonIdRequestDTO,
+    ) {
+        try {
+            const url: Input = 'course/exam_application/secretary_approval/get';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // parse the json response to object
             const result = CommonResponseConvert.fromResponse(response);
 
             if (result.status == 'success') {
