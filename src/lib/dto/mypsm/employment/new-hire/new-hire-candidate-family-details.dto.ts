4e466123dad5bd1ce9ac1322bfbd6ccff220cc24
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-03-05 17:41:42 +0800

    fix: fixes typo

--- src/lib/dto/mypsm/employment/new-hire/new-hire-candidate-family-details.dto.ts
+++ src/lib/dto/mypsm/employment/new-hire/new-hire-candidate-family-details.dto.ts
@@ -1,12 +1,12 @@
 import type {
+    _dependencyListResponseSchema,
     _familyListRequestSchema,
-    _familyListResponseSchema,
     _relationsSchema,
 } from '$lib/schemas/mypsm/employment/new-hire/schema';
 import type { z } from 'zod';
 
 export type CandidateFamilyDetailResponseDTO = z.infer<
-    typeof _familyListResponseSchema
+    typeof _dependencyListResponseSchema
 >;
 
 export type CandidateFamilyDetailRequestDTO = z.infer<
