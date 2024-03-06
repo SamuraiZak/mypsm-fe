e8d42f6a226890c97ffd5b1c84465a77a95f038f
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-03-06 17:44:33 +0800

    feat: creates page for add new exam

---
+++ src/routes/(guarded)/latihan/kemaskini-peperiksaan/tambah-peperiksaan/+page.svelte
@@ -0,0 +1,127 @@
+<script lang="ts">
+    import { _examInfoResponseSchema } from '$lib/schemas/mypsm/course/schema';
+    import { zod } from 'sveltekit-superforms/adapters';
+    import CustomSelectField from '$lib/components/inputs/select-field/CustomSelectField.svelte';
+    import Stepper from '$lib/components/stepper/Stepper.svelte';
+    import StepperContent from '$lib/components/stepper/StepperContent.svelte';
+    import StepperContentBody from '$lib/components/stepper/StepperContentBody.svelte';
+    import StepperContentHeader from '$lib/components/stepper/StepperContentHeader.svelte';
+    import { Toaster } from 'svelte-french-toast';
+    import { dateProxy, superForm } from 'sveltekit-superforms/client';
+    import type { PageData } from './$types';
+    import ContentHeader from '$lib/components/headers/ContentHeader.svelte';
+    import CustomTextField from '$lib/components/inputs/text-field/CustomTextField.svelte';
+    import TextIconButton from '$lib/components/button/TextIconButton.svelte';
+    import {
+        _academicInfoSchema,
+        _serviceInfoRequestSchema,
+    } from '$lib/schemas/mypsm/employment/new-hire/schema';
+    import { goto } from '$app/navigation';
+    import { _createExamForm } from './+page';
+    export let data: PageData;
+
+    // Superforms
+    const { form, errors, enhance } = superForm(data.examInfoForm, {
+        SPA: true,
+        dataType: 'json',
+        invalidateAll: true,
+        resetForm: true,
+        multipleSubmits: 'prevent',
+        validationMethod: 'oninput',
+        validators: zod(_examInfoResponseSchema),
+        onSubmit(formData) {
+            _createExamForm(formData.formData);
+        },
+        taintedMessage: false,
+    });
+
+    const proxyExamApplicationStartDate = dateProxy(form, 'startDate', {
+        format: 'date',
+    });
+    const proxyExamApplicationEndDate = dateProxy(form, 'endDate', {
+        format: 'date',
+    });
+    const proxyExamDate = dateProxy(form, 'examDate', { format: 'date' });
+</script>
+
+<ContentHeader title="Maklumat Lantikan Baru"
+    ><TextIconButton
+        label="Kembali"
+        type="neutral"
+        onClick={() => {
+            goto('../../latihan/kemaskini-peperiksaan');
+        }}
+    /></ContentHeader
+>
+<Stepper>
+    <StepperContent>
+        <StepperContentHeader title="Maklumat Peperiksaan LKIM">
+            <TextIconButton
+                type="primary"
+                label="Simpan"
+                form="examFormStepper"
+            />
+        </StepperContentHeader>
+        <StepperContentBody
+            ><!-- Maklumat Peperiksaan -->
+            <form
+                id="examFormStepper"
+                method="POST"
+                use:enhance
+                class="flex w-full flex-col gap-2"
+            >
+                <CustomSelectField
+                    disabled={false}
+                    errors={$errors.examTypeId}
+                    id="examTypeId"
+                    label="Jenis Peperiksaan"
+                    bind:val={$form.examTypeId}
+                    options={data.selectionOptions.examTypeLookup}
+                ></CustomSelectField>
+                <CustomTextField
+                    disabled={false}
+                    errors={$errors.examTitle}
+                    id="examTitle"
+                    label="Tajuk Peperiksaan"
+                    type="text"
+                    bind:val={$form.examTitle}
+                ></CustomTextField>
+
+                <CustomTextField
+                    disabled={false}
+                    errors={$errors.startDate}
+                    id="startDate"
+                    label="Tarikh Mula Permohonan"
+                    type="date"
+                    bind:val={$proxyExamApplicationStartDate}
+                ></CustomTextField>
+                <CustomTextField
+                    disabled={false}
+                    errors={$errors.endDate}
+                    id="endDate"
+                    label="Tarikh Tutup Permohonan"
+                    type="date"
+                    bind:val={$proxyExamApplicationEndDate}
+                ></CustomTextField>
+                <CustomTextField
+                    disabled={false}
+                    errors={$errors.examDate}
+                    id="examDate"
+                    label="Tarikh Peperiksaan"
+                    type="date"
+                    bind:val={$proxyExamDate}
+                ></CustomTextField>
+                <CustomTextField
+                    disabled={false}
+                    errors={$errors.examLocation}
+                    id="examLocation"
+                    label="Lokasi Peperiksaan"
+                    type="text"
+                    bind:val={$form.examLocation}
+                ></CustomTextField>
+            </form>
+        </StepperContentBody>
+    </StepperContent>
+</Stepper>
+
+<Toaster />
