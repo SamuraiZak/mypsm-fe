d35164cbe7315ab8448e8e4ebd12f7a46dcd5180
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-03-06 17:41:32 +0800

    feat: create exam dto

--- src/lib/dto/mypsm/course/exam/course-exam.dto.ts
+++ src/lib/dto/mypsm/course/exam/course-exam.dto.ts
@@ -1,4 +1,5 @@
 import type {
+    _editExamInfoRequestSchema,
     _examInfoRequestSchema,
     _examInfoResponseSchema,
 } from '$lib/schemas/mypsm/course/schema';
@@ -6,7 +7,10 @@ import type { z } from 'zod';
 
 export type CourseCreateExamRequestDTO = z.infer<typeof _examInfoRequestSchema>;
 
+export type CourseEditExamRequestDTO = z.infer<
+    typeof _editExamInfoRequestSchema
+>;
+
 export type CourseExamDetailResponseDTO = z.infer<
     typeof _examInfoResponseSchema
 >;
-
