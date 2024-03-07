b630b46ab16320ac0d92e814e831584da247043d
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-03-07 18:17:12 +0800

    feat: WIP service allowance

--- src/lib/schemas/mypsm/service-allowance/service-allowance.schema.ts
+++ src/lib/schemas/mypsm/service-allowance/service-allowance.schema.ts
@@ -2,7 +2,39 @@
 // Service Allowance Schema
 // =====================================================================
 
-import * as z from "zod";
+import * as z from 'zod';
+
+// common verification detail schema
+export const _serviceAllowanceVerificationSchema = z.object({
+    allowanceId: z.number(),
+    allowanceTypeCode: z.string(),
+    remark: z.string().optional(),
+    status: z.boolean(),
+});
+
+// common supporter and approver detail schema
+export const _serviceAllowanceSuppAppDetailSchema = z.object({
+    allowanceId: z.number(),
+    allowanceTypeCode: z.string(),
+    supporter: z.string(),
+    approver: z.string(),
+});
+
+// common supporter feedback schema
+export const _serviceAllowanceSupporterFeedbackSchema = z.object({
+    allowanceId: z.number(),
+    allowanceTypeCode: z.string(),
+    remark: z.string(),
+    status: z.boolean(),
+});
+
+// common approver feedback form
+export const _serviceAllowanceApproverFeedbackSchema = z.object({
+    allowanceId: z.number(),
+    allowanceTypeCode: z.string(),
+    remark: z.string(),
+    status: z.boolean(),
+});
 
 export const _allowanceFamilyDetailSchema = z.object({
     name: z.string(),
@@ -17,3 +49,12 @@ export const _addTambangMengunjungiWilayahAsalSchemaSchema = z.object({
     familyDetail: z.array(_allowanceFamilyDetailSchema),
 });
 
+// tambang upload schema
+export const _serviceAllowanceUploadDocsTambang = z.object({
+    documents: z
+        .instanceof(File, { message: 'Please upload a file.' })
+        .refine((f) => f.size < 100_000_000, 'Max 100 MB upload size.')
+        .array(),
+    allowanceId: z.string(),
+    allowanceTypeCode: z.string(),
+});
--- src/routes/(guarded)/elaun-elaun-perkhidmatan/permohonan/[id]/tambang-mengunjungi-wilayah-asal/+page.ts
+++ src/routes/(guarded)/elaun-elaun-perkhidmatan/permohonan/[id]/tambang-mengunjungi-wilayah-asal/+page.ts
@@ -1,11 +1,20 @@
 import { ApplyOnBehalfConstant } from '$lib/constants/core/apply-on-behalf.constant.js';
+import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key.constant.js';
 import { ServiceAllowanceTypeConstant } from '$lib/constants/core/service-allowance.constant.js';
 import { StateConstant } from '$lib/constants/core/state.constant.js';
 import type { CommonResponseDTO } from '$lib/dto/core/common/common-response.dto';
 import type { DropdownDTO } from '$lib/dto/core/dropdown/dropdown.dto';
 import type { LookupDTO } from '$lib/dto/core/lookup/lookup.dto.js';
+import type { RadioDTO } from '$lib/dto/core/radio/radio.dto';
 import { LookupHelper } from '$lib/helpers/core/lookup.helper';
-import { _addTambangMengunjungiWilayahAsalSchemaSchema } from '$lib/schemas/mypsm/service-allowance/service-allowance.schema';
+import {
+    _addTambangMengunjungiWilayahAsalSchemaSchema,
+    _serviceAllowanceApproverFeedbackSchema,
+    _serviceAllowanceSuppAppDetailSchema,
+    _serviceAllowanceSupporterFeedbackSchema,
+    _serviceAllowanceUploadDocsTambang,
+    _serviceAllowanceVerificationSchema,
+} from '$lib/schemas/mypsm/service-allowance/service-allowance.schema';
 import { LookupServices } from '$lib/services/implementation/core/lookup/lookup.service';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
@@ -18,6 +27,11 @@ export async function load({ params }) {
 
     // get application id; new = baru
     const applicationId = params.id;
+
+    // get currentRoleCode
+    const currentRoleCode = localStorage.getItem(
+        LocalStorageKeyConstant.currentRoleCode,
+    );
 
     // set default allowanceTypeCode
     let currentAllowanceType: string =
@@ -62,11 +76,46 @@ export async function load({ params }) {
         stateDropdown = LookupHelper.toDropdownProper(stateLookup);
     }
 
-    
+    // get verification status option
+    const verificationStatusOption: RadioDTO[] = [
+        {
+            value: true,
+            name: 'Sah',
+        },
+        {
+            value: false,
+            name: 'Tidak Sah',
+        },
+    ];
+
+    // get support status option
+    const supportStatusOption: RadioDTO[] = [
+        {
+            value: true,
+            name: 'Sokong',
+        },
+        {
+            value: false,
+            name: 'Tidak Sokong',
+        },
+    ];
+
+    // get approval status option
+    const approvalStatusOption: RadioDTO[] = [
+        {
+            value: true,
+            name: 'Lulus',
+        },
+        {
+            value: false,
+            name: 'Tidak Lulus',
+        },
+    ];
+
     // ===================================================================
     // Create Application Detail Form
     // ===================================================================
-    
+
     // create form
     const allowanceDetailForm = await superValidate(
         zod(_addTambangMengunjungiWilayahAsalSchemaSchema),
@@ -75,12 +124,37 @@ export async function load({ params }) {
     // ===================================================================
     // Create Document Detail Form
     // ===================================================================
-
+    const uploadDocumentsForm = await superValidate(
+        zod(_serviceAllowanceUploadDocsTambang),
+    );
 
     // ===================================================================
-    // Create verification Form
+    // Create Verification Form
     // ===================================================================
+    const verificationForm = await superValidate(
+        zod(_serviceAllowanceVerificationSchema),
+    );
 
+    // ===================================================================
+    // Create Supporter and Approver Detail Form
+    // ===================================================================
+    const suppporterApproverDetailForm = await superValidate(
+        zod(_serviceAllowanceSuppAppDetailSchema),
+    );
+
+    // ===================================================================
+    // Create Support Form
+    // ===================================================================
+    const supporterFeedbackForm = await superValidate(
+        zod(_serviceAllowanceSupporterFeedbackSchema),
+    );
+
+    // ===================================================================
+    // Create Approval Form
+    // ===================================================================
+    const approverFeedbackForm = await superValidate(
+        zod(_serviceAllowanceApproverFeedbackSchema),
+    );
 
     // check if user is making new application or viewing aplpication details
     if (applicationId == 'baru') {
@@ -102,11 +176,20 @@ export async function load({ params }) {
     return {
         props: {
             applicationId,
+            currentRoleCode,
             currentAllowanceType,
             allowanceTypeDropdown,
             applyOnBehalfDropdown,
             stateDropdown,
+            verificationStatusOption,
+            supportStatusOption,
+            approvalStatusOption,
             allowanceDetailForm,
+            uploadDocumentsForm,
+            verificationForm,
+            suppporterApproverDetailForm,
+            supporterFeedbackForm,
+            approverFeedbackForm,
         },
     };
 }
@@ -116,5 +199,35 @@ export const _submitAllowanceDetail = async (allowanceDetail: object) => {
     const form = await superValidate(
         allowanceDetail,
         zod(_addTambangMengunjungiWilayahAsalSchemaSchema),
+    );
+};
+
+// submit verification form
+export const _submitVerificationForm = async (formData: object) => {
+    const form = await superValidate(
+        formData,
+        zod(_serviceAllowanceVerificationSchema),
+    );
+};
+
+// submit supporter and approver detail form
+export const _submitSuppAppDetailsForm = async (formData: object) => {
+    const form = await superValidate(
+        formData,
+        zod(_serviceAllowanceSuppAppDetailSchema),
+    );
+};
+// submit supporter feedback form
+export const _submitSupporterFeedbackForm = async (formData: object) => {
+    const form = await superValidate(
+        formData,
+        zod(_serviceAllowanceSupporterFeedbackSchema),
+    );
+};
+// submit supporter feedback form
+export const _submitApproverFeedbackForm = async (formData: object) => {
+    const form = await superValidate(
+        formData,
+        zod(_serviceAllowanceApproverFeedbackSchema),
     );
 };
