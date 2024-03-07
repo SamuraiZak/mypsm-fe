086408f62c1d18f891c07e164f86768661c47e45
Holifield William <holifield.william@impactbs.my>
2024-03-07 17:40:25 +0800

    feat: WIP Integration for kakitangan-kontrak lantikan baru

--- src/lib/services/implementation/mypsm/kakitangan-kontrak/contract-employee.service.ts
+++ src/lib/services/implementation/mypsm/kakitangan-kontrak/contract-employee.service.ts
@@ -9,7 +9,15 @@ import {
     type CommonListRequestDTO,
 } from '$lib/dto/core/common/common-list-request.dto';
 import { CommonResponseConvert } from '$lib/dto/core/common/common-response.dto';
+import { AddNewContractEmployeeAcademicDTOConvert, type AddNewContractEmployeeAcademicDTO } from '$lib/dto/mypsm/kakitangan-kontrak/add-contract-academic.dto';
+import { AddNewContractEmployeeActivityDTOConvert, type AddNewContractEmployeeActivityDTO } from '$lib/dto/mypsm/kakitangan-kontrak/add-contract-activity.dto';
+import { AddNewContractEmployeeDependencyDTOConvert, type AddNewContractEmployeeDependencyDTO } from '$lib/dto/mypsm/kakitangan-kontrak/add-contract-dependency.dto';
+import { AddNewContractEmployeeExperienceDTOConvert, type AddNewContractEmployeeExperienceDTO } from '$lib/dto/mypsm/kakitangan-kontrak/add-contract-experience.dto';
+import { AddContractApproverSupporterDTOConvert, type AddContractApproverSupporterDTO } from '$lib/dto/mypsm/kakitangan-kontrak/add-contract-supproter-approver.dto';
 import { AddNewContractEmployeeDTOConvert, type AddNewContractEmployeeDTO } from '$lib/dto/mypsm/kakitangan-kontrak/add-new-contract-employee.dto';
+import { EditNewContractEmployeeDetailDTOConvert, type EditNewContractEmployeeDetailDTO } from '$lib/dto/mypsm/kakitangan-kontrak/edit-new-contract-employee-detail.dto';
+import { EditContractDetailSecretaryDTOConvert, type EditContractDetailSecretaryDTO } from '$lib/dto/mypsm/kakitangan-kontrak/update-contract-detail-secretary.dto';
+import { ContractCommonRoleResultDTOConvert, type ContractCommonRoleResultDTO } from '$lib/dto/mypsm/kakitangan-kontrak/update-contract-secretary-result.dto';
 import { getPromiseToast } from '$lib/helpers/core/toast.helper';
 import http from '$lib/services/implementation/service-provider.service';
 import type { Input } from 'ky';
@@ -47,6 +55,341 @@ export class ContractEmployeeServices {
             const promiseRes: Promise<Response> = http
                 .post(url, {
                     body: AddNewContractEmployeeDTOConvert.toJson(param),
+                })
+                .json();
+                
+            const response: Response = await getPromiseToast(promiseRes);
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                invalidateAll()
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    //edit new contract employee detail
+    static async editNewContractEmployeeDetail(param: EditNewContractEmployeeDetailDTO) {
+        try {
+            let url: Input = 'contracts/personal_detail/add';
+
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: EditNewContractEmployeeDetailDTOConvert.toJson(param),
+                })
+                .json();
+                
+            const response: Response = await getPromiseToast(promiseRes);
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                invalidateAll()
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    //edit new contract employee academic
+    static async addNewContractEmployeeAcademic(param: AddNewContractEmployeeAcademicDTO) {
+        try {
+            let url: Input = 'contracts/academic/add';
+
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: AddNewContractEmployeeAcademicDTOConvert.toJson(param),
+                })
+                .json();
+                
+            const response: Response = await getPromiseToast(promiseRes);
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                invalidateAll()
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    //add contract employee experiences
+    static async addNewContractEmployeeExperience(param: AddNewContractEmployeeExperienceDTO) {
+        try {
+            let url: Input = 'contracts/experience/add';
+
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: AddNewContractEmployeeExperienceDTOConvert.toJson(param),
+                })
+                .json();
+                
+            const response: Response = await getPromiseToast(promiseRes);
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                invalidateAll()
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    //add contract employee activities
+    static async addNewContractEmployeeActivity(param: AddNewContractEmployeeActivityDTO) {
+        try {
+            let url: Input = 'contracts/activity/add';
+
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: AddNewContractEmployeeActivityDTOConvert.toJson(param),
+                })
+                .json();
+                
+            const response: Response = await getPromiseToast(promiseRes);
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                invalidateAll()
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    //add contract employee family
+    static async addNewContractEmployeeFamily(param: AddNewContractEmployeeDependencyDTO) {
+        try {
+            let url: Input = 'contracts/family/add';
+
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: AddNewContractEmployeeDependencyDTOConvert.toJson(param),
+                })
+                .json();
+                
+            const response: Response = await getPromiseToast(promiseRes);
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                invalidateAll()
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    //add contract employee dependency
+    static async addNewContractEmployeeDependency(param: AddNewContractEmployeeDependencyDTO) {
+        try {
+            let url: Input = 'contracts/next_of_kin/add';
+
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: AddNewContractEmployeeDependencyDTOConvert.toJson(param),
+                })
+                .json();
+                
+            const response: Response = await getPromiseToast(promiseRes);
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                invalidateAll()
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    //add contract employee next of kin
+    static async addNewContractEmployeeNextOfKin(param: AddNewContractEmployeeDependencyDTO) {
+        try {
+            let url: Input = 'contracts/dependant/add';
+
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: AddNewContractEmployeeDependencyDTOConvert.toJson(param),
+                })
+                .json();
+                
+            const response: Response = await getPromiseToast(promiseRes);
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                invalidateAll()
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // create contract employee documents //multipart form
+    static async addNewContractEmployeeDocument(param: FormData) {
+        try {
+            const url: Input = 'contracts/document/add';
+
+            param.append('key', 'document');
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: param,
+                    headers: {
+                        Accept: 'multipart/form-data',
+                        'Content-type': 'multipart/form-data;',
+                    },
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    //edit contract detail secretary
+    static async updateContractDetail(param: EditContractDetailSecretaryDTO) {
+        try {
+            let url: Input = 'contracts/secretary_update/add';
+
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: EditContractDetailSecretaryDTOConvert.toJson(param),
+                })
+                .json();
+                
+            const response: Response = await getPromiseToast(promiseRes);
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                invalidateAll()
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    //add secretary result
+    static async addContractSecretaryResult(param: ContractCommonRoleResultDTO) {
+        try {
+            let url: Input = 'contracts/secretary_approval/add';
+
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: ContractCommonRoleResultDTOConvert.toJson(param),
+                })
+                .json();
+                
+            const response: Response = await getPromiseToast(promiseRes);
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                invalidateAll()
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    //add approver and supporter
+    static async addContractApproverSupporter(param: AddContractApproverSupporterDTO) {
+        try {
+            let url: Input = 'contracts/set_approver_supporter/add';
+
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: AddContractApproverSupporterDTOConvert.toJson(param),
+                })
+                .json();
+                
+            const response: Response = await getPromiseToast(promiseRes);
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                invalidateAll()
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    //add supporter result
+    static async addContractSupporterResult(param: ContractCommonRoleResultDTO) {
+        try {
+            let url: Input = 'contracts/supporter_approval/add';
+
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: ContractCommonRoleResultDTOConvert.toJson(param),
+                })
+                .json();
+                
+            const response: Response = await getPromiseToast(promiseRes);
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                invalidateAll()
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    //add approver result
+    static async addContractApproverResult(param: ContractCommonRoleResultDTO) {
+        try {
+            let url: Input = 'contracts/approver_approval/add';
+
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: ContractCommonRoleResultDTOConvert.toJson(param),
                 })
                 .json();
                 
--- src/routes/(guarded)/kakitangan-kontrak/lantikan-baru/butiran-[id]/+page.ts
+++ src/routes/(guarded)/kakitangan-kontrak/lantikan-baru/butiran-[id]/+page.ts
@@ -3,8 +3,121 @@ import { UserRoleConstant } from "$lib/constants/core/user-role.constant";
 import type { CommonFilterDTO } from "$lib/dto/core/common/common-filter.dto";
 import type { CommonListRequestDTO } from "$lib/dto/core/common/common-list-request.dto";
 import type { CommonResponseDTO } from "$lib/dto/core/common/common-response.dto";
+import type { DropdownDTO } from "$lib/dto/core/dropdown/dropdown.dto";
+import type { EditNewContractEmployeeDetailDTO } from "$lib/dto/mypsm/kakitangan-kontrak/edit-new-contract-employee-detail.dto.js";
+import { getErrorToast } from "$lib/helpers/core/toast.helper";
+import { _editNewContractEmployeeSchema, _uploadDocSchema } from "$lib/schemas/mypsm/contract-employee/contract-employee-schemas.js";
+import { LookupServices } from "$lib/services/implementation/core/lookup/lookup.service";
 import { ContractEmployeeServices } from "$lib/services/implementation/mypsm/kakitangan-kontrak/contract-employee.service";
+import { error } from "@sveltejs/kit";
+import { zod } from "sveltekit-superforms/adapters";
+import { superValidate } from "sveltekit-superforms/client";
 
 export const load = async ({params}) => {
-    const currentRoleCode = localStorage.getItem(LocalStorageKeyConstant.currentRoleCode)
+    const currentRoleCode = localStorage.getItem(LocalStorageKeyConstant.currentRoleCode);
+
+    //form validation
+    const editNewContractEmployeeDetailForm = await superValidate(zod(_editNewContractEmployeeSchema));
+    const contractUploadDocumentForm = await superValidate(zod(_uploadDocSchema));
+// =======================================================
+    // Dropdown Lookup =======================================
+    // =======================================================
+    const genderLookupResponse: CommonResponseDTO =
+        await LookupServices.getGenderEnums();
+
+    const genderLookup: DropdownDTO[] =
+        LookupServices.setSelectOptions(genderLookupResponse);
+    // -------------------------------------------------------
+    const identityCardTypeLookupResponse: CommonResponseDTO =
+        await LookupServices.getIdentityCardColor();
+
+    const identityCardTypeLookup: DropdownDTO[] =
+        LookupServices.setSelectOptionsInString(identityCardTypeLookupResponse)
+    // -------------------------------------------------------
+    const stateLookupResponse: CommonResponseDTO =
+        await LookupServices.getStateEnums();
+
+    const stateLookup: DropdownDTO[] =
+        LookupServices.setSelectOptions(stateLookupResponse)
+    // -------------------------------------------------------
+    const nationalityLookupResponse: CommonResponseDTO =
+        await LookupServices.getNationalityEnums();
+
+    const nationalityLookup: DropdownDTO[] =
+        LookupServices.setSelectOptions(nationalityLookupResponse)
+    // -------------------------------------------------------
+    const raceLookupResponse: CommonResponseDTO =
+        await LookupServices.getRaceEnums();
+
+    const raceLookup: DropdownDTO[] =
+        LookupServices.setSelectOptions(raceLookupResponse)
+    // -------------------------------------------------------
+    const maritalLookupResponse: CommonResponseDTO =
+        await LookupServices.getMaritalEnums();
+
+    const maritalLookup: DropdownDTO[] =
+        LookupServices.setSelectOptions(maritalLookupResponse)
+    // -------------------------------------------------------
+    const religionLookupResponse: CommonResponseDTO =
+        await LookupServices.getReligionEnums();
+
+    const religionLookup: DropdownDTO[] =
+        LookupServices.setSelectOptions(religionLookupResponse)
+    // -------------------------------------------------------
+    const ethnicLookupResponse: CommonResponseDTO =
+        await LookupServices.getEthnicEnums();
+
+    const ethnicLookup: DropdownDTO[] =
+        LookupServices.setSelectOptions(ethnicLookupResponse)
+    // -------------------------------------------------------
+
+    return {
+        currentRoleCode,
+        editNewContractEmployeeDetailForm,
+        contractUploadDocumentForm,
+        selectOption: {
+            genderLookup,
+            identityCardTypeLookup,
+            nationalityLookup,
+            raceLookup,
+            maritalLookup,
+            religionLookup,
+            stateLookup,
+            ethnicLookup,
+        }
+    }
 }
+
+export const _submitEditNewContractEmployeeDetailForm = async (formData: object) => {
+    const form = await superValidate(formData,zod(_editNewContractEmployeeSchema));
+    console.log(form)
+    if(form.valid){
+        const response: CommonResponseDTO =  
+            await ContractEmployeeServices.editNewContractEmployeeDetail(
+                form.data as EditNewContractEmployeeDetailDTO,
+            )
+        if (form.valid) {
+            const result: string | null = 'success';
+            return { response, result };
+        } else {
+            const result: string | null = 'fail';
+            return { response, result };
+        }
+    }
+};
+
+export const _submitDocumentForm = async (file: File | null | undefined) => {
+    const documentData = new FormData();
+    
+    const form = await superValidate(zod(_uploadDocSchema));
+    
+    if (!form.valid) {
+        getErrorToast();
+        error(400, { message: 'Validation Not Passed!' });
+    } else if (form.valid){
+        const response: CommonResponseDTO =
+        await ContractEmployeeServices.addNewContractEmployeeDocument(documentData);
+        return { response };
+    }
+    
+};
