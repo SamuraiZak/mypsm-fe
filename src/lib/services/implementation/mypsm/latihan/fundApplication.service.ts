7d20770bfb9c445d410b383c9b4e280dbebd2d23
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-03-08 17:56:34 +0800

    feat: creates fund application services.

---
+++ src/lib/services/implementation/mypsm/latihan/fundApplication.service.ts
@@ -0,0 +1,325 @@
+// ===============================================================
+// Fund Application Services
+// ===============================================================
+
+import { invalidateAll } from '$app/navigation';
+import { CommonResponseConstant } from '$lib/constants/core/common-response.constant';
+import {
+    CommonListRequestConvert,
+    type CommonListRequestDTO,
+} from '$lib/dto/core/common/common-list-request.dto';
+import { CommonResponseConvert } from '$lib/dto/core/common/common-response.dto';
+import type { commonIdRequestDTO } from '$lib/dto/core/common/id-request.dto';
+import type { CourseFundApplicationApprovalDTO } from '$lib/dto/mypsm/course/fund-application/course-fund-application-approval.dto';
+import { getPromiseToast } from '$lib/helpers/core/toast.helper';
+import http from '$lib/services/implementation/service-provider.service';
+import type { Input } from 'ky';
+
+export class CourseFundApplicationServices {
+    // get list of fund applications
+    static async getCourseFundApplicationList(param: CommonListRequestDTO) {
+        try {
+            const url: Input = 'course/fund_application/list';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: CommonListRequestConvert.toJson(param),
+                })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get course fund applications details
+    static async getCourseFundApplicationDetail(param: commonIdRequestDTO) {
+        try {
+            const url: Input = 'course/fund_application/get';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // create fund applications record
+    static async createCourseFundApplication<T>(param: T) {
+        try {
+            const url: Input = 'course/fund_application/add';
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get personal detail fund application details
+    static async getCourseFundApplicationPersonalDetail(
+        param: commonIdRequestDTO,
+    ) {
+        try {
+            const url: Input = 'course/fund_application/personal_detail';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get service detail fund application details
+    static async getCourseFundApplicationServiceDetail(
+        param: commonIdRequestDTO,
+    ) {
+        try {
+            const url: Input = 'course/fund_application/employee_detail';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // set course secretary approval for fund application
+    static async setCourseFundApplicationCourseSecretaryApproval(
+        param: CourseFundApplicationApprovalDTO,
+    ) {
+        try {
+            const url: Input = 'course/fund_application/certifier_approval/add';
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get course secretary approval exam application
+    static async getCourseFundApplicationCourseSecretaryApproval(
+        param: commonIdRequestDTO,
+    ) {
+        try {
+            const url: Input = 'course/fund_application/certifier_approval/get';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // set integrity secretary approval for fund application
+    static async setCourseFundApplicationIntegritySecretaryApproval(
+        param: CourseFundApplicationApprovalDTO,
+    ) {
+        try {
+            const url: Input = 'course/fund_application/confirmer_approval/get';
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get integrity secretary approval exam application
+    static async getCourseFundApplicationIntegritySecretaryApproval(
+        param: commonIdRequestDTO,
+    ) {
+        try {
+            const url: Input = 'course/fund_application/confirmer_approval/add';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // set state or unit director approval for fund application
+    static async setCourseFundApplicationStateUnitDirectorApproval(
+        param: CourseFundApplicationApprovalDTO,
+    ) {
+        try {
+            const url: Input = 'course/fund_application/supporter_approval/add';
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get state or unit director approval exam application
+    static async getCourseFundApplicationStateUnitDirectorApproval(
+        param: commonIdRequestDTO,
+    ) {
+        try {
+            const url: Input = 'course/fund_application/supporter_approval/get';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+}
