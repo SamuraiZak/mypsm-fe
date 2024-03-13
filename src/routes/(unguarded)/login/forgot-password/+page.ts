63c1c072086092038bba53fe8d6c8ba2ceacbd66
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-03-13 16:41:18 +0800

    feat: done forgot password

--- src/routes/(unguarded)/login/forgot-password/+page.ts
+++ src/routes/(unguarded)/login/forgot-password/+page.ts
@@ -1,13 +1,16 @@
 import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key.constant';
 import { UserGroupConstant } from '$lib/constants/core/user-group.constant';
+import type { ResetPasswordDTO } from '$lib/dto/core/account/reset-password.dto';
 import type { AuthenticationRequestDTO } from '$lib/dto/core/authentication/authentication-request.dto';
 import type { CommonResponseDTO } from '$lib/dto/core/common/common-response.dto';
 import type { DropdownDTO } from '$lib/dto/core/dropdown/dropdown.dto';
 import type { LookupDTO } from '$lib/dto/core/lookup/lookup.dto';
 import type { UserGroupDTO } from '$lib/dto/core/user-group/user-group.dto';
 import { LookupHelper } from '$lib/helpers/core/lookup.helper';
+import { AccountServices } from '$lib/services/implementation/core/account/account.service';
 import { AuthService } from '$lib/services/implementation/core/auth/authentication.service';
 import { fail } from '@sveltejs/kit';
+import { zod } from 'sveltekit-superforms/adapters';
 import { message, superValidate } from 'sveltekit-superforms/client';
 import { Schema, z } from 'zod';
 
@@ -18,7 +21,7 @@ export const _forgotPasswordSchema = z.object({
 // on page load
 export const load = async () => {
     // create form
-    const form = await superValidate(_forgotPasswordSchema);
+    const form = await superValidate(zod(_forgotPasswordSchema));
 
     return {
         props: {
@@ -28,24 +31,25 @@ export const load = async () => {
 };
 
 // on login form submit
-export const _submit = async (formData: object) => {
-    const form = await superValidate(formData, _forgotPasswordSchema);
+export const _submit = async (formData: ResetPasswordDTO) => {
+    const form = await superValidate(formData, zod(_forgotPasswordSchema));
+
+    let requestSuccess: boolean | null = null;
 
     if (form.valid) {
-        // post
-        // const response: CommonResponseDTO =
-        //     await AuthService.authenticateUser(formData);
-        // if (response.status == 'success') {
-        // } else {
-        //     localStorage.setItem(LocalStorageKeyConstant.userName, 'user01');
-        //     localStorage.setItem(LocalStorageKeyConstant.accessToken, 'token');
-        //     localStorage.setItem(
-        //         LocalStorageKeyConstant.currentRole,
-        //         'Kakitangan',
-        //     );
-        //     localStorage.setItem(LocalStorageKeyConstant.fullName, 'Jon Bovi');
-        // }
+        const response: CommonResponseDTO =
+            await AccountServices.forgotPassword(formData);
+
+        if (response.status == 'success') {
+            requestSuccess = true;
+        } else {
+            requestSuccess = false;
+        }
+
+        return {
+            requestSuccess,
+        };
     } else {
-        return fail(400, form);
+        return requestSuccess;
     }
 };
