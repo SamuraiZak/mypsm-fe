d54abb2d5793851c977e603e61f2afef7eb9c5d8
Nielton Didi James Mairodi <nielton@ibsswk.my>
2024-03-13 16:36:31 +0800

    feat: Added service provider for formdata

---
+++ src/lib/services/implementation/service-provider-formdata.service.ts
@@ -0,0 +1,33 @@
+// ===============================================================
+// Service Provider
+// ===============================================================
+
+import { env } from '$env/dynamic/public';
+import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key.constant';
+
+import ky from 'ky';
+
+const httpFormData = ky.create({
+    prefixUrl: env.PUBLIC_BASE_API_URL,
+    headers: {
+        Accept: 'multipart/form-data',
+        'Content-type': 'multipart/form-data',
+    },
+    timeout: 25000,
+    credentials: 'include',
+    hooks: {
+        beforeRequest: [
+            (request) => {
+                const token = localStorage.getItem(
+                    LocalStorageKeyConstant.accessToken,
+                );
+
+                if (token != null) {
+                    request.headers.set('Authorization', `Bearer ${token}`);
+                }
+            },
+        ],
+    },
+});
+
+export default httpFormData;
