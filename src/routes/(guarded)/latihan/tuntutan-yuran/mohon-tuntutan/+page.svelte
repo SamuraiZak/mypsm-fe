41d6f598d6d4231ea7e7c3246b61e421b01129bf
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-03-13 16:51:52 +0800

    feat: adds document forms

--- src/routes/(guarded)/latihan/tuntutan-yuran/mohon-tuntutan/+page.svelte
+++ src/routes/(guarded)/latihan/tuntutan-yuran/mohon-tuntutan/+page.svelte
@@ -16,10 +16,14 @@
         _serviceInfoRequestSchema,
     } from '$lib/schemas/mypsm/employment/new-hire/schema';
     import { goto } from '$app/navigation';
-    import { _createFundReimbursementForm } from './+page';
-    import { _fundReimbursementDetailResponseSchema } from '$lib/schemas/mypsm/course/fund-reimbursement-schema';
+    import { _createFundReimbursementForm, _submitDocumentForm } from './+page';
+    import { _fundReimbursementDetailResponseSchema, _fundReimbursementUploadDocSchema } from '$lib/schemas/mypsm/course/fund-reimbursement-schema';
     import type { PageData } from './$types';
+    import FileInputField from '$lib/components/inputs/file-input-field/FileInputField.svelte';
+    import FileInputFieldChildren from '$lib/components/inputs/file-input-field/FileInputFieldChildren.svelte';
     export let data: PageData;
+    let enableUploadDocument = false;
+    let createdApplicationId: number;
 
     // Superforms
     const { form, errors, enhance } = superForm(data.reimbursementInfoForm, {
@@ -30,11 +34,58 @@
         multipleSubmits: 'prevent',
         validationMethod: 'oninput',
         validators: zod(_fundReimbursementDetailResponseSchema),
-        onSubmit(formData) {
-            _createFundReimbursementForm(formData.formData);
+        async onSubmit(formData) {
+            const result = await _createFundReimbursementForm(formData.formData);
+
+            if (result.response.status === 'success') {
+                enableUploadDocument = true;
+                createdApplicationId = result.response.data?.details.id;
+            }
         },
         taintedMessage: false,
     });
+
+        
+    const {
+        form: fundReimbursementUploadDocumentForm,
+        errors: fundReimbursementUploadDocumentError,
+        enhance: fundReimbursementUploadDocumentEnhance,
+    } = superForm(data.fundReimbursementUploadDocumentForm, {
+        SPA: true,
+        resetForm: false,
+        id: 'documentUploadForm',
+        validators: zod(_fundReimbursementUploadDocSchema),
+        onSubmit() {
+            _submitDocumentForm(
+                createdApplicationId,
+                $fundReimbursementUploadDocumentForm.documents,
+            );
+        },
+        taintedMessage: 'Permohonon anda belum selesai.',
+    });
+
+    const handleOnInput = (e: Event) => {
+        const additionalFiles: File[] = Array.from(
+            (e.currentTarget as HTMLInputElement)?.files ?? [],
+        );
+
+        additionalFiles.forEach((file) => {
+            $fundReimbursementUploadDocumentForm.documents = [
+                ...$fundReimbursementUploadDocumentForm.documents,
+                file,
+            ];
+        });
+    };
+    const handleDelete = () => {
+        // const selectedFileName = (e.currentTarget as HTMLInputElement).files?.item(0)?.name
+
+        // $fundReimbursementUploadDocumentForm.documents =
+        //     $fundReimbursementUploadDocumentForm.documents.filter((file) => {
+        //         return file.name !== selectedFileName;
+        //     });
+
+        $fundReimbursementUploadDocumentForm.documents = [];
+    };
 </script>
 
 <ContentHeader title="Maklumat Pembiayaan Pelajaran"
@@ -168,6 +219,90 @@
             </form>
         </StepperContentBody>
     </StepperContent>
+    {#if enableUploadDocument}
+        <StepperContent>
+            <StepperContentHeader title="Dokumen Sokongan">
+                <TextIconButton
+                    label="Simpan"
+                    form="documentUploadForm"
+                    type="primary"
+                />
+            </StepperContentHeader>
+            <StepperContentBody>
+                <div class="flex w-full flex-col gap-2">
+                    <form
+                        class="flex w-full flex-col justify-start gap-2.5 pb-10"
+                        method="POST"
+                        id="documentUploadForm"
+                        enctype="multipart/form-data"
+                        use:fundReimbursementUploadDocumentEnhance
+                    >
+                        {#if $fundReimbursementUploadDocumentError.documents}
+                            <span
+                                class="font-sans text-sm italic text-system-danger"
+                                >Sila muat naik dokumen barkaitan dan pastikan
+                                tidak melebihi 4MB.</span
+                            >
+                        {/if}
+                        <ContentHeader
+                            title="Dokumen Sokongan"
+                            borderClass="border-none"
+                        >
+                            <div
+                                hidden={$fundReimbursementUploadDocumentForm
+                                    .documents.length < 1}
+                            >
+                                <FileInputField
+                                    id="documents"
+                                    handleOnInput={(e) => handleOnInput(e)}
+                                ></FileInputField>
+                            </div>
+                        </ContentHeader>
+                        <div
+                            class="flex h-fit w-full flex-col items-center justify-center gap-2.5 rounded-lg border border-bdr-primary p-2.5"
+                        >
+                            <div class="flex flex-wrap gap-3">
+                                {#each $fundReimbursementUploadDocumentForm.documents as _, i}
+                                    <FileInputFieldChildren
+                                        childrenType="grid"
+                                        {handleDelete}
+                                        fileName={$fundReimbursementUploadDocumentForm
+                                            .documents[i].name}
+                                    />
+                                {/each}
+                            </div>
+                            {#if $fundReimbursementUploadDocumentForm.documents.length < 1}
+                                <div
+                                    class="flex flex-col items-center justify-center gap-2.5 text-sm text-txt-tertiary"
+                                >
+                                    <span>Pilih fail dari peranti anda.</span>
+                                    <svg
+                                        width={40}
+                                        height={40}
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.5"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                                        />
+                                    </svg>
+                                    <FileInputField
+                                        id="documents"
+                                        handleOnInput={(e) => handleOnInput(e)}
+                                    ></FileInputField>
+                                </div>
+                            {/if}
+                        </div>
+                    </form>
+                </div></StepperContentBody
+            >
+        </StepperContent>
+    {/if}
 </Stepper>
 
 <Toaster />
--- src/routes/(guarded)/latihan/tuntutan-yuran/mohon-tuntutan/+page.ts
+++ src/routes/(guarded)/latihan/tuntutan-yuran/mohon-tuntutan/+page.ts
@@ -2,11 +2,13 @@ import { goto } from '$app/navigation';
 import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key.constant';
 import { RoleConstant } from '$lib/constants/core/role.constant';
 import type { CommonResponseDTO } from '$lib/dto/core/common/common-response.dto';
+import type { CourseFundReimbursementUploadDocumentsRequestDTO } from '$lib/dto/mypsm/course/fund-reimbursement/course-fund-reimbursement-document.dto';
 import type { CourseAddFundReimbursementRequestDTO } from '$lib/dto/mypsm/course/fund-reimbursement/course-fund-reimbursement.dto';
 import { getErrorToast } from '$lib/helpers/core/toast.helper';
 import {
     _createFundReimbursementRequestSchema,
     _fundReimbursementDetailResponseSchema,
+    _fundReimbursementUploadDocSchema,
 } from '$lib/schemas/mypsm/course/fund-reimbursement-schema';
 import { CourseFundReimbursementServices } from '$lib/services/implementation/mypsm/latihan/fundReimbursement.service';
 import { error } from '@sveltejs/kit';
@@ -34,8 +36,13 @@ export async function load() {
         zod(_fundReimbursementDetailResponseSchema),
     );
 
+    const fundReimbursementUploadDocumentForm = await superValidate(
+        zod(_fundReimbursementUploadDocSchema),
+    );
+
     return {
         reimbursementInfoForm,
+        fundReimbursementUploadDocumentForm,
     };
 }
 
@@ -63,6 +70,38 @@ export const _createFundReimbursementForm = async (formData: FormData) => {
     if (response.status === 'success')
         setTimeout(() => {
             goto('../tuntutan-yuran');
+        }, 2000);
+
+    return { response };
+};
+
+export const _submitDocumentForm = async (id: number, files: File[]) => {
+    const documentData = new FormData();
+
+    files.forEach((file) => {
+        documentData.append('documents', file);
+    });
+
+    const form = await superValidate(
+        documentData,
+        zod(_fundReimbursementUploadDocSchema),
+    );
+
+    form.data.id = 1;
+    console.log(form);
+
+    if (!form.valid || form.data.id === undefined) {
+        getErrorToast();
+        error(400, { message: 'Validation Not Passed!' });
+    }
+    const response: CommonResponseDTO =
+        await CourseFundReimbursementServices.uploadFundReimbursementEmployeeDocument(
+            form.data as CourseFundReimbursementUploadDocumentsRequestDTO,
+        );
+
+    if (response.status === 'success')
+        setTimeout(() => {
+            goto('../pembiayaan');
         }, 2000);
 
     return { response };
