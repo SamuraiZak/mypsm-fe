46aa8e8e54d8924da85c0fd9cd27de5e4bd6b804
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-03-14 17:29:29 +0800

    feat: adds stepper for stating file not uploaded.

---
+++ src/lib/components/stepper/StepperFileNotUploaded.svelte
@@ -0,0 +1,8 @@
+<div
+    class="mb-2.5 flex w-full flex-row text-start text-sm font-medium italic text-system-danger"
+>
+    <p>
+        Harap maklum. Proses belum dapat diteruskan. <br /> Pemohon sedang memuat
+        naik dokumen (- dokumen) yang diperlukan.
+    </p>
+</div>
