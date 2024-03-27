9b46fb3d1f0b10097cc53156aeedd1ae7cabfb32
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-03-27 15:27:40 +0800

    feat: added service allowance dto

--- src/lib/dto/mypsm/elaun-elaun-perkhidmatan/service-allowance.dto.ts
+++ src/lib/dto/mypsm/elaun-elaun-perkhidmatan/service-allowance.dto.ts
@@ -1,77 +1,33 @@
 // ================================================================
-// Service Allowance Common DTO
+// Service Allowance DTO
 // ================================================================
 
-// TODO 1: Service Allowance List Item DTO
-export interface ServiceAllowanceListItemDTO {
-    allowanceId: string | null;
-    employeeId: string | null;
-    employeeNumber: string | null;
-    name: string | null;
-    identityCardNumber: string | null;
-    allowanceType: string | null;
-    allowanceTypeCode: string | null;
-    category: string | null;
-    applicationDate: Date | null;
-    total: number | null;
-    status: string | null;
-    remark: null | null;
-}
+import type { DocumentDTO } from '$lib/dto/core/document/document.dto';
 
-export interface ServiceAllowanceListFilterDTO {
+// TODO: Application List DTO
+export interface AllowanceApplicationItemDTO {
+    allowanceId?: string | null;
+    employeeId?: string | null;
     employeeNumber?: string | null;
     name?: string | null;
+    identityCardNumber?: string | null;
     allowanceType?: string | null;
+    allowanceTypeCode?: string | null;
+    category?: string | null;
     applicationDate?: string | null;
+    total?: number | null;
     status?: string | null;
-}
-
-export interface ServiceAllowanceViewRequestDTO {
-    allowanceId: number;
-    allowanceTypeCode: string;
-}
-
-// TODO 2: Service Allowance Endorsement DTO
-export interface ServiceAllowanceEndorsementDTO {
-    allowanceId?: number | null;
-    allowanceTypeCode?: string | null;
-    name?: string | null;
     remark?: string | null;
-    status?: boolean | null;
-    statusDescription?: string | null;
 }
 
-// TODO 3: Service Allowance
-export interface ServiceAllowanceEndorserDetailDTO {
-    allowanceId?: number | null;
-    allowanceTypeCode?: string | null;
-    supporter?: string | null;
-    approver?: string | null;
-}
+// TODO: Application Detail DTO
 
-// TODO : Service Allowance Document DTO
-export interface ServiceAllowanceDocumentDTO {
-    name: string;
-    base64: string;
-}
 
-export interface DocumentList {
-    documents: ServiceAllowanceDocumentDTO[];
-}
 
-// TODO: Service Allowance Family Detail DTO
-export interface ServiceAllowanceFamilyDetailDTO {
-    name?: string;
-    age?: number;
-    relationshipCode?: string;
-}
-
-// ================================================================
-// Service Allowance Info DTO
-// ================================================================
+// TODO: Application Info DTO
 // Bantuan Pakaian Istiadat
-export interface ServiceAllowanceInfoCeremonyDressDTO {
-    documents?: ServiceAllowanceDocumentDTO[] | null;
+export interface AllowanceInfoCeremonyDressDTO {
+    documents?: DocumentDTO[] | null;
     allowanceId?: number | null;
     allowanceTypeCode?: string | null;
     allowanceType?: string | null;
@@ -81,49 +37,85 @@ export interface ServiceAllowanceInfoCeremonyDressDTO {
 }
 
 // Bantuan Pakaian Panas
-export interface ServiceAllowanceInfoWarmClothesDTO {}
+export interface AllowanceInfoWarmClothesDTO {
+    documents?: DocumentDTO[] | null;
+    allowanceId?: number | null;
+    allowanceTypeCode?: string | null;
+    allowanceType?: string | null;
+    reason?: string | null;
+    personal?: number | null;
+    partner?: number | null;
+}
 
 // Tambang Mengunjungi Wilayah Asal
-export interface ServiceAllowanceInfoHometownVisitDTO {}
+export interface AllowanceInfoHometownVisitDTO {
+    allowanceTypeCode?: string | null;
+    allowanceId?: number | null;
+    applyCode?: string;
+    stateCode?: string;
+    familyDetail?: AllowanceFamilyDTO[];
+    documents?: Document[];
+}
 
 // Bantuan Mengurus Jenazah
-export interface ServiceAllowanceInfoFuneralDTO {}
+export interface AllowanceInfoFuneralDTO {
+    documents?: DocumentDTO[] | null;
+    allowanceTypeCode?: string | null;
+    allowanceId?: number | null;
+    deathDate?: string | null;
+    deathTime?: string | null;
+}
 
 // Tabung Kebajikan Kakitangan
-export interface ServiceAllowanceInfoWelfareFundDTO {}
+export interface AllowanceInfoWelfareFundDTO {
+    documents?: DocumentDTO[] | null;
+    allowanceTypeCode?: string | null;
+    allowanceId?: number | null;
+    welfareTypeCode?: string | null;
+}
 
 // Perpindahan Rumah
-export interface ServiceAllowanceInfoHouseMovingDTO {}
+export interface AllowanceInfoHouseMovingDTO {
+    documents?: DocumentDTO[] | null;
+    allowanceTypeCode?: string | null;
+    allowanceId?: number | null;
+    movingDate?: string | null;
+    oldAddress?: string | null;
+    newAddress?: string | null;
+}
 
 // Bayaran Balik Passport
-export interface ServiceAllowanceInfoPassportClaimDTO {}
+export interface AllowanceInfoPassportClaimDTO {
+    documents?: DocumentDTO[] | null;
+    allowanceTypeCode?: string | null;
+    allowanceId?: number | null;
+    renewDate?: Date | null;
+    reason?: string | null;
+}
 
 // Bayaran Balik Insurans Kesihatan
-export interface ServiceAllowanceInfoHealthInsuranceDTO {}
+export interface AllowanceInfoHealthInsuranceDTO {
+    documents?: DocumentDTO[] | null;
+    allowanceTypeCode?: string | null;
+    allowanceId?: number | null;
+    regionCode?: string | null;
+    insuranceType?: string | null;
+    startDate?: Date | null;
+    endDate?: Date | null;
+    reason?: string | null;
+}
 
 //  Bayaran Balik Pengangkutan Barang Melalui Jalan Darat
-export interface ServiceAllowanceShippingClaimDTO {}
-
-// ================================================================
-// Service Allowance Applciation DTO
-// ================================================================
-export interface ServiceAllowanceApplicationDTO {
-    applicationDetail?:
-        | ServiceAllowanceInfoCeremonyDressDTO
-        | ServiceAllowanceInfoWarmClothesDTO
-        | ServiceAllowanceInfoHometownVisitDTO
-        | ServiceAllowanceInfoFuneralDTO
-        | ServiceAllowanceInfoWelfareFundDTO
-        | ServiceAllowanceInfoHouseMovingDTO
-        | ServiceAllowanceInfoPassportClaimDTO
-        | ServiceAllowanceInfoHealthInsuranceDTO
-        | ServiceAllowanceShippingClaimDTO
-        | null;
-    directorSupport?: ServiceAllowanceEndorsementDTO | null;
-    verification?: ServiceAllowanceEndorsementDTO | null;
-    supportApprover?: ServiceAllowanceEndorserDetailDTO | null;
-    support?: ServiceAllowanceEndorsementDTO | null;
-    approval?: ServiceAllowanceEndorsementDTO | null;
-    secretaryVerification?: ServiceAllowanceEndorsementDTO | null;
-    confirmation?: ServiceAllowanceEndorsementDTO | null;
+export interface AllowanceShippingClaimDTO {
+    allowanceTypeCode?: string | null;
+    allowanceId?: string | null;
 }
+
+// TODO: Family Detail DTO
+export interface AllowanceFamilyDTO {
+    name?: string | null;
+    age?: string | null;
+    relationshipCode?: string | null;
+}
+
+// TODO: Application Process DTO
