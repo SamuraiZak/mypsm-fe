864f227ba960887f4c42c76e73835b11c888fe47
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-04-03 09:46:17 +0800

    feat: added lookup management services

---
+++ src/lib/services/implementation/core/lookup/lookup-management.services.ts
@@ -0,0 +1,123 @@
+// ===============================================================
+// Lookup Management Services
+// ===============================================================
+
+import { CommonResponseConstant } from '$lib/constants/core/common-response.constant';
+import { LookupItemConstant } from '$lib/constants/core/lookup-item.constant';
+import {
+    CommonListRequestConvert,
+    type CommonListRequestDTO,
+} from '$lib/dto/core/common/common-list-request.dto';
+import {
+    CommonResponseConvert,
+    type CommonResponseDTO,
+} from '$lib/dto/core/common/common-response.dto';
+import type {
+    LookupItemDTO,
+    LookupTypeDTO,
+} from '$lib/dto/core/lookup/lookup-item.dto';
+import { getPromiseToast } from '$lib/helpers/core/toast.helper';
+import type { Input } from 'ky';
+import http from '../../service-provider.service';
+
+export class LookupManagementServices {
+    static async getTypeList(param: CommonListRequestDTO) {
+        try {
+            let list: LookupTypeDTO[] = LookupItemConstant.list;
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get list
+    static async getList(type: LookupTypeDTO) {
+        try {
+            let url: Input = type.get;
+
+            const response: Response = await http.get(url).json();
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // filter list
+    static async filterList(param: CommonListRequestDTO, type: LookupTypeDTO) {
+        try {
+            let url: Input = type.filterUrl;
+
+            const response: Response = await http
+                .post(url, {
+                    body: CommonListRequestConvert.toJson(param),
+                })
+                .json();
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add list
+    static async add(param: LookupItemDTO, type: LookupTypeDTO) {
+        try {
+            let url: Input = type.add;
+
+            const responsePromise: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const response = await getPromiseToast(responsePromise);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // edit list
+    static async edit(param: LookupItemDTO, type: LookupTypeDTO) {
+        try {
+            let url: Input = type.edit;
+
+            const responsePromise: Promise<Response> = http
+                .put(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const response = await getPromiseToast(responsePromise);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+}
