2b985793cc58b154f6b7bdadfd0f9abf734e3bbb
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-04-04 20:29:09 +0800

    feat: modified table dto

--- src/lib/dto/core/table/table.dto.ts
+++ src/lib/dto/core/table/table.dto.ts
@@ -1,10 +1,11 @@
-import type { CommonListRequestDTO } from "../common/common-list-request.dto";
-import type { Meta } from "../common/common-response.dto";
+import type { CommonListRequestDTO } from '../common/common-list-request.dto';
+import type { Meta } from '../common/common-response.dto';
 
-export interface TableDTO{
+export interface TableDTO {
     param: CommonListRequestDTO;
     meta: Meta;
     data: object[];
     selectedData?: object[];
     hiddenData?: string[];
+    printData?: object[];
 }
