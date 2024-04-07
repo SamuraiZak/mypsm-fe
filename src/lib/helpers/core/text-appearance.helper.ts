5892c0d45a901c707650f289139a588739f03c37
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-04-07 18:01:20 +0800

    feat: modified text-appearance helper

--- src/lib/helpers/core/text-appearance.helper.ts
+++ src/lib/helpers/core/text-appearance.helper.ts
@@ -7,29 +7,33 @@ export class TextAppearanceHelper {
         if (keyword.length <= 0) {
             return keyword;
         } else {
-            
             const words = keyword.split(' ');
-    
+
             for (let i = 0; i < words.length; i++) {
                 words[i] = words[i][0].toUpperCase() + words[i].substr(1);
             }
-    
+
             return words.join(' ');
         }
     }
     static toProper(keyword: string) {
-
         if (keyword.length <= 0) {
             return keyword;
         } else {
-            
-            keyword = keyword.toLowerCase();
+            keyword = keyword.toLowerCase().trim().replaceAll('  ', ' ');
             const words = keyword.split(' ');
-    
+
             for (let i = 0; i < words.length; i++) {
+                // console.log(words[i]);
+
+                // if (words[i][0].match(/[a-z]/i)) {
+                //     words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+                // } else {
+                //     words[i] = words[i][0] + words[i].substr(1);
+                // }
                 words[i] = words[i][0].toUpperCase() + words[i].substr(1);
             }
-    
+
             return words.join(' ');
         }
     }
