558145ce70c636be5b36b7b7f0ee6c922b530629
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-04-07 18:01:45 +0800

    feat: modified lookup schema

--- src/lib/schemas/core/lookup.schema.ts
+++ src/lib/schemas/core/lookup.schema.ts
@@ -1,110 +1,52 @@
 import * as z from 'zod';
 
-// grade edit
-export const LookupGradeSchema = z.object({
+// shared between most lookup
+export const LookupCommonSchema = z.object({
     id: z.number().optional(),
-    code: z.string(),
-    description: z.string().min(1, {
-        message: 'Ruangan ini tidak boleh dibiarkan kosong.',
-    }),
-    ssmCode: z.string().min(1, {
-        message: 'Ruangan ini tidak boleh dibiarkan kosong.',
-    }),
-    minimumSalary: z.number(),
-    maximumSalary: z.number(),
-    annualIncrementRate: z.number(),
+    code: z.string().optional(),
+    description: z.string(),
 });
 
-// department
-export const LookupDepartmentSchema = z.object({
+// district
+export const LookupDistrictSchema = z.object({
     id: z.number().optional(),
-    code: z.string(),
-    description: z.string().min(1, {
-        message: 'Ruangan ini tidak boleh dibiarkan kosong.',
-    }),
+    code: z.string().optional(),
+    description: z.string(),
+    divisionCode: z.string(),
+});
+
+// meeting
+export const LookupMeetingSchema = z.object({
+    id: z.number().optional(),
+    code: z.string().optional(),
+    description: z.string(),
+    module: z.string(),
+    subModule: z.string(),
+});
+
+// relationship
+export const LookupRelationshipSchema = z.object({
+    id: z.number().optional(),
+    code: z.string().optional(),
+    description: z.string(),
+    isFamily: z.boolean(),
 });
 
 // employment status
 export const LookupEmploymentStatusSchema = z.object({
     id: z.number().optional(),
-    occSectorCode: z.string().min(1, {
-        message: 'Ruangan ini tidak boleh dibiarkan kosong.',
-    }),
-    occSectorName: z.string().min(1, {
-        message: 'Ruangan ini tidak boleh dibiarkan kosong.',
-    }),
+    code: z.string().optional(),
+    occSectorCode: z.string().optional(),
+    occSectorName: z.string(),
 });
 
-// placements
-export const LookupPlacementSchema = z.object({
+// grade
+export const LookupGradeSchema = z.object({
     id: z.number().optional(),
-    code: z.string(),
-    description: z.string().min(1, {
-        message: 'Ruangan ini tidak boleh dibiarkan kosong.',
-    }),
-});
-
-// position
-export const LookupPositionSchema = z.object({
-    id: z.number().optional(),
-    code: z.string(),
-    description: z.string().min(1, {
-        message: 'Ruangan ini tidak boleh dibiarkan kosong.',
-    }),
-});
-
-// programme
-export const LookupProgramSchema = z.object({
-    id: z.number().optional(),
-    code: z.string(),
-    description: z.string().min(1, {
-        message: 'Ruangan ini tidak boleh dibiarkan kosong.',
-    }),
-});
-
-// scheme
-export const LookupSchemeSchema = z.object({
-    id: z.number().optional(),
-    code: z.string(),
-    description: z.string().min(1, {
-        message: 'Ruangan ini tidak boleh dibiarkan kosong.',
-    }),
-});
-
-// sections
-export const LookupSectionSchema = z.object({
-    id: z.number().optional(),
-    code: z.string(),
-    description: z.string().min(1, {
-        message: 'Ruangan ini tidak boleh dibiarkan kosong.',
-    }),
-    departmentCode: z.string(),
-});
-
-// service group
-export const LookupServiceGroupSchema = z.object({
-    id: z.number().optional(),
-    code: z.string(),
-    description: z.string().min(1, {
-        message: 'Ruangan ini tidak boleh dibiarkan kosong.',
-    }),
-});
-
-// service
-export const LookupServiceTypeSchema = z.object({
-    id: z.number().optional(),
-    code: z.string(),
-    description: z.string().min(1, {
-        message: 'Ruangan ini tidak boleh dibiarkan kosong.',
-    }),
-});
-
-// units
-export const LookupUnitSchema = z.object({
-    id: z.number().optional(),
-    code: z.string(),
-    description: z.string().min(1, {
-        message: 'Ruangan ini tidak boleh dibiarkan kosong.',
-    }),
-    sectionCode: z.string(),
+    code: z.string().optional(),
+    description: z.string(),
+    ssmCode: z.string(),
+    minimumSalary: z.number(),
+    maximumSalary: z.number(),
+    annualIncrementRate: z.number(),
 });
