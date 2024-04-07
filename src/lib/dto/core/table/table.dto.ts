962679196c70e8f1080673ab753206f1893d51e1
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-04-07 18:00:34 +0800

    feat: modified table dto

--- src/lib/dto/core/table/table.dto.ts
+++ src/lib/dto/core/table/table.dto.ts
@@ -1,5 +1,6 @@
 import type { CommonListRequestDTO } from '../common/common-list-request.dto';
 import type { Meta } from '../common/common-response.dto';
+import type { DictionaryDTO } from '../dictionary/dictionary.dto';
 
 export interface TableDTO {
     param: CommonListRequestDTO;
@@ -8,4 +9,26 @@ export interface TableDTO {
     selectedData?: object[];
     hiddenData?: string[];
     printData?: object[];
+}
+
+export interface TableSettingDTO {
+    param: CommonListRequestDTO;
+    meta: Meta;
+    data: object[];
+    selectedData: object[];
+    exportData: object[];
+    hiddenColumn: string[];
+    dictionary: DictionaryDTO[];
+    url: string;
+    id: string;
+    option: {
+        checkbox: boolean;
+        detail: boolean;
+        edit: boolean;
+        select: boolean;
+        filter: boolean;
+    };
+    controls: {
+        add: boolean;
+    };
 }
