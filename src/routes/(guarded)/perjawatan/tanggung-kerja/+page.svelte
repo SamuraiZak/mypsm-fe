7d3f7aa0c4f4e2026b4c04d6095a6695fc849877
Holifield William <holifield.william@impactbs.my>
2024-04-09 13:32:37 +0800

    feat: WIP integration for interim

--- src/routes/(guarded)/perjawatan/tanggung-kerja/+page.svelte
+++ src/routes/(guarded)/perjawatan/tanggung-kerja/+page.svelte
@@ -7,74 +7,60 @@
     import { UserRoleConstant } from '$lib/constants/core/user-role.constant';
     import type { PageData } from './$types';
     import { goto } from '$app/navigation';
-    import { _updateTable } from './+page';
-    import type { TableDTO } from '$lib/dto/core/table/table.dto';
+    import type {
+        TableDTO,
+        TableSettingDTO,
+    } from '$lib/dto/core/table/table.dto';
     import type { CommonListRequestDTO } from '$lib/dto/core/common/common-list-request.dto';
     import FilterCard from '$lib/components/table/filter/FilterCard.svelte';
     import FilterTextField from '$lib/components/table/filter/FilterTextField.svelte';
+    import type { EmployeeInterimApplicationListResponseDTO } from '$lib/dto/mypsm/employment/tanggung-kerja/interim-employee-application-list-response.dto';
+    import DataTable from '$lib/components/table/DataTable.svelte';
+    import FilterWrapper from '$lib/components/table/filter/FilterWrapper.svelte';
     export let data: PageData;
-    // Role Code
-    let employeeRoleCode: string = UserRoleConstant.kakitangan.code;
-    let secretaryRoleCode: string = UserRoleConstant.urusSetiaPerjawatan.code;
-    let stateDirectorRoleCode: string = UserRoleConstant.pengarahNegeri.code;
-    let depDirectorRoleCode: string = UserRoleConstant.pengarahBahagian.code;
-    let managementServiceDirectorRoleCode: string =
-        UserRoleConstant.pengarahKhidmatPengurusan.code;
-    let rowData: any;
-    
+
+    let rowData = {} as EmployeeInterimApplicationListResponseDTO;
+
     //Employees' POV: Table for application list
-    let employeeApplicationParam: CommonListRequestDTO = data.employeeApplicationParam;
-    let employeeApplicationTable: TableDTO = {
-        param: employeeApplicationParam,
+    let employeeApplicationTable: TableSettingDTO = {
+        param: data.employeeApplicationParam,
         meta: data.employeeInterimApplicationResponse.data?.meta ?? {
-            pageSize: 5,
+            pageSize: 1,
             pageNum: 1,
-            totalData: 4,
+            totalData: 1,
             totalPage: 1,
         },
         data: data.employeeInterimApplicationList ?? [],
-    }
-
-    let param: CommonListRequestDTO = data.param;
-    let table: TableDTO = {
-        param: param,
-        meta: {
-            pageSize: 5,
-            pageNum: 1,
-            totalData: 4,
-            totalPage: 1,
+        selectedData: [],
+        exportData: [],
+        hiddenColumn: ['id'],
+        dictionary: [
+            {
+                english: 'description',
+                malay: 'Nama',
+            },
+        ],
+        url:
+            data.currentRoleCode == UserRoleConstant.kakitangan.code
+                ? 'employment/interim/application/list_employee'
+                : 'employment/interim/application/list',
+        id: 'employeeApplicationTable',
+        option: {
+            checkbox: false,
+            detail: true,
+            edit: false,
+            select: false,
+            filter: true,
         },
-        data: data.dataList ?? [],
-    };
-    let tamatTanggungKerjaTable: TableDTO = {
-        param: param,
-        meta: {
-            pageSize: 5,
-            pageNum: 1,
-            totalData: 4,
-            totalPage: 1,
+        controls: {
+            add: false,
         },
-        data: data.dataList2 ?? [],
     };
-
-    async function _search() {
-        _updateTable(table.param).then((value) => {
-            table.data = value.response?.dataList ?? [];
-            table.meta = value.response?.meta ?? {
-                pageSize: 1,
-                pageNum: 1,
-                totalData: 1,
-                totalPage: 1,
-            };
-            table.param.pageSize = table.meta.pageSize;
-            table.param.pageNum = table.meta.pageNum;
-        });
-    }
 </script>
 
 <!-- content header starts here -->
 <section class="flex w-full flex-col items-start justify-start">
-    {#if data.currentRoleCode === employeeRoleCode}
+    {#if data.currentRoleCode === UserRoleConstant.kakitangan.code}
         <ContentHeader title="Tanggung Kerja">
             <TextIconButton
                 onClick={() => goto('/perjawatan/tanggung-kerja/baru')}
@@ -93,32 +79,44 @@
 <section
     class="max-h-[calc(100vh - 172px)] flex h-full w-full flex-col items-center justify-start"
 >
-    {#if data.currentRoleCode === employeeRoleCode || data.currentRoleCode === secretaryRoleCode}
+    {#if data.currentRoleCode === UserRoleConstant.kakitangan.code || data.currentRoleCode === UserRoleConstant.urusSetiaPerjawatan.code}
         <CustomTab>
             <CustomTabContent title="Permohonan Tanggung Kerja">
-                <CustomTable
-                    title="Senarai Permohonan Tanggung Kerja"
-                    bind:tableData={employeeApplicationTable}
-                    bind:passData={rowData}
-                    enableDetail
-                    detailActions={() =>
-                        goto('/perjawatan/tanggung-kerja/butiran-'+rowData.id)}
-                />  
+                <div class="h h-fit w-full">
+                    <DataTable
+                        title="Senarai Permohonan Tanggung Kerja"
+                        bind:tableData={employeeApplicationTable}
+                        bind:passData={rowData}
+                        detailActions={() => {
+                            goto(
+                                '/perjawatan/tanggung-kerja/butiran-' +
+                                    rowData.id,
+                            );
+                        }}
+                    >
+                        <FilterWrapper slot="filter">
+                            <FilterTextField
+                                label="Nama"
+                                bind:inputValue={employeeApplicationTable.param
+                                    .filter.name}
+                            ></FilterTextField>
+                        </FilterWrapper>
+                    </DataTable>
+                </div>
             </CustomTabContent>
 
             <CustomTabContent title="Penamatan Tanggung Kerja">
-                <CustomTable
-                    title="Sejarah Tanggung Kerja"
-                    bind:tableData={tamatTanggungKerjaTable}
-                    onUpdate={_search}
-                />
+                <div class="flex w-full flex-col justify-start gap-2.5 p-3">
+                    <!-- <CustomTable
+                        title="Sejarah Tanggung Kerja"
+                        bind:tableData={tamatTanggungKerjaTable}
+                    /> -->
+                </div>
             </CustomTabContent>
         </CustomTab>
-    {:else if data.currentRoleCode === depDirectorRoleCode || data.currentRoleCode === stateDirectorRoleCode || data.currentRoleCode === managementServiceDirectorRoleCode}
-        <div
-            class="flex max-h-full w-full flex-col items-start justify-start gap-2.5 p-3"
-        >
-            <FilterCard onSearch={_search}>
+    {:else if data.currentRoleCode === UserRoleConstant.pengarahBahagian.code || data.currentRoleCode === UserRoleConstant.pengarahNegeri.code || data.currentRoleCode === UserRoleConstant.pengarahKhidmatPengurusan.code}
+        <div class="flex w-full flex-col items-start justify-start gap-2.5 p-3">
+            <!-- <FilterCard>
                 <FilterTextField
                     bind:inputValue={table.param.filter.grade}
                     label="No. Pekerja"
@@ -131,14 +129,27 @@
                     bind:inputValue={table.param.filter.name}
                     label="No. Kad Pengenalan"
                 ></FilterTextField>
-            </FilterCard>
-            <CustomTable
-                title="Sejarah Tanggung Kerja"
-                bind:tableData={table}
-                enableDetail
-                detailActions={() => goto('/perjawatan/tanggung-kerja/butiran')}
-                onUpdate={_search}
-            />
+            </FilterCard> -->
+            <div class="h h-fit w-full">
+                <DataTable
+                    title="Senarai Permohonan Tanggung Kerja"
+                    bind:tableData={employeeApplicationTable}
+                    bind:passData={rowData}
+                    detailActions={() => {
+                        goto(
+                            '/perjawatan/tanggung-kerja/butiran-' + rowData.id,
+                        );
+                    }}
+                >
+                    <FilterWrapper slot="filter">
+                        <FilterTextField
+                            label="Nama"
+                            bind:inputValue={employeeApplicationTable.param
+                                .filter.name}
+                        ></FilterTextField>
+                    </FilterWrapper>
+                </DataTable>
+            </div>
         </div>
     {/if}
 </section>
