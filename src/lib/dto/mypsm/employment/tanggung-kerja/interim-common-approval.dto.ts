c604da57d55877d0512afda289e4d018576cb014
Holifield William <holifield.william@impactbs.my>
2024-04-09 13:32:05 +0800

    feat: updated schema,services and dto for interim

---
+++ src/lib/dto/mypsm/employment/tanggung-kerja/interim-common-approval.dto.ts
@@ -0,0 +1,16 @@
+export interface InterimCommonApproval {
+    interimId: number;
+    status: boolean;
+    remark: string;
+}
+
+// Converts JSON strings to/from your types
+export class InterimCommonApprovalConvert {
+    public static fromJson(json: string): InterimCommonApproval {
+        return JSON.parse(json);
+    }
+
+    public static toJson(value: InterimCommonApproval): string {
+        return JSON.stringify(value);
+    }
+}
