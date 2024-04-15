ee2fbf067263464e527314623e9755e97919a8f2
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-04-15 10:29:56 +0800

    feat: modified new user group constant

--- src/lib/constants/core/user-group.constant.ts
+++ src/lib/constants/core/user-group.constant.ts
@@ -8,11 +8,11 @@ import type { UserGroupDTO } from '$lib/dto/core/user-group/user-group.dto';
 export class UserGroupConstant {
     public static employee: UserGroupDTO = {
         code: '889f7c6d-7373-4edc-a3cb-c7be522ec41c',
-        description: 'Kakitangan LKIM',
+        description: 'Warga LKIM',
     };
     public static contractor: UserGroupDTO = {
         code: 'ff063e92-84be-45cb-bb27-7c0f96cfdc00',
-        description: 'Kakitangan Kontrak',
+        description: 'Kontraktor',
     };
     public static candidate: UserGroupDTO = {
         code: 'f82fe23c-d4fd-4d61-9267-b16555c9db12',
