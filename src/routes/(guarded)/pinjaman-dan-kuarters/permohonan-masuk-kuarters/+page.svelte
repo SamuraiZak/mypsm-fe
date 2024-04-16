b2d868309657f7a17fba476b14b8feb4614bc4cd
Holifield William <holifield.william@impactbs.my>
2024-04-16 13:42:17 +0800

    feat: WIP integration for kuarters

--- src/routes/(guarded)/pinjaman-dan-kuarters/permohonan-masuk-kuarters/+page.svelte
+++ src/routes/(guarded)/pinjaman-dan-kuarters/permohonan-masuk-kuarters/+page.svelte
@@ -3,11 +3,15 @@
     import TextIconButton from '$lib/components/button/TextIconButton.svelte';
     import ContentHeader from '$lib/components/headers/ContentHeader.svelte';
     import CustomTable from '$lib/components/table/CustomTable.svelte';
+    import DataTable from '$lib/components/table/DataTable.svelte';
     import FilterCard from '$lib/components/table/filter/FilterCard.svelte';
     import FilterSelectField from '$lib/components/table/filter/FilterSelectField.svelte';
     import FilterTextField from '$lib/components/table/filter/FilterTextField.svelte';
     import { UserRoleConstant } from '$lib/constants/core/user-role.constant';
-    import type { TableDTO } from '$lib/dto/core/table/table.dto';
+    import type {
+        TableDTO,
+        TableSettingDTO,
+    } from '$lib/dto/core/table/table.dto';
     import type { PageData } from './$types';
     export let data: PageData;
 
@@ -23,17 +27,51 @@
         data: data.dataList ?? [],
         hiddenData: ['id'],
     };
+    let quartersTable: TableSettingDTO = {
+        param: data.param,
+        meta: data.quartersListResponse.data?.meta ?? {
+            pageSize: 1,
+            pageNum: 1,
+            totalData: 1,
+            totalPage: 1,
+        },
+        data: data.quartersList ?? [],
+        selectedData: [],
+        exportData: [],
+        hiddenColumn: ['id'],
+        dictionary: [
+            {
+                english: 'description',
+                malay: 'Nama',
+            },
+        ],
+        url: 'quarter/moving_in/list',
+        id: 'quartersTable',
+        option: {
+            checkbox: false,
+            detail: true,
+            edit: false,
+            select: false,
+            filter: false,
+        },
+        controls: {
+            add: false,
+        },
+    };
 </script>
 
 <!-- content header starts here -->
 <section class="flex w-full flex-col items-start justify-start">
     <ContentHeader title="Permohonan Menduduki Kuarters">
         {#if data.currentRoleCode == UserRoleConstant.kakitangan.code || data.currentRoleCode == UserRoleConstant.urusSetiaPeringkatNegeri.code}
-        <TextIconButton
-            label="Permohonan {data.currentRoleCode == UserRoleConstant.kakitangan.code ? "Baru" : "Luar"}"
-            icon="add"
-            onClick={() => goto('./permohonan-masuk-kuarters/butiran/baru')}
-        />
+            <TextIconButton
+                label="Permohonan {data.currentRoleCode ==
+                UserRoleConstant.kakitangan.code
+                    ? 'Baru'
+                    : 'Luar'}"
+                icon="add"
+                onClick={() => goto('./permohonan-masuk-kuarters/butiran/baru')}
+            />
         {/if}
     </ContentHeader>
 </section>
@@ -41,7 +79,7 @@
 <section
     class="max-h-[calc(100vh - 172px)] flex h-full w-full flex-col items-center justify-start overflow-y-auto"
 >
-    <div class="flex w-full flex-col justify-start gap-2.5 p-5">
+    <div class="flex w-full flex-col justify-start gap-2.5 p-10">
         <FilterCard>
             <FilterTextField label="No. Pemohon" inputValue={''} />
             <FilterTextField label="Nama Pemohon" inputValue={''} />
@@ -54,7 +92,11 @@
             bind:tableData={kuartersTable}
             bind:passData={rowData}
             enableDetail
-            detailActions={() => goto('./permohonan-masuk-kuarters/butiran/' + rowData.id)}
+            detailActions={() =>
+                goto('./permohonan-masuk-kuarters/butiran/' + rowData.id)}
         />
+        <div class="h h-fit w-full">
+            <DataTable title="Rekod Permohonan" tableData={quartersTable} />
+        </div>
     </div>
 </section>
--- src/routes/(guarded)/pinjaman-dan-kuarters/permohonan-masuk-kuarters/+page.ts
+++ src/routes/(guarded)/pinjaman-dan-kuarters/permohonan-masuk-kuarters/+page.ts
@@ -1,33 +1,41 @@
 import { LocalStorageKeyConstant } from "$lib/constants/core/local-storage-key.constant"
 import { UserRoleConstant } from "$lib/constants/core/user-role.constant"
 import type { CommonListRequestDTO } from "$lib/dto/core/common/common-list-request.dto"
+import type { CommonResponseDTO } from "$lib/dto/core/common/common-response.dto"
+import type { MovingInKuarters } from "$lib/dto/mypsm/pinjaman/kuarters/moving-in-list.dto"
+import { QuartersServices } from "$lib/services/implementation/mypsm/pinjaman-kuarters/quarters.service"
 
 export const load = async () => {
     let currentRoleCode = localStorage.getItem(LocalStorageKeyConstant.currentRoleCode)
-    let dataList:any = [];
-    
+    let quartersList: MovingInKuarters[] = [];
+    let quartersListResponse: CommonResponseDTO = {};
+    let dataList: any = [];
+
 
     let param: CommonListRequestDTO = {
         pageNum: 1,
         pageSize: 5,
-        orderBy: null,
-        orderType: null,
+        orderBy: "id",
+        orderType: 1,
         filter: {}
     }
-    if(currentRoleCode === UserRoleConstant.urusSetiaPeringkatNegeri.code){
-        dataList = [
-            { id:1, noPemohon: "K204", namaPemohon: "John K.", noKadPengenalan: "990011135667", jenisPemohon: "Kakitangan LKIM", tarikhMohon: "2024-04-22", status: "Baru", ulasan: "Menunggu Keputusan Urus Setia" },
-            { id:2, noPemohon: "K404", namaPemohon: "John C.", noKadPengenalan: "990011135667", jenisPemohon: "Agensi/Jabatan Luar", tarikhMohon: "2024-04-22", status: "Selesai", ulasan: "Diluluskan" }
-        ]
-    } else if(currentRoleCode === UserRoleConstant.kakitangan.code){
-        dataList = [
-            { id:1, noPekerja: "12394", namaKakitangan: "John K.", noKadPengenalan: "990011135667", Kategori: "Tetap", tarikhMohon: "2024-04-22", tarikhLulus: "2024-05-30", status: "Selesai", sedangDidiami: "Ya"},
-        ]
-    }
-    
+    // if (currentRoleCode === UserRoleConstant.urusSetiaPeringkatNegeri.code) {
+    //     dataList = [
+    //         { id: 1, noPemohon: "K204", namaPemohon: "John K.", noKadPengenalan: "990011135667", jenisPemohon: "Kakitangan LKIM", tarikhMohon: "2024-04-22", status: "Baru", ulasan: "Menunggu Keputusan Urus Setia" },
+    //         { id: 2, noPemohon: "K404", namaPemohon: "John C.", noKadPengenalan: "990011135667", jenisPemohon: "Agensi/Jabatan Luar", tarikhMohon: "2024-04-22", status: "Selesai", ulasan: "Diluluskan" }
+    //     ]
+    // } else if (currentRoleCode === UserRoleConstant.kakitangan.code) {
+        quartersListResponse =
+            await QuartersServices.getMovingInList(param);
+        quartersList =
+            quartersListResponse.data?.dataList as MovingInKuarters[];
+    // }
+    console.log(quartersListResponse)
     return {
         currentRoleCode,
         param,
         dataList,
+        quartersListResponse,
+        quartersList,
     }
 }   
