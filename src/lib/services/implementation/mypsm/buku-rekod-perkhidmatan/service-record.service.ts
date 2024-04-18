4a1a8898e661eb34a863c9d944f46b2f42b16b00
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-04-18 11:51:21 +0800

    feat: create services for service records

--- src/lib/services/implementation/mypsm/buku-rekod-perkhidmatan/service-record.service.ts
+++ src/lib/services/implementation/mypsm/buku-rekod-perkhidmatan/service-record.service.ts
@@ -12,6 +12,27 @@ import http from '$lib/services/implementation/service-provider.service';
 import type { Input } from 'ky';
 
 export class ServiceRecordServices {
+    // get personal info
+    static async getPersonalDetails() {
+        try {
+            const url: Input = 'service_record/personal_detail';
+
+            // get the promise response
+            const response: Response = await http.get(url).json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
     // get list of qualifications
     static async getQualificationList() {
         try {
@@ -37,6 +58,52 @@ export class ServiceRecordServices {
     static async getServiceStatementList(param: CommonListRequestDTO) {
         try {
             const url: Input = 'service_record/service_statement';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, { body: CommonListRequestConvert.toJson(param) })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get list of leave statement
+    static async getLeaveStatementList(param: CommonListRequestDTO) {
+        try {
+            const url: Input = 'service_record/leave_statement';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, { body: CommonListRequestConvert.toJson(param) })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get list of beahviour histories
+    static async getBehaviourHistoriesList(param: CommonListRequestDTO) {
+        try {
+            const url: Input = 'service_record/behaviour_histories';
 
             // get the promise response
             const response: Response = await http
