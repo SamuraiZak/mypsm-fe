afe3a7336bdab25d427989e76e0864cf385ce366
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-04-23 07:56:38 +0800

    feat: modified gcr and leave schema

--- src/lib/schemas/mypsm/leave/gcr.schema.ts
+++ src/lib/schemas/mypsm/leave/gcr.schema.ts
@@ -1,13 +1,32 @@
 import * as z from 'zod';
 
+// accumulation detail schema
 export const GcrAccumulationDetailSchema = z.object({
     collectedGCR: z.number(),
 });
 export type GcrAccumulationDetail = z.infer<typeof GcrAccumulationDetailSchema>;
 
+// gcr endorsement schema
 export const GcrEndorsementSchema = z.object({
     id: z.number(),
     status: z.boolean(),
     remark: z.string(),
 });
 export type GcrEndorsement = z.infer<typeof GcrEndorsementSchema>;
+
+// gcr add detail
+export const GcrWithdrawalDetailAddSchema = z.object({
+    employeeId: z.number(),
+    withdrawalType: z.number(),
+});
+export type GcrWithdrawalDetailAdd = z.infer<
+    typeof GcrWithdrawalDetailAddSchema
+>;
+
+export const GcrWithdrawalCalculationSchema = z.object({
+    id: z.number(),
+    totalPayment: z.number(),
+});
+export type GcrWithdrawalCalculation = z.infer<
+    typeof GcrWithdrawalCalculationSchema
+>;
