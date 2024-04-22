da0186eafa9d6ef8a2ce52d99789b3acb3303417
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-04-22 14:20:28 +0800

    feat: modified transfer services

--- src/lib/services/implementation/mypsm/employment/transfer/transfer.service.ts
+++ src/lib/services/implementation/mypsm/employment/transfer/transfer.service.ts
@@ -17,6 +17,8 @@ import type {
     TransferCommonMeeting,
     TransferCommonPostponeApplication,
     TransferCommonPostponeResult,
+    TransferSelfDetail,
+    TransferSelfReason,
 } from '$lib/schemas/mypsm/employment/transfer/transfer.schema';
 import http from '$lib/services/implementation/service-provider.service';
 import type { Input } from 'ky';
@@ -313,6 +315,431 @@ export class TransferApplicationServices {
             const promiseResponse: Promise<Response> = http
                 .post(url, {
                     body: param,
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // =====================================================
+    // SELF TRANSFER
+    // =====================================================
+    // get history list
+    static async getSelfTransferHistory(param: CommonListRequestDTO) {
+        try {
+            let url: Input = 'employment/self_transfer/list';
+
+            const response: Response = await http
+                .post(url, {
+                    body: CommonListRequestConvert.toJson(param),
+                })
+                .json();
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get self transfer application detail
+    static async getSelfTransferApplicationDetail(
+        param: TransferApplicationDetailRequestDTO,
+    ) {
+        try {
+            let url: Input = 'employment/self_transfer/get';
+
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add self transfer detail
+    static async addSelfTransferDetail(param: TransferSelfDetail) {
+        try {
+            let url: Input =
+                'employment/self_transfer/add';
+
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add self transfer reason
+    static async addSelfTransferReason(param: TransferSelfReason) {
+        try {
+            let url: Input =
+                'employment/self_transfer/add_reason';
+
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add self transfer confirmation
+    static async addSelfTransferConfirmation(param: TransferCommonEndorsement) {
+        try {
+            let url: Input =
+                'employment/self_transfer/add_confirmation';
+
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+    
+    // add common transfer document
+    static async addSelfTransferDocument(param: string) {
+        try {
+            let url: Input =
+                'employment/self_transfer/add_document';
+
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: param,
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add self transfer first director approval
+    static async addSelfTransferFirstDirectorApproval(param: TransferCommonEndorsement) {
+        try {
+            let url: Input =
+                'employment/self_transfer/add_first_director';
+
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add self transfer second director approval
+    static async addSelfTransferSecondDirectorApproval(param: TransferCommonEndorsement) {
+        try {
+            let url: Input =
+                'employment/self_transfer/add_second_director';
+
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add self transfer meeting
+    static async addSelfTransferMeeting(param: TransferCommonMeeting) {
+        try {
+            let url: Input = 'employment/self_transfer/add_meeting';
+
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add common transfer postponement application
+    static async addSelfTransferPostponeApplication(
+        param: TransferCommonPostponeApplication,
+    ) {
+        try {
+            let url: Input = 'employment/self_transfer/add_postpone';
+
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add self transfer postpone document
+    static async uploadSelfTransferPostponeDocument(param: string) {
+        try {
+            let url: Input = 'employment/self_transfer/upload_postpone_document';
+
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: param,
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add self transfer postpone result
+    static async addSelfTransferPostponeResult(param: TransferCommonPostponeResult) {
+        try {
+            let url: Input = 'employment/self_transfer/add_postpone_result';
+
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add self transfer postpone result
+    static async addSelfTransferPostponeApproval(param: TransferCommonEndorsement) {
+        try {
+            let url: Input = 'employment/self_transfer/add_postpone_approval';
+
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add self transfer endorser detail
+    static async addSelfTransferEndorserDetail(param: TransferCommonEndorserDetail) {
+        try {
+            let url: Input = 'employment/self_transfer/add_endorser';
+
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add common transfer supporter feedback
+    static async addSelfTransferSupporterFeedback(param: TransferCommonEndorsement) {
+        try {
+            let url: Input =
+                'employment/self_transfer/add_supporter_approval';
+
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add common transfer supporter feedback
+    static async addSelfTransferApproverFeedback(param: TransferCommonEndorsement) {
+        try {
+            let url: Input =
+                'employment/self_transfer/add_approver_approval';
+
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
                 })
                 .json();
 
