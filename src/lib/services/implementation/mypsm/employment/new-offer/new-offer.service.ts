b443fe29347fc1ada1542fe713775855b916a8bb
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-04-22 14:20:31 +0800

    feat: creates new offer services

---
+++ src/lib/services/implementation/mypsm/employment/new-offer/new-offer.service.ts
@@ -0,0 +1,248 @@
+import { invalidateAll } from '$app/navigation';
+import { CommonResponseConstant } from '$lib/constants/core/common-response.constant';
+import {
+    CommonListRequestConvert,
+    type CommonListRequestDTO,
+} from '$lib/dto/core/common/common-list-request.dto';
+import { CommonResponseConvert } from '$lib/dto/core/common/common-response.dto';
+import type { commonIdRequestDTO } from '$lib/dto/core/common/id-request.dto';
+import type {
+    ApproverRequestDTO,
+    MeetingResultDTO,
+    NewOfferMeetingRequestDTO,
+    NewOfferProcessRequestDTO,
+    SetSupporterApproverRequestDTO,
+} from '$lib/dto/mypsm/employment/new-offer/new-offer-request-response.dto';
+import { getPromiseToast } from '$lib/helpers/core/toast.helper';
+import http from '$lib/services/implementation/service-provider.service';
+import type { Input } from 'ky';
+
+export class NewOfferServices {
+    // get list of new offer meetings
+    static async getNewOfferMeetingList(param: CommonListRequestDTO) {
+        try {
+            const url: Input = 'employment/new_offer/list';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: CommonListRequestConvert.toJson(param),
+                })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get new offer full detail
+    static async getNewOfferFullDetail(param: commonIdRequestDTO) {
+        try {
+            const url: Input = 'employment/new_offer/detail';
+
+            // get the promise response
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // create meeting batch
+    static async createNewOfferMeetingBatchResult(
+        param: NewOfferMeetingRequestDTO,
+    ) {
+        try {
+            const url: Input = 'employment/new_offer/add';
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // update meeting result
+    static async updateNewOfferMeetingResult(param: MeetingResultDTO) {
+        try {
+            const url: Input = 'employment/new_offer/meeting/add';
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // update process result
+    static async updateNewOfferProcess(param: NewOfferProcessRequestDTO) {
+        try {
+            const url: Input = 'employment/new_offer/process/add';
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // update set approvers
+    static async setNewOfferSupporterApprover(
+        param: SetSupporterApproverRequestDTO,
+    ) {
+        try {
+            const url: Input = 'employment/new_offer/supporter_approver/add';
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // update set supporter result
+    static async createNewOfferSupporterResult(param: ApproverRequestDTO) {
+        try {
+            const url: Input = 'employment/new_offer/supporter_approval/add';
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // update approver result
+    static async createNewOfferApproverResult(param: ApproverRequestDTO) {
+        try {
+            const url: Input = 'employment/new_offer/approver_approval/add';
+
+            // get the promise response
+            const promiseRes: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            // await toast for resolved or rejected state
+            const response: Response = await getPromiseToast(promiseRes);
+
+            // parse the json response to object
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+}
