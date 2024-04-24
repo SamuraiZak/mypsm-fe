5e114b67b41d0028958c58919dff1b26233637b2
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-04-24 14:51:08 +0800

    fix: fixes typo

--- src/lib/components/stepper/StepperFailStatement.svelte
+++ src/lib/components/stepper/StepperFailStatement.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex w-full flex-col gap-10 px-3">
-    <Alert color="blue">
+    <Alert color="red">
         <p>
             <span class="font-medium">Harap maklum. </span>
             Proses permohonan diberhentikan.
