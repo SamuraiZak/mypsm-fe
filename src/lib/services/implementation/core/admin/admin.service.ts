59857a75e376e44b43ccf06d9708df440170898f
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-04-25 10:24:55 +0800

    feat: added admin services

---
+++ src/lib/services/implementation/core/admin/admin.service.ts
@@ -0,0 +1,111 @@
+import { invalidateAll } from '$app/navigation';
+import { CommonResponseConstant } from '$lib/constants/core/common-response.constant';
+import type {
+    ADEmployeeDetailRequestDTO,
+    ADRolePlusDTO,
+} from '$lib/dto/core/admin/admin.dto';
+import {
+    CommonListRequestConvert,
+    type CommonListRequestDTO,
+} from '$lib/dto/core/common/common-list-request.dto';
+import { CommonResponseConvert } from '$lib/dto/core/common/common-response.dto';
+import type { Input } from 'ky';
+import http from '../../service-provider.service';
+import { getPromiseToast } from '$lib/helpers/core/toast.helper';
+
+export class AdminServices {
+    // get list of employee
+    static async getEmployeeList(param: CommonListRequestDTO) {
+        try {
+            let url: Input = 'employee/list';
+
+            const response: Response = await http
+                .post(url, {
+                    body: CommonListRequestConvert.toJson(param),
+                })
+                .json();
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get detail of employee
+    static async getEmployeeDetail(param: ADEmployeeDetailRequestDTO) {
+        try {
+            let url: Input = 'user_management/detail';
+
+            const response: Response = await http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get list of roles
+    static async getRoleList(param: CommonListRequestDTO) {
+        try {
+            let url: Input = 'lookup/roles/filter';
+
+            const response: Response = await http
+                .post(url, {
+                    body: CommonListRequestConvert.toJson(param),
+                })
+                .json();
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // add roles
+    static async addRoles(param: ADRolePlusDTO) {
+        try {
+            let url: Input = 'user_management/update';
+
+            const promiseResponse: Promise<Response> = http
+                .put(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+
+            const response = await getPromiseToast(promiseResponse);
+
+            const result = CommonResponseConvert.fromResponse(response);
+
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+}
