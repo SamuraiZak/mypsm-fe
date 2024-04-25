08e280784eb1f8341f7c0c335e750c704c7aef53
Holifield William <holifield.william@impactbs.my>
2024-04-25 10:14:11 +0800

    feat: integration for kenaikan pangkat

--- src/routes/(guarded)/perjawatan/kenaikan-pangkat/butiran/[id]-[type]/+page.ts
+++ src/routes/(guarded)/perjawatan/kenaikan-pangkat/butiran/[id]-[type]/+page.ts
@@ -5,8 +5,8 @@ import type { DropdownDTO } from '$lib/dto/core/dropdown/dropdown.dto';
 import type { AddNewPromotion } from '$lib/dto/mypsm/employment/promotion/add-promotion.dto';
 import type { PromotionCertificationEmployee, PromotionCommonEmployee, PromotionDetail, PromotionPlacement } from '$lib/dto/mypsm/employment/promotion/promotion-common-employee.dto';
 import type { PromotionCertificationGet, PromotionGroupID } from '$lib/dto/mypsm/employment/promotion/promotion-common-groupid.dto';
-import type { PromotionCertification, PromotionEmployeeEdit, PromotionPlacementEdit } from '$lib/dto/mypsm/employment/promotion/promotion-form.dto.js';
-import { _addNewPromotion, _editEmployeePromotion, _editPromotionCertification, _editPromotionPlacement } from '$lib/schemas/mypsm/employment/promotion/promotion-schemas';
+import type { PromotionCertification, PromotionCommonApproval, PromotionEmployeeEdit, PromotionPlacementEdit } from '$lib/dto/mypsm/employment/promotion/promotion-form.dto.js';
+import { _addNewPromotion, _editEmployeePromotion, _editPromotionCertification, _editPromotionPlacement, _promotionCommonApproval } from '$lib/schemas/mypsm/employment/promotion/promotion-schemas';
 import { LookupServices } from '$lib/services/implementation/core/lookup/lookup.service';
 import { EmploymentPromotionServices } from '$lib/services/implementation/mypsm/perjawatan/employment-promotion.service';
 import { superValidate } from 'sveltekit-superforms';
@@ -49,20 +49,15 @@ export const load = async ({ params }) => {
         orderType: null,
         filter: currentId,
     };
-    const promotionEmployeeParam: CommonListRequestDTO = {
-        pageNum: 1,
-        pageSize: 5,
-        orderBy: null,
-        orderType: null,
-        filter: {
-            promotionType: promotionType,
-        }
-    }
 
     const addnewPromotionForm = await superValidate(zod(_addNewPromotion));
+    const directorForm = await superValidate(zod(_promotionCommonApproval))
+    const integrityForm = await superValidate(zod(_promotionCommonApproval))
     const certificationForm = await superValidate(zod(_editPromotionCertification), { errors: false });
-    const placementForm = await superValidate(zod(_editPromotionPlacement), { errors: false })
-    const employeePromotion = await superValidate(zod(_editEmployeePromotion), {errors: false});
+    const placementForm = await superValidate(zod(_editPromotionPlacement), { errors: false });
+    const employeePromotion = await superValidate(zod(_editEmployeePromotion), { errors: false });
+    const supporterApproval = await superValidate(zod(_promotionCommonApproval));
+    const approverApproval = await superValidate(zod(_promotionCommonApproval));
 
     employeeListResponse =
         await EmploymentPromotionServices.getEmployeeList(param);
@@ -77,29 +72,13 @@ export const load = async ({ params }) => {
     placementList =
         placementListResponse.data?.dataList as PromotionPlacement[];
     promotionDetailResponse =
-        await EmploymentPromotionServices.getPromotionEmployeeList(promotionEmployeeParam)
+        await EmploymentPromotionServices.getPromotionEmployeeList(commonParam)
     promotionDetail =
         promotionDetailResponse.data?.dataList as PromotionDetail[]
 
 
-    const dataList4 = [
-        { tarikhKuatkuasa: "17/3/2024", gajiSekarang: 'RM 1,932.13', gajiBaru: "-", gred: '-', catatan: 'Gaji Akhir Di Gred Jawatan Asal Sebelum Diberi Pergerakan Gaji', },
-        { tarikhKuatkuasa: "18/3/2024", gajiSekarang: 'RM 2,032.13', gajiBaru: "2,254.00", gred: '-', catatan: 'Gaji Sekarang - Diberi 1 KGT RM 100.00 Di Gred E19, Gaji Baru - Diberi Gaji Permulaan Di Gred E22', },
-    ];
-
-    const dataList5 = [
-        { noPekerja: 28339, name: 'David Beckham', noKadPengenalan: 990122136443, pengesahanUrusSetia: "Sedang Diproses", sokonganPenyokong: "SedangDiproses", kelulusanPelulus: "Sedang Diproses" },
-        { noPekerja: 28340, name: 'Cristiano Ronaldo', noKadPengenalan: 990122136443, pengesahanUrusSetia: "Sedang Diproses", sokonganPenyokong: "SedangDiproses", kelulusanPelulus: "Sedang Diproses" },
-        { noPekerja: 28341, name: 'Gareth Bale', noKadPengenalan: 990122136443, pengesahanUrusSetia: "Sedang Diproses", sokonganPenyokong: "SedangDiproses", kelulusanPelulus: "Sedang Diproses" },
-    ];
-    const dataList6 = [
-        { noPekerja: 28339, name: 'David Beckham', noKadPengenalan: 990122136443, skim: "tetap", gred: "1-54", penempatanSekarang: "-", status: "Dalam Proses", tindakan: "-" },
-        { noPekerja: 28340, name: 'Cristiano Ronaldo', noKadPengenalan: 990122136443, skim: "tetap", gred: "1-54", penempatanSekarang: "-", status: "Diperaku - Urus Setia Integriti", tindakan: "-" },
-    ];
 
     return {
-        dataList4, dataList5, dataList6,
-
         param,
         promotionType,
         isNewPromotion,
@@ -116,10 +95,13 @@ export const load = async ({ params }) => {
         placementListResponse,
         placementList,
         placementForm,
-        promotionEmployeeParam,
         promotionDetailResponse,
         promotionDetail,
         employeePromotion,
+        directorForm,
+        integrityForm,
+        supporterApproval,
+        approverApproval,
     };
 
 };
@@ -129,6 +111,27 @@ export const _submitAddNewPromotion = async (formData: AddNewPromotion) => {
     if (form.valid) {
         const response: CommonResponseDTO =
             await EmploymentPromotionServices.addNewPromotion(form.data as AddNewPromotion)
+
+        return { response }
+    }
+}
+
+export const _submitDirectorForm = async (formData: PromotionCommonApproval) => {
+    const form = await superValidate(formData, zod(_promotionCommonApproval));
+    console.log(form)
+    if (form.valid) {
+        const response: CommonResponseDTO =
+            await EmploymentPromotionServices.addDirectorApproval(form.data as PromotionCommonApproval)
+
+        return { response }
+    }
+}
+
+export const _submitIntegrityForm = async (formData: PromotionCommonApproval) => {
+    const form = await superValidate(formData, zod(_promotionCommonApproval));
+    if (form.valid) {
+        const response: CommonResponseDTO =
+            await EmploymentPromotionServices.addIntegrityApproval(form.data as PromotionCommonApproval)
 
         return { response }
     }
@@ -167,6 +170,28 @@ export const _submitEmployeePromotion = async (formData: PromotionEmployeeEdit) 
     }
 }
 
+export const _submitSupporterApproval = async (formData: PromotionCommonApproval) => {
+    const form = await superValidate(formData, zod(_promotionCommonApproval));
+
+    if (form.valid) {
+        const response: CommonResponseDTO =
+            await EmploymentPromotionServices.addSupporterApproval(form.data as PromotionCommonApproval)
+
+        return { response }
+    }
+}
+
+export const _submitApproverApproval = async (formData: PromotionCommonApproval) => {
+    const form = await superValidate(formData, zod(_promotionCommonApproval));
+
+    if (form.valid) {
+        const response: CommonResponseDTO =
+            await EmploymentPromotionServices.addAppproverApproval(form.data as PromotionCommonApproval)
+
+        return { response }
+    }
+}
+
 //get table detail
 export const _getPlacementDetail = async (id: PromotionCertificationGet, req: CommonListRequestDTO) => {
 
@@ -176,13 +201,30 @@ export const _getPlacementDetail = async (id: PromotionCertificationGet, req: Co
         await EmploymentPromotionServices.getSalaryAdjustmentTable(req);
     const salaryDetail: CommonResponseDTO =
         await EmploymentPromotionServices.getSalaryAdjustmentDetail(id)
+    const directorResponse: CommonResponseDTO = 
+        await EmploymentPromotionServices.getDirectorApproval(id)
+    const integrityResponse: CommonResponseDTO = 
+        await EmploymentPromotionServices.getIntegrityApproval(id)
 
-    return { response, tableResponse, salaryDetail, }
+    return { response, tableResponse, salaryDetail, directorResponse,
+        integrityResponse,}
 }
 
-export const _getPromotionDetail = async(id: PromotionCertificationGet) => {
+export const _getPromotionDetail = async (id: PromotionCertificationGet) => {
     const response: CommonResponseDTO =
         await EmploymentPromotionServices.getPromotionEmployeeDetail(id);
+
+    const suppResponse: CommonResponseDTO =
+        await EmploymentPromotionServices.getSupporterApproval(id);
+
+    const appResponse: CommonResponseDTO =
+        await EmploymentPromotionServices.getApproverApproval(id);
+
+    return { response, suppResponse, appResponse }
+}
+export const _getCertifications = async (id: PromotionCertificationGet) => {
+    const response: CommonResponseDTO =
+        await EmploymentPromotionServices.getCertificationDetail(id);
 
     return { response }
 }
