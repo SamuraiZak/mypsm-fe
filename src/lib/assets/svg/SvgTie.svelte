332bba92c1be735aa13e163cb3e529988f29719e
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-04-29 17:38:01 +0800

    feat: added svg as component

---
+++ src/lib/assets/svg/SvgTie.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    export let size: string = '16';
+</script>
+
+<svg
+    width={size}
+    height={size}
+    fill="currentColor"
+    version="1.1"
+    id="Layer_1"
+    viewBox="0 0 24 24"
+    enable-background="new 0 0 24 24"
+    xml:space="preserve"
+    ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    ></g><g id="SVGRepo_iconCarrier">
+        <metadata>
+            <sfw xmlns="&amp;ns_sfw;">
+                <slices> </slices>
+                <slicesourcebounds
+                    width="505"
+                    height="984"
+                    bottomleftorigin="true"
+                    x="0"
+                    y="-120"
+                >
+                </slicesourcebounds>
+            </sfw>
+        </metadata>
+        <g>
+            <g>
+                <g>
+                    <path
+                        d="M14,6h-4C9.6,6,9.3,5.8,9.1,5.4l-2-4C7,1.1,7,0.8,7.1,0.5S7.7,0,8,0h8c0.3,0,0.7,0.2,0.9,0.5s0.2,0.7,0,1l-2,4 C14.7,5.8,14.4,6,14,6z M10.6,4h2.8l1-2H9.6L10.6,4z"
+                    ></path>
+                </g>
+            </g>
+            <g>
+                <g>
+                    <path
+                        d="M17,19c-0.5,0-0.9-0.3-1-0.8L13.2,6h-2.4L8,18.2c-0.1,0.5-0.7,0.9-1.2,0.8c-0.5-0.1-0.9-0.7-0.8-1.2l3-13 C9.1,4.3,9.5,4,10,4h4c0.5,0,0.9,0.3,1,0.8l3,13c0.1,0.5-0.2,1.1-0.8,1.2C17.1,19,17.1,19,17,19z"
+                    ></path>
+                </g>
+            </g>
+            <g>
+                <g>
+                    <path
+                        d="M12,24c-0.3,0-0.5-0.1-0.7-0.3l-5-5c-0.4-0.4-0.4-1,0-1.4s1-0.4,1.4,0l4.3,4.3l4.3-4.3c0.4-0.4,1-0.4,1.4,0s0.4,1,0,1.4 l-5,5C12.5,23.9,12.3,24,12,24z"
+                    ></path>
+                </g>
+            </g>
+        </g>
+    </g></svg
+>
