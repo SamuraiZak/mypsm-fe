633d32bac7107c64e73422c0be4affa3f6b9d08b
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-04-29 17:36:27 +0800

    feat: Added new routercard component

--- src/lib/components/card/RouterCard.svelte
+++ src/lib/components/card/RouterCard.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
     import { goto } from '$app/navigation';
+    import SvgAccount from '$lib/assets/svg/SvgAccount.svelte';
+    import SvgAllowance from '$lib/assets/svg/SvgAllowance.svelte';
+    import SvgCalendar from '$lib/assets/svg/SvgCalendar.svelte';
+    import SvgContract from '$lib/assets/svg/SvgContract.svelte';
+    import SvgJustice from '$lib/assets/svg/SvgJustice.svelte';
+    import SvgLoan from '$lib/assets/svg/SvgLoan.svelte';
+    import SvgPerformance from '$lib/assets/svg/SvgPerformance.svelte';
+    import SvgProfile from '$lib/assets/svg/SvgProfile.svelte';
+    import SvgSalary from '$lib/assets/svg/SvgSalary.svelte';
     import SvgSetting from '$lib/assets/svg/SvgSetting.svelte';
+    import SvgTie from '$lib/assets/svg/SvgTie.svelte';
+    import SvgStethescope from '$lib/assets/svg/SvgStethescope.svelte';
     import { Badge } from 'flowbite-svelte';
+    import SvgAcademic from '$lib/assets/svg/SvgAcademic.svelte';
+    import SvgRecord from '$lib/assets/svg/SvgRecord.svelte';
+    import SvgPreference from '$lib/assets/svg/SvgPreference.svelte';
     export let url: string = '';
     export let icon: string = '';
     export let title: string = 'Title';
@@ -29,6 +43,32 @@
             <span class="text text-ios-basic-white">
                 {#if icon == 'setting'}
                     <SvgSetting size="20"></SvgSetting>
+                {:else if icon == 'user'}
+                    <SvgProfile size="21"></SvgProfile>
+                {:else if icon == 'tie'}
+                    <SvgTie size="21"></SvgTie>
+                {:else if icon == 'contract'}
+                    <SvgContract size="21"></SvgContract>
+                {:else if icon == 'calendar'}
+                    <SvgCalendar size="21"></SvgCalendar>
+                {:else if icon == 'justice'}
+                    <SvgJustice size="21"></SvgJustice>
+                {:else if icon == 'performance'}
+                    <SvgPerformance size="21"></SvgPerformance>
+                {:else if icon == 'allowance'}
+                    <SvgAllowance size="21"></SvgAllowance>
+                {:else if icon == 'loan'}
+                    <SvgLoan size="21"></SvgLoan>
+                {:else if icon == 'salary'}
+                    <SvgSalary size="21"></SvgSalary>
+                {:else if icon == 'medical'}
+                    <SvgStethescope size="21"></SvgStethescope>
+                {:else if icon == 'academic'}
+                    <SvgAcademic size="22"></SvgAcademic>
+                {:else if icon == 'record'}
+                    <SvgRecord size="22"></SvgRecord>
+                {:else if icon == 'preference'}
+                    <SvgPreference size="22"></SvgPreference>
                 {:else}
                     <slot name="icon" />
                 {/if}
