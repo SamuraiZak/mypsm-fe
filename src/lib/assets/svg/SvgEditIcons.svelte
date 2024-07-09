2fd451fa4a0c4d3ca4b8b1f3b5387f544424a76e
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-07-09 09:34:09 +0800

    feat: Created new svgs icons

---
+++ src/lib/assets/svg/SvgEditIcons.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let size: string = '16';
+</script>
+
+<svg
+    width={size}
+    height={size}
+    fill="currentColor"
+    viewBox="0 0 16 16"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <path
+        d="M11.5 1.5L14.5 4.5L5 14H2V11L11.5 1.5Z"
+        stroke="#4A5568"
+        stroke-width="1"
+        fill="none"
+    />
+    <path
+        d="M9.5 3.5L12.5 6.5"
+        stroke="#4A5568"
+        stroke-width="1"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+</svg>
---
+++ src/lib/assets/svg/checkbox.svelte/SvgCheckedFalse.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let size: string = '16';
+</script>
+
+<svg
+    viewBox="0 0 24 24"
+    width={size}
+    height={size}
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <rect
+        x="2"
+        y="2"
+        width="20"
+        height="20"
+        rx="4"
+        fill="white"
+        stroke="currentColor"
+        stroke-width="1"
+    />
+</svg>
