a01608b0c0ed8168d52360f63264276d5ff10c4b
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-07-10 13:37:46 +0800

    feat: created new schema for login

--- src/lib/schemas/core/authentication.schema.ts
+++ src/lib/schemas/core/authentication.schema.ts
@@ -1,8 +1,14 @@
 import * as z from 'zod';
 
 export const AuthenticationRequestSchema = z.object({
-    username: z.string(),
-    password: z.string(),
+    username: z.string().min(5, {
+        message:
+            'No. Kad Pengenalan / ID pengguna hendaklah diisi dengan sekurang-kurangnya 5 aksara',
+    }),
+    password: z.string().min(5, {
+        message:
+            'Kata laluan hendaklah diisi dengan sekurang-kurangnya 5 aksara',
+    }),
 });
 
 export type AuthenticationRequestDTO = z.infer<
--- src/lib/schemas/core/user-account.schema.ts
+++ src/lib/schemas/core/user-account.schema.ts
@@ -1,6 +1,8 @@
 import * as z from 'zod';
 
 export const AccountPasswordResetSchema = z.object({
-    email: z.string(),
+    email: z.string().email(),
 });
-export type AccountPasswordResetDTO = z.infer<typeof AccountPasswordResetSchema>;
+export type AccountPasswordResetDTO = z.infer<
+    typeof AccountPasswordResetSchema
+>;
