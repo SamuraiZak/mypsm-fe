6c0303965df7d6d457928380e8c961005e97019e
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-07-10 16:27:37 +0800

    fix: removes unneeded codes for adding fund application

--- src/routes/(guarded)/latihan/pembiayaan/mohon-pembiayaan/+page.ts
+++ src/routes/(guarded)/latihan/pembiayaan/mohon-pembiayaan/+page.ts
@@ -1,11 +1,8 @@
 import { goto } from '$app/navigation';
 import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key.constant';
 import { RoleConstant } from '$lib/constants/core/role.constant';
-import type { DocumentBase64RequestDTO } from '$lib/dto/core/common/base-64-document-request.dto';
 import type { CommonResponseDTO } from '$lib/dto/core/common/common-response.dto';
 import type { CourseAddFundApplicationRequestDTO } from '$lib/dto/mypsm/course/fund-application/course-fund-application.dto';
-import type { CourseFundReimbursementUploadDocumentsBase64RequestDTO } from '$lib/dto/mypsm/course/fund-reimbursement/course-fund-reimbursement-document.dto';
-import { _fileToBase64String } from '$lib/helpers/core/fileToBase64String.helper';
 import { getErrorToast } from '$lib/helpers/core/toast.helper';
 import {
     _createFundApplicationRequestSchema,
@@ -71,51 +68,9 @@ export const _createFundApplicationForm = async (formData: object) => {
             form.data as CourseAddFundApplicationRequestDTO,
         );
 
-    return { response };
-};
-
-export const _submitDocumentForm = async (id: number, files: File[]) => {
-    const documentData = new FormData();
-
-    // check file size validation
-    files.forEach((file) => {
-        documentData.append('documents', file, file.name);
-    });
-
-    const form = await superValidate(
-        documentData,
-        zod(_fundApplicationUploadDocSchema),
-    );
-
-    if (!form.valid || id === undefined) {
-        getErrorToast();
-        error(400, { message: 'Validation Not Passed!' });
-    }
-
-    // turns file into base 64 format
-    const requestBody: CourseFundReimbursementUploadDocumentsBase64RequestDTO =
-        {
-            documents: [],
-            id: id,
-        };
-
-    for (let i = 0; i < files.length; i++) {
-        const base64String = await _fileToBase64String(files[i]);
-        const documentObject: DocumentBase64RequestDTO = {
-            base64: base64String,
-            name: files[i].name,
-        };
-        requestBody.documents?.push(documentObject);
-    }
-
-    const response: CommonResponseDTO =
-        await CourseFundApplicationServices.uploadFundApplicationEmployeeDocument(
-            requestBody,
-        );
-
     if (response.status === 'success')
         setTimeout(() => {
-            goto(`../pembiayaan/${id}`);
+            goto(`../pembiayaan/${response.data?.details.id}`);
         }, 1000);
 
     return { response };
