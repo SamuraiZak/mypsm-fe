de777b01f5b1575364a668c1c8a779b6fae92ff1
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-07-10 13:35:36 +0800

    feat: added new hamburger svg

---
+++ src/lib/assets/svg/SvgHamburger.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let size: string = '16';
+</script>
+
+<svg
+    width={size}
+    height={size}
+    fill="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <rect y="4" width="24" height="3" fill="currentColor" />
+    <rect y="11" width="24" height="3" fill="currentColor" />
+    <rect y="18" width="24" height="3" fill="currentColor" />
+</svg>
