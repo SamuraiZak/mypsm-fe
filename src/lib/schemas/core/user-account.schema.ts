20ac5182a50bed798a63e988366776fccfa760f4
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-07-11 09:11:47 +0800

    feat: added user account schema

--- src/lib/schemas/core/user-account.schema.ts
+++ src/lib/schemas/core/user-account.schema.ts
@@ -6,3 +6,25 @@ export const AccountPasswordResetSchema = z.object({
 export type AccountPasswordResetDTO = z.infer<
     typeof AccountPasswordResetSchema
 >;
+
+export const AccountUpdatePasswordSchema = z.object({
+    oldPassword: z
+        .string()
+        .min(5, { message: 'Kataluan hendaklah melebihi 5 aksara' }),
+    newPassword: z
+        .string()
+        .min(5, { message: 'Kataluan hendaklah melebihi 5 aksara' }),
+    confirmPassword: z
+        .string()
+        .min(5, { message: 'Kataluan hendaklah melebihi 5 aksara' }),
+})
+.superRefine(({ confirmPassword, newPassword }, ctx) => {
+    if (confirmPassword !== newPassword) {
+        ctx.addIssue({
+            code: 'custom',
+            message: 'Kata laluan tidak sepadan',
+            path: ['confirmPassword'],
+        });
+    }
+});
+export type AccountUpdatePasswordDTO = z.infer<typeof AccountUpdatePasswordSchema>;
