e5e667828e40f2fcfa5e408f37595f61cbcdfeab
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-07-11 09:13:28 +0800

    feat: created new dashboard as the home page

--- src/routes/v2/(guarded)/dashboard/+page.ts
+++ src/routes/v2/(guarded)/dashboard/+page.ts
@@ -1,12 +1,18 @@
 import { SystemConstant } from '$lib/constants/core/system/system.constant';
+import type { CommonResponseDTO } from '$lib/dto/core/common/common-response.dto';
 import type { ModuleDTO } from '$lib/dto/core/system/system.dto';
+import type { AccountDetailsDTO } from '$lib/dto/core/user-account/user-account.dto';
+import { SystemServices } from '$lib/services/implementation/core/system/system.service';
 
-export async function load() {
+export async function load({ parent }) {
     let modules: ModuleDTO[] = SystemConstant.systemModules;
+
+    const { layoutData } = await parent();
 
     return {
         props: {
             modules,
+            layoutData,
         },
     };
 }
