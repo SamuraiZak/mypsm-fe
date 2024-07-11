d74824804d1c2a6a1f395fa425333a7fc757960d
Nielton Didi Anak James Mairodi <nielton.didi@impactbs.my>
2024-07-11 09:12:14 +0800

    feat: added services for account and service allowance

--- src/lib/services/implementation/core/system/system.service.ts
+++ src/lib/services/implementation/core/system/system.service.ts
@@ -1,3 +1,24 @@
+import { CommonResponseConstant } from "$lib/constants/core/common-response.constant";
+import { CommonResponseConvert } from "$lib/dto/core/common/common-response.dto";
+import { toasterGetMethod } from "$lib/helpers/core/french-toast.helper";
+import type { Input } from "ky";
+import http from "../../service-provider.service";
+
 export class SystemServices {
     // get current user details
+    static async getAccountDetails() {
+       try {
+          let url: Input = 'auth/detail';
+          const promiseResponse: Promise<Response> = http.get(url, {}).json();
+          const response = await toasterGetMethod(promiseResponse);
+          const result = CommonResponseConvert.fromResponse(response);
+          if (result.status == 'success') {
+             return result;
+          } else {
+             return CommonResponseConstant.httpError;
+          }
+       } catch (error) {
+          return CommonResponseConstant.httpError;
+       }
+    }
 }
--- src/lib/services/implementation/core/user-account/user-account.service.ts
+++ src/lib/services/implementation/core/user-account/user-account.service.ts
@@ -1,7 +1,15 @@
+import { invalidateAll } from '$app/navigation';
 import { CommonResponseConstant } from '$lib/constants/core/common-response.constant';
 import { CommonResponseConvert } from '$lib/dto/core/common/common-response.dto';
-import { toasterCommon } from '$lib/helpers/core/french-toast.helper';
-import type { AccountPasswordResetDTO } from '$lib/schemas/core/user-account.schema';
+import type { SwitchRoleDTO } from '$lib/dto/core/user-account/user-account.dto';
+import {
+    toasterCommon,
+    toasterGetMethod,
+} from '$lib/helpers/core/french-toast.helper';
+import type {
+    AccountPasswordResetDTO,
+    AccountUpdatePasswordDTO,
+} from '$lib/schemas/core/user-account.schema';
 import type { Input } from 'ky';
 import http from '../../service-provider.service';
 
@@ -9,6 +17,64 @@ export class AccountServices {
     static async resetPassword(param: AccountPasswordResetDTO) {
         try {
             let url: Input = 'url';
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+            const response = await toasterCommon(promiseResponse);
+            const result = CommonResponseConvert.fromResponse(response);
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    static async updatePassword(param: AccountUpdatePasswordDTO) {
+        try {
+            let url: Input = 'account/update_password';
+            const promiseResponse: Promise<Response> = http
+                .post(url, {
+                    body: JSON.stringify(param),
+                })
+                .json();
+            const response = await toasterCommon(promiseResponse);
+            const result = CommonResponseConvert.fromResponse(response);
+            if (result.status == 'success') {
+                await invalidateAll();
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    // get current user details
+    static async getAccountDetails() {
+        try {
+            let url: Input = 'auth/detail';
+            const promiseResponse: Promise<Response> = http.get(url, {}).json();
+            const response = await toasterGetMethod(promiseResponse);
+            const result = CommonResponseConvert.fromResponse(response);
+            if (result.status == 'success') {
+                return result;
+            } else {
+                return CommonResponseConstant.httpError;
+            }
+        } catch (error) {
+            return CommonResponseConstant.httpError;
+        }
+    }
+
+    static async switchRole(param: SwitchRoleDTO) {
+        try {
+            let url: Input = 'auth/switch';
             const promiseResponse: Promise<Response> = http
                 .post(url, {
                     body: JSON.stringify(param),
