12f1c6140043f3370a38db028e27923a8ad7afe0
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-07-11 17:22:09 +0800

    feat: adds isDraft property to needed schemas

--- src/lib/schemas/mypsm/employment/new-hire/schema.ts
+++ src/lib/schemas/mypsm/employment/new-hire/schema.ts
@@ -89,6 +89,7 @@ export const _personalInfoResponseSchema = z.object({
     employeePosition: z.string().nullable(),
     relationshipId: z.number().nullable(),
     isReadonly: z.boolean().readonly(),
+    isDraft: z.boolean().default(false),
 });
 
 export const _personalInfoRequestSchema = _personalInfoResponseSchema
@@ -172,10 +173,12 @@ export const _academicInfoSchema = z
 export const _academicListResponseSchema = z.object({
     academics: z.array(_academicInfoSchema),
     isReadonly: z.boolean().readonly(),
+    isDraft: z.boolean().default(false),
 });
 
 export const _academicListRequestSchema = _academicListResponseSchema.pick({
     academics: true,
+    isDraft: true,
 });
 
 //==========================================================
@@ -195,10 +198,12 @@ export const _experienceInfoSchema = z.object({
 export const _experienceListResponseSchema = z.object({
     experiences: z.array(_experienceInfoSchema),
     isReadonly: z.boolean().readonly(),
+    isDraft: z.boolean().default(false),
 });
 
 export const _experienceListRequestSchema = _experienceListResponseSchema.pick({
     experiences: true,
+    isDraft: true,
 });
 
 //==========================================================
@@ -215,10 +220,12 @@ export const _activityInfoSchema = z.object({
 export const _activityListResponseSchema = z.object({
     activities: z.array(_activityInfoSchema),
     isReadonly: z.boolean().readonly(),
+    isDraft: z.boolean().default(false),
 });
 
 export const _activityListRequestSchema = _activityListResponseSchema.pick({
     activities: true,
+    isDraft: true,
 });
 
 //==========================================================
@@ -275,6 +282,7 @@ export const _relationsSchema = z
 export const _dependencyListResponseSchema = z.object({
     dependencies: z.array(_relationsSchema),
     isReadonly: z.boolean().readonly(),
+    isDraft: z.boolean().default(false),
 });
 
 // export const _nextOfKinListResponseSchema = z.object({
@@ -284,14 +292,17 @@ export const _dependencyListResponseSchema = z.object({
 
 export const _familyListRequestSchema = _dependencyListResponseSchema.pick({
     dependencies: true,
+    isDraft: true,
 });
 
 export const _dependencyListRequestSchema = _dependencyListResponseSchema.pick({
     dependencies: true,
+    isDraft: true,
 });
 
 export const _nextOfKinListRequestSchema = z.object({
     nextOfKins: z.array(_relationsSchema),
+    isDraft: z.boolean().default(false),
 });
 
 //==========================================================
@@ -342,6 +353,7 @@ export const _serviceDetailSchema = z.object({
     epw: numberSchema,
     cola: numberSchema,
     isReadonly: z.boolean().readonly(),
+    isDraft: z.boolean().default(false),
 });
 
 export const _serviceInfoResponseSchema = _serviceDetailSchema.omit({
@@ -362,6 +374,7 @@ export const _approvalResultSchema = z.object({
     remark: longTextSchema,
     status: booleanSchema,
     isReadonly: z.boolean().readonly(),
+    isDraft: z.boolean().default(false),
     approvalDate: z.string().readonly(),
 });
 
@@ -380,9 +393,10 @@ export const _setApproversSchema = z.object({
 //==========================================================
 
 export const _getNewHireApproversSchema = z.object({
-    isReadonly: z.boolean().readonly(),
     supporterId: z.number().readonly(),
     approverId: z.number().readonly(),
+    isReadonly: z.boolean().readonly(),
+    isDraft: z.boolean().default(false),
 });
 
 //==========================================================
@@ -403,9 +417,11 @@ export const _documentsSchema = z.object({
     attachment: z.string().readonly(),
     attachmentName: z.string().readonly(),
     isReadonly: z.boolean().readonly(),
+    isDraft: z.boolean().default(false),
 });
 
 export const _uploadDocumentsSchema = z.object({
+    isDraft: z.boolean().default(false),
     document: z
         .instanceof(File, { message: 'Sila muat naik dokumen berkenaan.' })
         .refine((f) => f.size < 10_000_000, 'Maximum 10 MB saiz muat naik.')
