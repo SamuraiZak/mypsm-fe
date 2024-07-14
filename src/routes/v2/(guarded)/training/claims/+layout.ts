b5911b1588de8d19248842f043b9281ca6c18d20
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-07-14 12:15:30 +0800

    chore: migrates training-claims files to v2 folder and renames goto route and folder name to english

---
+++ src/routes/v2/(guarded)/training/claims/+layout.ts
@@ -0,0 +1,151 @@
+import { goto } from '$app/navigation';
+import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key.constant.js';
+import { RoleConstant } from '$lib/constants/core/role.constant';
+import type { CommonListRequestDTO } from '$lib/dto/core/common/common-list-request.dto';
+import type { CommonResponseDTO } from '$lib/dto/core/common/common-response.dto';
+import type { commonIdRequestDTO } from '$lib/dto/core/common/id-request.dto';
+import type { DropdownDTO } from '$lib/dto/core/dropdown/dropdown.dto';
+import type { CourseFundReimbursementListResponseDTO } from '$lib/dto/mypsm/course/fund-reimbursement/course-fund-reimbursement.dto';
+import { LookupServices } from '$lib/services/implementation/core/lookup/lookup.service';
+import { CourseFundReimbursementServices } from '$lib/services/implementation/mypsm/latihan/fundReimbursement.service';
+
+export const load = async () => {
+    let fundReimbursementResponse: CommonResponseDTO = {};
+    let fundReimbursementList = [];
+
+    const currentRoleCode = localStorage.getItem(
+        LocalStorageKeyConstant.currentRoleCode,
+    );
+
+    const isStaffRole = currentRoleCode === RoleConstant.kakitangan.code;
+
+    const param: CommonListRequestDTO = {
+        pageNum: 1,
+        pageSize: 5,
+        orderBy: 'id',
+        orderType: 1,
+        filter: {
+            employeeNumber: null,
+            employeeName: null,
+            identityDocumentNumber: null,
+            courseApplicationDate: null,
+            totalClaim: null,
+            status: null,
+        },
+    };
+
+    // fund reimbursement list
+    fundReimbursementResponse =
+        await CourseFundReimbursementServices.getCourseFundReimbursementList(
+            param,
+        );
+
+    fundReimbursementList =
+        (fundReimbursementResponse.data
+            ?.dataList as CourseFundReimbursementListResponseDTO) ?? [];
+
+    // ==========================================================================
+    // Get Lookup Functions
+    // ==========================================================================
+    const statusLookupResponse: CommonResponseDTO =
+        await LookupServices.getStatusEnums();
+
+    const statusLookup: DropdownDTO[] =
+        LookupServices.setSelectOptionsValueIsDescription(statusLookupResponse);
+
+    // ===========================================================================
+
+    const examTypeLookupResponse: CommonResponseDTO =
+        await LookupServices.getExamTypeEnums();
+
+    const examTypeLookup: DropdownDTO[] = LookupServices.setSelectOptions(
+        examTypeLookupResponse,
+    );
+
+    // ===========================================================================
+
+    const examResultLookupResponse: CommonResponseDTO =
+        await LookupServices.getExamResultEnums();
+
+    const examResultLookup: DropdownDTO[] =
+        LookupServices.setSelectOptionsValueIsDescription(
+            examResultLookupResponse,
+        );
+
+    // ===========================================================================
+
+    const educationLookupResponse: CommonResponseDTO =
+        await LookupServices.getHighestEducationEnums();
+
+    const educationLookup: DropdownDTO[] =
+        LookupServices.setSelectOptionsValueIsDescription(
+            educationLookupResponse,
+        );
+
+    // ===========================================================================
+
+    const institutionLookupResponse: CommonResponseDTO =
+        await LookupServices.getInstitutionEnums();
+
+    const institutionLookup: DropdownDTO[] =
+        LookupServices.setSelectOptionsValueIsDescription(
+            institutionLookupResponse,
+        );
+
+    return {
+        param,
+        roles: {
+            currentRoleCode,
+            isStaffRole,
+        },
+        list: {
+            fundReimbursementList,
+        },
+        responses: {
+            fundReimbursementResponse,
+        },
+        lookups: {
+            statusLookup,
+            examTypeLookup,
+            examResultLookup,
+            educationLookup,
+            institutionLookup,
+        },
+    };
+};
+
+export const _updateTable = async (param: CommonListRequestDTO) => {
+    const response: CommonResponseDTO =
+        await CourseFundReimbursementServices.getCourseFundReimbursementList(
+            param,
+        );
+
+    return {
+        param,
+        response,
+    };
+};
+
+export const _checkIfDocumentExist = async (isStaff: boolean, id: number) => {
+    const requestBody: commonIdRequestDTO = {
+        id: Number(id),
+    };
+
+    // ==========================================================================
+    // Check if document exist
+    // ==========================================================================
+    const fundReimbursementDocumentInfoResponse: CommonResponseDTO =
+        await CourseFundReimbursementServices.getCurrentCandidateDocuments(
+            requestBody,
+        );
+
+    if (fundReimbursementDocumentInfoResponse.data?.details.document === null) {
+        if (isStaff) {
+            goto(`./claims/${id}/document-upload`);
+        } else {
+            goto(`./claims/${id}`);
+        }
+    } else {
+        goto(`./claims/${id}`);
+    }
+};
---
+++ src/routes/v2/(guarded)/training/claims/add-claim/+page.ts
@@ -0,0 +1,123 @@
+import { goto } from '$app/navigation';
+import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key.constant';
+import { RoleConstant } from '$lib/constants/core/role.constant';
+import type { CommonResponseDTO } from '$lib/dto/core/common/common-response.dto';
+import type { CourseAddFundReimbursementRequestDTO } from '$lib/dto/mypsm/course/fund-reimbursement/course-fund-reimbursement.dto';
+import { getErrorToast } from '$lib/helpers/core/toast.helper';
+import {
+    _createFundReimbursementRequestSchema,
+    _fundReimbursementDetailResponseSchema,
+    _fundReimbursementUploadDocSchema,
+} from '$lib/schemas/mypsm/course/fund-reimbursement-schema';
+import { CourseFundReimbursementServices } from '$lib/services/implementation/mypsm/latihan/fundReimbursement.service';
+import { error } from '@sveltejs/kit';
+import { zod } from 'sveltekit-superforms/adapters';
+import { superValidate } from 'sveltekit-superforms/client';
+
+//==================================================
+//=============== Load Function ====================
+//==================================================
+export async function load() {
+    const currentRoleCode = localStorage.getItem(
+        LocalStorageKeyConstant.currentRoleCode,
+    );
+
+    const isStaffRole = currentRoleCode === RoleConstant.kakitangan.code;
+
+    if (!isStaffRole) {
+        error(401, { message: 'Akses ditolak' });
+    }
+
+    // ============================================================
+    // Supervalidated form initialization
+    // ============================================================
+    const reimbursementInfoForm = await superValidate(
+        zod(_fundReimbursementDetailResponseSchema),
+        { errors: false },
+    );
+
+    const fundReimbursementUploadDocumentForm = await superValidate(
+        zod(_fundReimbursementUploadDocSchema),
+        { errors: false },
+    );
+
+    return {
+        reimbursementInfoForm,
+        fundReimbursementUploadDocumentForm,
+    };
+}
+
+//==================================================
+//=============== Submit Functions =================
+//==================================================
+export const _createFundReimbursementForm = async (formData: FormData) => {
+    const form = await superValidate(
+        formData,
+        zod(_createFundReimbursementRequestSchema),
+    );
+
+    console.log(form);
+
+    if (!form.valid) {
+        getErrorToast();
+        error(400, { message: 'Validation Not Passed!' });
+    }
+
+    const response: CommonResponseDTO =
+        await CourseFundReimbursementServices.createCourseFundReimbursement(
+            form.data as CourseAddFundReimbursementRequestDTO,
+        );
+
+    if (response.status === 'success')
+        setTimeout(() => {
+            goto(`../claims/${response.data?.details.id}`);
+        }, 1000);
+
+    return { response };
+};
+
+// export const _submitDocumentForm = async (id: number, files: File[]) => {
+//     const documentData = new FormData();
+
+//     // check file size validation
+//     files.forEach((file) => {
+//         documentData.append('documents', file, file.name);
+//     });
+
+//     const form = await superValidate(
+//         documentData,
+//         zod(_fundReimbursementUploadDocSchema),
+//     );
+
+//     if (!form.valid || id === undefined) {
+//         getErrorToast();
+//         error(400, { message: 'Validation Not Passed!' });
+//     }
+
+//     // turns file into base 64 format
+//     const requestBody: CourseFundApplicationUploadDocumentsBase64RequestDTO = {
+//         documents: [],
+//         id: id,
+//     };
+
+//     for (let i = 0; i < files.length; i++) {
+//         const base64String = await _fileToBase64String(files[i]);
+//         const documentObject: DocumentBase64RequestDTO = {
+//             base64: base64String,
+//             name: files[i].name,
+//         };
+//         requestBody.documents?.push(documentObject);
+//     }
+
+//     const response: CommonResponseDTO =
+//         await CourseFundReimbursementServices.uploadFundReimbursementEmployeeDocument(
+//             requestBody,
+//         );
+
+//     if (response.status === 'success')
+//         setTimeout(() => {
+//             goto(`../claims/${id}`);
+//         }, 1000);
+
+//     return { response };
+// };
