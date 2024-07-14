91f26d36940d1ea4a420f60597cb3f44165305b1
Calvin Cliff <calvin.cliff@impact-multimedia.com>
2024-07-14 12:17:11 +0800

    chore: migrates training-record files to v2 folder and renames goto route and folder name to english

---
+++ src/routes/v2/(guarded)/training/record/+layout.ts
@@ -0,0 +1,92 @@
+import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key.constant.js';
+import { RoleConstant } from '$lib/constants/core/role.constant';
+import type { CommonListRequestDTO } from '$lib/dto/core/common/common-list-request.dto';
+import type { CommonResponseDTO } from '$lib/dto/core/common/common-response.dto';
+import type { DropdownDTO } from '$lib/dto/core/dropdown/dropdown.dto';
+import type { CourseExamApplicationListResponseDTO } from '$lib/dto/mypsm/course/exam/course-exam-application.dto';
+import { LookupServices } from '$lib/services/implementation/core/lookup/lookup.service';
+import { CourseServices } from '$lib/services/implementation/mypsm/latihan/course.service';
+
+export const load = async () => {
+    let examApplicationResponse: CommonResponseDTO = {};
+    let examApplicationList = [];
+
+    const currentRoleCode = localStorage.getItem(
+        LocalStorageKeyConstant.currentRoleCode,
+    );
+
+    const isStaffRole = currentRoleCode === RoleConstant.kakitangan.code;
+
+    const param: CommonListRequestDTO = {
+        pageNum: 1,
+        pageSize: 5,
+        orderBy: 'applicationId',
+        orderType: 1,
+        filter: {
+            employeeNumber: null,
+            employeeName: null,
+            identityDocumentNumber: null,
+            examTypeId: null,
+            examTitle: null,
+            examResult: null,
+            examStatus: null,
+        },
+    };
+
+    // exam application list
+    examApplicationResponse =
+        await CourseServices.getCourseExamApplicationList(param);
+
+    examApplicationList =
+        (examApplicationResponse.data
+            ?.dataList as CourseExamApplicationListResponseDTO) ?? [];
+
+    // ==========================================================================
+    // Get Lookup Functions
+    // ==========================================================================
+    const statusLookupResponse: CommonResponseDTO =
+        await LookupServices.getStatusEnums();
+
+    const statusLookup: DropdownDTO[] =
+        LookupServices.setSelectOptionsValueIsDescription(statusLookupResponse);
+
+    // ===========================================================================
+
+    const examTypeLookupResponse: CommonResponseDTO =
+        await LookupServices.getExamTypeEnums();
+
+    const examTypeLookup: DropdownDTO[] = LookupServices.setSelectOptions(
+        examTypeLookupResponse,
+    );
+
+    // ===========================================================================
+
+    const examResultLookupResponse: CommonResponseDTO =
+        await LookupServices.getExamResultEnums();
+
+    const examResultLookup: DropdownDTO[] =
+        LookupServices.setSelectOptionsValueIsDescription(
+            examResultLookupResponse,
+        );
+
+    // ===========================================================================
+
+    return {
+        param,
+        roles: {
+            currentRoleCode,
+            isStaffRole,
+        },
+        list: {
+            examApplicationList,
+        },
+        responses: {
+            examApplicationResponse,
+        },
+        selectionOptions: {
+            statusLookup,
+            examTypeLookup,
+            examResultLookup,
+        },
+    };
+};
---
+++ src/routes/v2/(guarded)/training/record/+page.svelte
@@ -0,0 +1,147 @@
+<script lang="ts">
+    import type { CourseExamApplicationDetailResponseDTO } from '$lib/dto/mypsm/course/exam/course-exam-application.dto';
+    import { goto } from '$app/navigation';
+    import ContentHeader from '$lib/components/headers/ContentHeader.svelte';
+    import FilterSelectField from '$lib/components/table/filter/FilterSelectField.svelte';
+    import FilterTextField from '$lib/components/table/filter/FilterTextField.svelte';
+    import type { TableSettingDTO } from '$lib/dto/core/table/table.dto';
+    import type { LayoutData } from './$types';
+    import DataTable from '$lib/components/table/DataTable.svelte';
+    import FilterWrapper from '$lib/components/table/filter/FilterWrapper.svelte';
+
+    export let data: LayoutData;
+    let rowData: CourseExamApplicationDetailResponseDTO;
+
+    // Table list - new application view for secretary role
+    let examTable: TableSettingDTO = {
+        param: data.param ?? data.param,
+        meta: data.responses.examApplicationResponse.data?.meta ?? {
+            pageSize: 1,
+            pageNum: 1,
+            totalData: 1,
+            totalPage: 1,
+        },
+        data: data.responses.examApplicationResponse.data?.dataList ?? [],
+        selectedData: [],
+        exportData: [],
+        hiddenColumn: ['applicationId', 'examId'],
+        dictionary: [
+            {
+                english: 'examTitle',
+                malay: 'Tajuk Peperiksaan',
+            },
+            {
+                english: 'examType',
+                malay: 'Jenis Peperiksaan',
+            },
+            {
+                english: 'identityDocumentNumber',
+                malay: 'No. Kad Pengenalan',
+            },
+            {
+                english: 'examApplicationOpenDate',
+                malay: 'Tarikh Buka Permohonan Peperiksaan',
+            },
+            {
+                english: 'examApplicationCloseDate',
+                malay: 'Tarikh Tutup Permohonan Peperiksaan',
+            },
+            {
+                english: 'examDate',
+                malay: 'Tarikh Peperiksaan',
+            },
+            {
+                english: 'examLocation',
+                malay: 'Lokasi Peperiksaan',
+            },
+            {
+                english: 'examResult',
+                malay: 'Keputusan Peperiksaan',
+            },
+            {
+                english: 'examStatus',
+                malay: 'Status Peperiksaan',
+            },
+            {
+                english: 'examAttendance',
+                malay: 'Kehadiran',
+            },
+        ],
+        url: 'course/exam_application/list',
+        id: 'examTable',
+        option: {
+            checkbox: false,
+            detail: true,
+            edit: false,
+            select: false,
+            filter: true,
+        },
+        controls: {
+            add: data.roles.isStaffRole,
+        },
+    };
+</script>
+
+<!-- content header starts here -->
+<section class="flex w-full flex-col items-start justify-start">
+    <ContentHeader title="Rekod Permohonan Peperiksaan"></ContentHeader>
+</section>
+
+<!-- content body starts here -->
+<!-- notes: you may change the flex directions but do not change the height and max height of this section -->
+<section
+    class="flex h-full w-full flex-col items-center justify-start overflow-y-auto"
+>
+    <div
+        class="flex h-full w-full flex-col items-center justify-start gap-2.5 p-2.5"
+    >
+        <DataTable
+            title="Senarai Permohonan Peperiksaan"
+            bind:tableData={examTable}
+            bind:passData={rowData}
+            detailActions={() => {
+                const route = `./record/${rowData.applicationId}`;
+                goto(route);
+            }}
+            addActions={() =>
+                goto('./record/add-record')}
+        >
+            <FilterWrapper slot="filter">
+                <FilterTextField
+                    label="No. Kad Pengenalan"
+                    bind:inputValue={examTable.param.filter
+                        .identityDocumentNumber}
+                ></FilterTextField>
+                <FilterTextField
+                    label="Nombor Pekerja"
+                    bind:inputValue={examTable.param.filter.employeeNumber}
+                ></FilterTextField>
+                <FilterTextField
+                    label="Nama Pekerja"
+                    bind:inputValue={examTable.param.filter.employeeName}
+                ></FilterTextField>
+                <FilterTextField
+                    label="Tajuk Peperiksaan"
+                    bind:inputValue={examTable.param.filter.examTitle}
+                ></FilterTextField>
+                <FilterSelectField
+                    label="Jenis Peperiksaan"
+                    options={data.selectionOptions.examTypeLookup}
+                    bind:inputValue={examTable.param.filter.examTypeId}
+                ></FilterSelectField>
+                <FilterSelectField
+                    label="Keputusan Peperiksaan"
+                    options={data.selectionOptions.examResultLookup}
+                    bind:inputValue={examTable.param.filter.examResult}
+                ></FilterSelectField>
+                <FilterSelectField
+                    label="Status"
+                    options={data.selectionOptions.statusLookup}
+                    bind:inputValue={examTable.param.filter.examStatus}
+                ></FilterSelectField>
+            </FilterWrapper>
+        </DataTable>
+    </div>
+</section>
+
+<!-- <Toaster /> -->
