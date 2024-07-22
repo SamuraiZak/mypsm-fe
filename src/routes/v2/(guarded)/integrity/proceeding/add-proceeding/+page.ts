705657f7f36e12a7c27b81ae9e3eb7405e0cffba
calvin.cliff <calvin.cliff@impact-multimedia.com>
2024-07-22 18:04:59 +0800

    fix: renames the route folder of add proceeding.

--- src/routes/v2/(guarded)/integrity/proceeding/add-proceeding/+page.ts
+++ src/routes/v2/(guarded)/integrity/proceeding/add-proceeding/+page.ts
@@ -1,4 +1,3 @@
-import { LocalStorageKeyConstant } from '$lib/constants/core/local-storage-key.constant.js';
 import type { CommonListRequestDTO } from '$lib/dto/core/common/common-list-request.dto';
 import type { CommonResponseDTO } from '$lib/dto/core/common/common-response.dto';
 import type { DropdownDTO } from '$lib/dto/core/dropdown/dropdown.dto';
@@ -10,9 +9,9 @@ export const load = async ({ parent }) => {
     let proceedingListResponse: CommonResponseDTO = {};
     let proceedingList = [];
 
-    const currentRoleCode = localStorage.getItem(
-        LocalStorageKeyConstant.currentRoleCode,
-    );
+    const { layoutData } = await parent();
+
+    const currentRoleCode = layoutData.accountDetails.currentRoleCode;
 
     const { roles } = await parent();
 
