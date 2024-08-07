6a493b8d0c19467b6fae24e38363e3f3d7ed8bd0
calvin.cliff <calvin.cliff@impact-multimedia.com>
2024-08-07 14:25:32 +0800

    fix: fix where div of uploaded dont show properly

--- src/routes/v2/(guarded)/employment/new-hire/new-hire-detail/[id]-[status]/+page.svelte
+++ src/routes/v2/(guarded)/employment/new-hire/new-hire-detail/[id]-[status]/+page.svelte
@@ -848,11 +848,9 @@
         });
     };
 
-    // const handleDeleteUploadedFile = (i: number) => {
-    //     $data.newHireFullDetailView.document =
-    //         $data.newHireFullDetailView.document.filter((_, index) => {
-    //             return index !== i;
-    //         });
+    // const handleDeleteUploadedFile = () => {
+    //     data.newHireFullDetailView.document.attachment = null;
+    //     data.newHireFullDetailView.document.attachmentName = null;
     // };
 </script>
 
@@ -2641,13 +2639,6 @@
                             >
                         </li>
                     </ul>
-
-                    <!-- {#if $documentFormErrors.document}
-                        <span
-                            class="font-sans text-sm italic text-system-danger"
-                            >Sila muat naik dokumen barkaitan.</span
-                        >
-                    {/if} -->
                     <form
                         id="documentUploadForm"
                         method="POST"
@@ -2683,89 +2674,93 @@
                                         document={$documentForm.document[i]}
                                     />
                                 {/each}
-                                <!-- <div class="flex flex-row">
-                                    <div
-                                        class="0 flex h-fit w-fit flex-col items-center justify-center gap-2.5 rounded-md bg-bgr-secondary p-2.5 text-sm hover:bg-bgr-tertiary"
-                                    >
-                                        <svg
-                                            fill="#ffffff"
-                                            class="mr-2 h-16 w-16 text-system-primary"
-                                            viewBox="0 0 24 24"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            ><g
-                                                id="SVGRepo_bgCarrier"
-                                                stroke-width="0"
-                                            ></g><g
-                                                id="SVGRepo_tracerCarrier"
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                            ></g><g id="SVGRepo_iconCarrier">
-                                                <path
-                                                    d="M4 4C4 3.44772 4.44772 3 5 3H14H14.5858C14.851 3 15.1054 3.10536 15.2929 3.29289L19.7071 7.70711C19.8946 7.89464 20 8.149 20 8.41421V20C20 20.5523 19.5523 21 19 21H5C4.44772 21 4 20.5523 4 20V4Z"
-                                                    stroke="currentColor"
-                                                    stroke-width="2"
-                                                    stroke-linecap="round"
-                                                ></path>
-                                                <path
-                                                    d="M20 8H15V3"
-                                                    stroke="currentColor"
-                                                    stroke-width="2"
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                ></path>
-                                                <path
-                                                    d="M11.5 13H11V17H11.5C12.6046 17 13.5 16.1046 13.5 15C13.5 13.8954 12.6046 13 11.5 13Z"
-                                                    stroke="currentColor"
-                                                    stroke-width="1.5"
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                ></path>
-                                                <path
-                                                    d="M15.5 17V13L17.5 13"
-                                                    stroke="currentColor"
-                                                    stroke-width="1.5"
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                ></path>
-                                                <path
-                                                    d="M16 15H17"
-                                                    stroke="currentColor"
-                                                    stroke-width="1.5"
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                ></path>
-                                                <path
-                                                    d="M7 17L7 15.5M7 15.5L7 13L7.75 13C8.44036 13 9 13.5596 9 14.25V14.25C9 14.9404 8.44036 15.5 7.75 15.5H7Z"
-                                                    stroke="currentColor"
-                                                    stroke-width="1.5"
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                ></path>
-                                            </g></svg
+                                <!-- {#if data.newHireFullDetailView.document.attachment !== null && $documentForm.document.length < 1}
+                                    <div class="flex flex-row">
+                                        <div
+                                            class="0 flex h-fit w-fit flex-col items-center justify-center gap-2.5 rounded-md bg-bgr-secondary p-2.5 text-sm hover:bg-bgr-tertiary"
                                         >
-                                        {#if data.newHireFullDetailView.document.attachmentName.length < 15}
-                                            {data.newHireFullDetailView.document
-                                                .attachmentName}
-                                        {:else}
-                                            {data.newHireFullDetailView.document.attachmentName.substring(
-                                                0,
-                                                15,
-                                            ) + '...'}
-                                        {/if}
-                                    </div>
-                                    <div
-                                        class=" flex h-fit w-fit flex-col items-center justify-center rounded-xl bg-bgr-primary"
-                                    >
-                                        <button
-                                            type="button"
-                                            on:click={() =>
-                                                handleDeleteUploadedFile(i)}
-                                            class="text-system-danger"
-                                            ><SvgMinusCircle size="22"
-                                            ></SvgMinusCircle></button
+                                            <svg
+                                                fill="#ffffff"
+                                                class="mr-2 h-16 w-16 text-system-primary"
+                                                viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                ><g
+                                                    id="SVGRepo_bgCarrier"
+                                                    stroke-width="0"
+                                                ></g><g
+                                                    id="SVGRepo_tracerCarrier"
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                ></g><g
+                                                    id="SVGRepo_iconCarrier"
+                                                >
+                                                    <path
+                                                        d="M4 4C4 3.44772 4.44772 3 5 3H14H14.5858C14.851 3 15.1054 3.10536 15.2929 3.29289L19.7071 7.70711C19.8946 7.89464 20 8.149 20 8.41421V20C20 20.5523 19.5523 21 19 21H5C4.44772 21 4 20.5523 4 20V4Z"
+                                                        stroke="currentColor"
+                                                        stroke-width="2"
+                                                        stroke-linecap="round"
+                                                    ></path>
+                                                    <path
+                                                        d="M20 8H15V3"
+                                                        stroke="currentColor"
+                                                        stroke-width="2"
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                    ></path>
+                                                    <path
+                                                        d="M11.5 13H11V17H11.5C12.6046 17 13.5 16.1046 13.5 15C13.5 13.8954 12.6046 13 11.5 13Z"
+                                                        stroke="currentColor"
+                                                        stroke-width="1.5"
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                    ></path>
+                                                    <path
+                                                        d="M15.5 17V13L17.5 13"
+                                                        stroke="currentColor"
+                                                        stroke-width="1.5"
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                    ></path>
+                                                    <path
+                                                        d="M16 15H17"
+                                                        stroke="currentColor"
+                                                        stroke-width="1.5"
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                    ></path>
+                                                    <path
+                                                        d="M7 17L7 15.5M7 15.5L7 13L7.75 13C8.44036 13 9 13.5596 9 14.25V14.25C9 14.9404 8.44036 15.5 7.75 15.5H7Z"
+                                                        stroke="currentColor"
+                                                        stroke-width="1.5"
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                    ></path>
+                                                </g></svg
+                                            >
+                                            {#if data.newHireFullDetailView.document.attachmentName.length < 15}
+                                                {data.newHireFullDetailView
+                                                    .document.attachmentName}
+                                            {:else}
+                                                {data.newHireFullDetailView.document.attachmentName.substring(
+                                                    0,
+                                                    15,
+                                                ) + '...'}
+                                            {/if}
+                                        </div>
+                                        <div
+                                            class=" flex h-fit w-fit flex-col items-center justify-center rounded-xl bg-bgr-primary"
                                         >
+                                            <button
+                                                type="button"
+                                                on:click={() =>
+                                                    handleDeleteUploadedFile()}
+                                                class="text-system-danger"
+                                                ><SvgMinusCircle size="22"
+                                                ></SvgMinusCircle></button
+                                            >
+                                        </div>
                                     </div>
-                                </div> -->
+                                {/if} -->
                             </div>
                             {#if $documentForm.document.length < 1}
                                 <div
@@ -2795,7 +2790,7 @@
                             {/if}
                         </div>
                     </form>
-                    {#if $isReadonlyDocumentFormStepper}
+                    {#if data.newHireFullDetailView.document.attachment !== null}
                         <div
                             class="flex max-h-full w-full flex-col items-start justify-start gap-2.5 border-b border-bdr-primary pb-5"
                         >
