64d2368d6d93d7c630e1ffc4155c76d6720b16d9
calvin.cliff <calvin.cliff@impact-multimedia.com>
2024-08-21 13:29:31 +0800

    fix: fixes wrong grade constant use for select input

--- src/routes/v2/(guarded)/employment/confirmation/[applicationId]-[employeeId]-[status]/+page.ts
+++ src/routes/v2/(guarded)/employment/confirmation/[applicationId]-[employeeId]-[status]/+page.ts
@@ -280,7 +280,7 @@ export async function load({ params, parent }) {
         await LookupServices.getServiceGradeEnums();
 
     const gradeLookup: DropdownDTO[] =
-        LookupServices.setSelectOptionsNameIsCode(gradeLookupResponse);
+        LookupServices.setSelectOptionCode(gradeLookupResponse);
 
     // ===========================================================================
 
